70417757781119100c3a6ef987909ee1f5ae31a3
Victor Rentea <victorrentea@gmail.com>
2018-11-21 10:42:56 +0200

    Singleton

--- src/main/java/victor/training/oo/creational/singleton/SingletonSpringApp.java
+++ src/main/java/victor/training/oo/creational/singleton/SingletonSpringApp.java
@@ -3,8 +3,7 @@ package victor.training.oo.creational.singleton;
 import java.util.Locale;
 import java.util.Map;
 
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.CommandLineRunner;
@@ -12,7 +11,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.context.support.SimpleThreadScope;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
@@ -35,14 +37,14 @@ public class SingletonSpringApp implements CommandLineRunner{
 	@Autowired 
 	private OrderExporter exporter;
 	
-	// TODO [1] make singleton; test multi-thread: state is [ | | | ]
-	// TODO [2] instantiate manually, set dependencies, pass around; no AOP
-	// TODO [3] prototype scope + ObjectFactory or @Lookup. Did you said "Factory"? ...
-	// TODO [4] thread/request scope. HOW it works?! Leaks: @see SimpleThreadScope javadoc
+	// [1] make singleton; test multi-thread: state is [ | | | ]
+	// [2] instantiate manually, set dependencies, pass around; no AOP
+	// [3] prototype scope + ObjectFactory or @Lookup. Did you said "Factory"? ...
+	// [4] thread/request scope. HOW it works?! Leaks: @see SimpleThreadScope javadoc
 	// TODO [5] (after AOP): RequestContext, @Cacheable. on thread?! @ThreadLocal
 	public void run(String... args) throws Exception {
-		exporter.export(Locale.ENGLISH);
-//		exporter.export(Locale.FRENCH);
+		new Thread(() -> exporter.export(Locale.ENGLISH)).start();
+		new Thread(() -> exporter.export(Locale.FRENCH)).start();
 	}
 }
 
@@ -55,6 +57,8 @@ class OrderExporter  {
 	private LabelService labelService;
 
 	public void export(Locale locale) {
+		System.out.println("Who are you? " + labelService.getClass());
+		labelService.load(locale);
 		log.debug("Running export in " + locale);
 		log.debug("Origin Country: " + labelService.getCountryName("rO")); 
 		invoiceExporter.exportInvoice();
@@ -73,7 +77,8 @@ class InvoiceExporter {
 }
 
 @Slf4j
-@Service
+@Component
+@Scope(value = "thread", proxyMode=ScopedProxyMode.TARGET_CLASS)
 class LabelService {
 	@Autowired
 	private CountryRepo countryRepo;
@@ -84,9 +89,9 @@ class LabelService {
 
 	private Map<String, String> countryNames;
 	
-	@PostConstruct
-	public void load() {
-		countryNames = countryRepo.loadCountryNamesAsMap(Locale.ENGLISH);
+//	@PostConstruct
+	public void load(Locale locale) {
+		countryNames = countryRepo.loadCountryNamesAsMap(locale);
 	}
 	
 	public String getCountryName(String iso2Code) {
