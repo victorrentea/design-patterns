d7efb9b32d65fb60ec739f651ad962dd3d725ddb
Victor Rentea <victorrentea@gmail.com>
2018-12-03 11:27:23 +0200

    make it compile

--- src/main/java/victor/training/oo/creational/singleton/bonus/ThreadScopeListener.java
+++ src/main/java/victor/training/oo/creational/singleton/bonus/ThreadScopeListener.java
@@ -1,17 +1,17 @@
-package victor.training.oo.creational.singleton.bonus;
-
-import javax.servlet.ServletRequestEvent;
-import javax.servlet.ServletRequestListener;
-
-public class ThreadScopeListener implements ServletRequestListener {
-
-	@Override
-	public void requestDestroyed(ServletRequestEvent sre) {
-		ThreadScopeContextHolder.currentThreadScopeAttributes().clear();
-	}
-
-	@Override
-	public void requestInitialized(ServletRequestEvent sre) {
-	}
-
-}
+//package victor.training.oo.creational.singleton.bonus;
+//
+//import javax.servlet.ServletRequestEvent;
+//import javax.servlet.ServletRequestListener;
+//
+//public class ThreadScopeListener implements ServletRequestListener {
+//
+//	@Override
+//	public void requestDestroyed(ServletRequestEvent sre) {
+//		ThreadScopeContextHolder.currentThreadScopeAttributes().clear();
+//	}
+//
+//	@Override
+//	public void requestInitialized(ServletRequestEvent sre) {
+//	}
+//
+//}
