7cc98ac7aa48c035f55715156756f2a3452071cd
Victor Rentea <victorrentea@gmail.com>
2019-03-27 15:23:28 +0200

    chaining events republicand alt event

--- src/main/java/victor/training/oo/behavioral/observer/ObserverSpringApp.java
+++ src/main/java/victor/training/oo/behavioral/observer/ObserverSpringApp.java
@@ -46,26 +46,30 @@ class OrderPlaced {
 	public final long orderId;
 }
 
+@Data
+class OrderInStock {
+	public final long orderId;
+}
+
 @Slf4j
 @Service
 class StockManagementService {
 	@Autowired
 	private ApplicationEventPublisher publisher;
 
-	@Order(1)
 	@EventListener
-	public void handle(OrderPlaced event) { 
+	public void handle(OrderPlaced event) {
 		log.info("Checking stock for products in order " + event.orderId);
 		log.info("If something goes wrong - throw an exception");
+		publisher.publishEvent(new OrderInStock(event.orderId));
 	}
 }
 
 @Slf4j
 @Service
 class InvoiceService {
-	@Order(2)
 	@EventListener
-	public void handle(OrderPlaced event) {
+	public void handle(OrderInStock event) {
 		log.info("Generating invoice for order " + event.orderId);
 //		new RuntimeException("thrown from generate invoice").printStackTrace(System.out);
 	} 
