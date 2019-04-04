fe420358b56cb550934c760de7a1d79e71b4db2d
Victor Rentea <victorrentea@gmail.com>
2019-04-04 16:55:11 +0300

    Chain of responsibility. Democracy. People are asked: do you want to do your job ?

--- src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
@@ -1,5 +1,7 @@
 package victor.training.oo.behavioral.strategy;
 
+import static java.util.Arrays.asList;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,10 @@ public class StrategySpringApp implements CommandLineRunner {
 	// TODO [2] Convert it to Chain Of Responsibility
 	// TODO [3] Wire with Spring
 	// TODO [4] ConfigProvider: selected based on environment props, with Spring
+	
+	@Autowired
+	CustomsService service;
 	public void run(String... args) throws Exception {
-		CustomsService service = new CustomsService();
 		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
 		System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
 		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
@@ -34,48 +38,65 @@ public class StrategySpringApp implements CommandLineRunner {
 }
 
 
-
-
-
-
-
+@Service
 class CustomsService {
+	@Autowired
+	private List<TaxCalculator> calculators;
+	
 	public double computeCustomsTax(String originCountry, double tobacoValue, double regularValue) { // UGLY API we CANNOT change
 		TaxCalculator calculator = determineCalculator(originCountry); 
 		return calculator.compute(tobacoValue, regularValue);
 	}
 
 	private TaxCalculator determineCalculator(String originCountry) {
-		switch (originCountry) { 
-		case "UK": return new UKTaxCalculator(); 
-		case "CN": return new ChinaTaxCalculator(); 
-		case "FR": 
-		case "ES": // other EU country codes...
-		case "RO": return new EUTaxCalculator();
-		default: throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
-		}
+		return calculators.stream()
+				.filter(c -> c.accepts(originCountry))
+				.findAny()
+				.orElseThrow(() -> new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry));
 	}
 
 }
 interface TaxCalculator {
 	double compute(double tobacoValue, double regularValue);
+	boolean accepts(String originCountry);
 }
 
+@Service
 class EUTaxCalculator implements TaxCalculator {
 	public double compute(double tobacoValue, double regularValue) {
 		return tobacoValue/3;
 	}
+	public boolean accepts(String originCountry) {
+		return asList("RO","FR","ES").contains(originCountry);
+	}
 }
 
+@Service
 class ChinaTaxCalculator implements TaxCalculator{
 	public double compute(double tobacoValue, double regularValue) {
 		return tobacoValue + regularValue;
 	}
+	public boolean accepts(String originCountry) {
+		return "CN".equalsIgnoreCase(originCountry);
+	}
+}
+@Service
+class RussiaTaxCalculator implements TaxCalculator{
+	public double compute(double tobacoValue, double regularValue) {
+		return tobacoValue + regularValue;
+	}
+	public boolean accepts(String originCountry) {
+		return "RU".equalsIgnoreCase(originCountry);
+	}
 }
 
-
+@Service
 class UKTaxCalculator implements TaxCalculator{
 	public double compute(double tobacoValue, double regularValue) {
 		return tobacoValue/2 + regularValue/2;
+	}
+
+	public boolean accepts(String originCountry) {
+		return "UK".equals(originCountry); 
 	}
 }
