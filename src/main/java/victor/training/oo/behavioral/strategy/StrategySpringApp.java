3613af602e7317ad028c9740f16ea6bc7c50fb0a
Victor Rentea <victorrentea@gmail.com>
2019-05-13 16:54:59 +0300

    Chain of respo cu spring @AUtowired List<Interface>

--- src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
@@ -1,5 +1,7 @@
 package victor.training.oo.behavioral.strategy;
 
+import static java.util.Arrays.asList;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +25,9 @@ public class StrategySpringApp implements CommandLineRunner {
 	// TODO [2] Convert it to Chain Of Responsibility
 	// TODO [3] Wire with Spring
 	// TODO [4] ConfigProvider: selected based on environment props, with Spring
+	@Autowired
+	CustomsService service;
 	public void run(String... args) throws Exception {
-		CustomsService service = new CustomsService();
 		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
 		System.out.println("Tax for (CH,100,100) = " + service.computeCustomsTax("CH", 100, 100));
 		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
@@ -34,43 +37,54 @@ public class StrategySpringApp implements CommandLineRunner {
 }
 
 
-
+@Service
 class CustomsService {
+	@Autowired
+	private List<CustomsCalculator> toate;
+
 	public double computeCustomsTax(String originCountry, double tobacoValue, double regularValue) { // UGLY API we CANNOT change
 		CustomsCalculator calculator = selectCustomsCalculator(originCountry);
 		return calculator.calculate(tobacoValue, regularValue);
 	}
-
+	
 	private CustomsCalculator selectCustomsCalculator(String originCountry) {
-		switch (originCountry) { 
-		case "UK": return new UKCustomsCalculator();
-		case "CH": return new CHCustomsCalculator();
-		case "FR": 
-		case "ES": // other EU country codes...
-		case "RO": return new EUCustomsCalculator();
-		default: throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
-		}
+		return toate.stream()
+				.filter(c -> c.isApplicable(originCountry))
+				.findFirst()
+				.orElseThrow(() -> new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry));
 	}
+	
 
 	interface CustomsCalculator {
 		double calculate(double tobacoValue, double regularValue);
+
+		boolean isApplicable(String originCountry);
 	}
-	
+	@Service
 	public static class EUCustomsCalculator implements CustomsCalculator { 
 		public double calculate(double tobacoValue, double regularValue) {
 			return tobacoValue/3;
 		}
+		public boolean isApplicable(String originCountry) {
+			return asList("RO","ES","FR").contains(originCountry);
+		}
 	}
-
+	@Service
 	public static class CHCustomsCalculator implements CustomsCalculator {
 		public double calculate(double tobacoValue, double regularValue) {
 			return tobacoValue + regularValue;
 		}
+		public boolean isApplicable(String originCountry) {
+			return "CH".equals(originCountry);
+		}
 	}
-
+	@Service
 	public static class UKCustomsCalculator implements CustomsCalculator {
 		public double calculate(double tobacoValue, double regularValue) {
 			return tobacoValue/2 + regularValue/2;
+		}
+		public boolean isApplicable(String originCountry) {
+			return "UK".equals(originCountry);
 		}
 	}
 }
