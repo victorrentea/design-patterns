ec6c2a34559c0a6721ec0477bd8fb9a78cad807f
Victor Rentea <victorrentea@gmail.com>
2019-05-21 16:44:58 +0300

    Chain of responsibility ("filters") care decid fiecare pentru ele daca se baga sau nu (dreptul la autodeterminare si neatarnare)

--- src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
@@ -1,8 +1,10 @@
 package victor.training.oo.behavioral.strategy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,12 +20,13 @@ public class StrategySpringApp implements CommandLineRunner {
 	
 	private ConfigProvider configProvider = new ConfigFileProvider(); 
 	
+	@Autowired
+	CustomsService service;
 	// TODO [1] Break CustomsService logic into Strategies
 	// TODO [2] Convert it to Chain Of Responsibility
 	// TODO [3] Wire with Spring
 	// TODO [4] ConfigProvider: selected based on environment props, with Spring
 	public void run(String... args) throws Exception {
-		CustomsService service = new CustomsService();
 		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
 		System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
 		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
@@ -32,47 +35,50 @@ public class StrategySpringApp implements CommandLineRunner {
 	}
 }
 
+@Service
 class CustomsService {
 	public double computeCustomsTax(String originCountry, double tobacoValue, double regularValue) { // UGLY API we CANNOT change
 		ITax tax = getTax(originCountry);
 		return tax.compute(tobacoValue, regularValue);
 
 	}
-
+	@Autowired
+	List<ITax> taxe;
 	private ITax getTax(String originCountry) {
-		List<ITax> taxe = Arrays.asList(new UKTax(), new CNTax(), new EUTax());
-		for (ITax tax : taxe) {
-			if (tax.getIsoCodes().contains(originCountry)) {
-				return tax;
-			}
-		}
-		throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
+		return taxe.stream()
+				.filter(tax -> tax.canHandle(originCountry))
+				.findFirst()
+				.orElseThrow(() -> new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry));
 	}
 }
 interface ITax {
-	List<String> getIsoCodes();
+	boolean canHandle(String originCountry);
 	double compute(double tobacoValue, double regularValue);
 }
+@Service
 class UKTax implements ITax {
-	public List<String> getIsoCodes() {
-		return Arrays.asList("UK");
+	public boolean canHandle(String originCountry) {
+		return "UK".equals(originCountry);
 	}
+
 	public double compute(double tobacoValue, double regularValue) {
 		return tobacoValue /2 + regularValue/2;
 	}
 }
+@Service
 class CNTax implements ITax {
-	public List<String> getIsoCodes() {
-		return Arrays.asList("CN");
+	public boolean canHandle(String originCountry) {
+		return "CN".equals(originCountry);
 	}
 
 	public double compute(double tobacoValue, double regularValue) {
 		return tobacoValue + regularValue;
 	}
 }
+@Service
 class EUTax implements ITax {
-	public List<String> getIsoCodes() {
-		return Arrays.asList("RO", "ES", "FR");
+	public boolean canHandle(String originCountry) {
+		return Arrays.asList("RO", "ES", "FR").contains(originCountry);
 	}
 
 	public double compute(double tobacoValue, double regularValue) {
