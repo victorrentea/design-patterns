8047712e29aef9f160c83cd1b42020b4cc3254eb
Victor Rentea <victorrentea@gmail.com>
2019-06-05 16:56:23 +0300

    Async methods

--- src/main/java/victor/training/oo/behavioral/command/CommandSpringApp.java
+++ src/main/java/victor/training/oo/behavioral/command/CommandSpringApp.java
@@ -1,10 +1,7 @@
 package victor.training.oo.behavioral.command;
 
 import java.util.Arrays;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
@@ -50,38 +48,43 @@ class Drinker implements CommandLineRunner {
 	@Autowired
 	private Barman barman;
 
-	@Autowired
-	ThreadPoolTaskExecutor pool;
-	
 	// TODO [1] inject and use a ThreadPoolTaskExecutor.submit
 	// TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
 	public void run(String... args) throws Exception {
 		log.debug("Submitting my order");
 
-		Future<Ale> futureAle = pool.submit(() -> barman.getOneAle());
-		Future<Whiskey> futureWhiskey = pool.submit(() -> barman.getOneWhiskey());
+		Future<Ale> futureAle = barman.getOneAle();
+		Future<Whiskey> futureWhiskey = barman.getOneWhiskey();
 		log.debug("The waitress left with my order");
 		Ale ale = futureAle.get();
 		Whiskey whiskey = futureWhiskey.get();
 		log.debug("Got my order! Thank you lad! " + Arrays.asList(ale, whiskey));
 
+		barman.curse("***E@!#^#&@!#&!");
+		log.debug("I go home");
 	}
 }
 
 @Slf4j
 @Service
 class Barman {
-	public Ale getOneAle() {
+	@Async
+	public Future<Ale> getOneAle() {
+		 ThreadUtils.sleep(1000);
 		 log.debug("Pouring Ale...");
-		 ThreadUtils.sleep(1000);
-		 return new Ale();
+		 return CompletableFuture.completedFuture(new Ale());
 	 }
-	
-	 public Whiskey getOneWhiskey() {
+	@Async
+	 public Future<Whiskey> getOneWhiskey() {
+		 ThreadUtils.sleep(1000);
 		 log.debug("Pouring Whiskey...");
-		 ThreadUtils.sleep(1000);
-		 return new Whiskey();
+		 return CompletableFuture.completedFuture(new Whiskey());
 	 }
+
+	 @Async
+	public void curse(String s) {
+		throw new IllegalArgumentException("I'll kill you!");
+	}
 }
 
 @Data
