5fcdd9f2aa1ca2973693ad1d476555b07904ae1d
Victor Rentea <victorrentea@gmail.com>
2019-06-05 15:35:42 +0300

    cache evict

--- src/main/java/victor/training/oo/structural/proxy/ProxySpringApp.java
+++ src/main/java/victor/training/oo/structural/proxy/ProxySpringApp.java
@@ -45,11 +45,17 @@ public class ProxySpringApp implements CommandLineRunner {
 		log.debug("10000169 is prime ? ");
 		log.debug("Got: " + ops.isPrime(10000169) + "\n");
 		
-//		log.debug("---- I/O Intensive ~ \"There are only two things hard in programming...\"");
-//		log.debug("Folder MD5: ");
-//		log.debug("Got: " + ops.hashAllFiles(new File(".")) + "\n");
-//		log.debug("Folder MD5: ");
-//		log.debug("Got: " + ops.hashAllFiles(new File(".")) + "\n");
+		log.debug("---- I/O Intensive ~ \"There are only two things hard in programming...\"");
+		log.debug("Folder MD5: ");
+		log.debug("Got: " + ops.hashAllFiles(new File(".")) + "\n");
+		log.debug("Folder MD5: ");
+		log.debug("Got: " + ops.hashAllFiles(new File(".")) + "\n");
+
+		log.debug("Suppose someone changed a file here and you find out about that?");
+		ops.killCache(new File("."));
+
+		log.debug("Folder MD5: ");
+		log.debug("Got: " + ops.hashAllFiles(new File(".")) + "\n");
 	}
 	
 	private static List<Object> getCacheKey(String methodName, Object... args) {
