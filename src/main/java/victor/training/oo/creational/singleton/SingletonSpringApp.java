4a568e075364fd452549a937e5ded470592e7b4b
Victor Rentea <victorrentea@gmail.com>
2019-08-12 10:57:08 +0300

    narrow dependency: ObjectFactor<> in loc de Spring (~gaura neagra)

--- src/main/java/victor/training/oo/creational/singleton/SingletonSpringApp.java
+++ src/main/java/victor/training/oo/creational/singleton/SingletonSpringApp.java
@@ -3,6 +3,7 @@ package victor.training.oo.creational.singleton;
 import java.util.Locale;
 import java.util.Map;
 
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.CommandLineRunner;
@@ -53,10 +54,10 @@ class OrderExporter  {
 	private InvoiceExporter invoiceExporter;
 
 	@Autowired
-	private ApplicationContext spring;
+	private ObjectFactory<LabelCache> labelCacheObjectFactory;
 
 	public void export(Locale locale) {
-		LabelCache labelCache = spring.getBean(LabelCache.class);
+		LabelCache labelCache = labelCacheObjectFactory.getObject();
 		log.debug("Running export in " + locale);
 		labelCache.load(locale);
 		log.debug("Origin Country: " + labelCache.getCountryName("rO"));
