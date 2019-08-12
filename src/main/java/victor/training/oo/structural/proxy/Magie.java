928e9f21583683b0c189bc80c92774d3aa9b904c
Victor Rentea <victorrentea@gmail.com>
2019-08-12 12:12:41 +0300

    Class Proxies

--- src/main/java/victor/training/oo/structural/proxy/Magie.java
+++ src/main/java/victor/training/oo/structural/proxy/Magie.java
@@ -1,5 +1,10 @@
 package victor.training.oo.structural.proxy;
 
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -10,27 +15,38 @@ public class Magie {
 
         MateImpl mateImpl = new MateImpl();
 
-        InvocationHandler h = new InvocationHandler() {
+//        InvocationHandler h = new InvocationHandler() {
+//            @Override
+//            public Object invoke(Object proxy,
+//                                 Method method,
+//                                 Object[] args) throws Throwable {
+//                System.out.println("Calls " +
+//                        method.getName() + " cu param " + Arrays.toString(args));
+//                return method.invoke(mateImpl, args);
+//            }
+//        };
+
+        MethodInterceptor callback = new MethodInterceptor() {
             @Override
-            public Object invoke(Object proxy,
-                                 Method method,
-                                 Object[] args) throws Throwable {
+            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
                 System.out.println("Calls " +
                         method.getName() + " cu param " + Arrays.toString(args));
                 return method.invoke(mateImpl, args);
             }
         };
-
-        Mate mate = (Mate) Proxy.newProxyInstance(
-                Magie.class.getClassLoader(),
-                new Class<?>[]{Mate.class},
-                h
-        );
+        MateImpl mate = (MateImpl) Enhancer.create(MateImpl.class,
+            callback);
+//        MateImpl mate = (Mate) Proxy.newProxyInstance(
+//                Magie.class.getClassLoader(),
+//                new Class<?>[]{Mate.class},
+//                h
+//        );
 
         businessLogic(mate);
     }
 
-    private static void businessLogic(Mate mate) {
+    private static void businessLogic(MateImpl mate) {
+        System.out.println("Oare cu cine vorbesc ? " + mate.getClass());
         System.out.println(mate.sum(1,1));
         System.out.println(mate.sum(0,2));
         System.out.println(mate.sum(1,2));
@@ -40,26 +56,14 @@ public class Magie {
 }
 
 
-interface Mate {
-    Integer sum(int a, int b);
-    Integer product(int a, int b);
-    Double sqrt(int a);
-}
 
-class MateImpl implements  Mate
-{
-
-    @Override
+class MateImpl {
     public Integer sum(int a, int b) {
         return a + b;
     }
-
-    @Override
     public Integer product(int a, int b) {
         return a * b;
     }
-
-    @Override
     public Double sqrt(int a) {
         return Math.sqrt(a);
     }
