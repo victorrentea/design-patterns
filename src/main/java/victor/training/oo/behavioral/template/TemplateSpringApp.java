6dedd17965f668d8bad13356c3b02b04bdae672c
Victor Rentea <victorrentea@gmail.com>
2019-08-13 16:45:40 +0300

    Template Method

--- src/main/java/victor/training/oo/behavioral/template/TemplateSpringApp.java
+++ src/main/java/victor/training/oo/behavioral/template/TemplateSpringApp.java
@@ -2,60 +2,76 @@ package victor.training.oo.behavioral.template;
 
 import java.util.Random;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
 
 import lombok.Data;
 
 @SpringBootApplication
 public class TemplateSpringApp implements CommandLineRunner {
-	public static void main(String[] args) {
-		SpringApplication.run(TemplateSpringApp.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateSpringApp.class, args);
+    }
 
-	@Autowired
-	private EmailService service;
-	
-	public void run(String... args) {
-		service.sendOrderReceivedEmail("a@b.com");
-	}
+//    @Autowired
+//    private AbstractEmailSender service;
+
+    public void run(String... args) {
+        new OrderReceivedEmailSender().sendEmail("a@b.com");
+        new OrderShippedEmailSender().sendEmail("a@b.com");
+    }
 }
 
-@Service
-class EmailService {
+//@Service
+abstract class AbstractEmailSender {
 
-	public void sendOrderReceivedEmail(String emailAddress) {
-		EmailContext context = new EmailContext(/*smtpConfig,etc*/);
-		int MAX_RETRIES = 3;
-		for (int i = 0; i < MAX_RETRIES; i++ ) {
-			Email email = new Email(); // constructor generates new unique ID
-			email.setSender("noreply@corp.com");
-			email.setReplyTo("/dev/null");
-			email.setTo(emailAddress);
-			email.setSubject("Order Received");
-			email.setBody("Thank you for your order");
-			boolean success = context.send(email);
-			if (success) break;
-		}
-	}
+    public void sendEmail(String emailAddress) {
+        EmailContext context = new EmailContext(/*smtpConfig,etc*/);
+        int MAX_RETRIES = 3;
+        for (int i = 0; i < MAX_RETRIES; i++) {
+            Email email = new Email(); // constructor generates new unique ID
+            email.setSender("noreply@corp.com");
+            email.setReplyTo("/dev/null");
+            email.setTo(emailAddress);
+            p(email);
+            boolean success = context.send(email);
+            if (success) break;
+        }
+    }
+
+    protected abstract void p(Email email);
+}
+
+class OrderReceivedEmailSender extends AbstractEmailSender {
+    @Override
+    protected void p(Email email) {
+        email.setSubject("Order Received");
+        email.setBody("Thank you for your order");
+    }
+}
+
+class OrderShippedEmailSender extends AbstractEmailSender {
+    @Override
+    protected void p(Email email) {
+        email.setSubject("Order Shipped");
+        email.setBody("Ti-am trimas! Speram s-ajunga (de data asta)!");
+    }
 }
 
 class EmailContext {
-	public boolean send(Email email) {
-		System.out.println("Trying to send " + email);
-		return new Random(System.nanoTime()).nextBoolean();
-	}
+    public boolean send(Email email) {
+        System.out.println("Trying to send " + email);
+        return new Random(System.nanoTime()).nextBoolean();
+    }
 }
 
 @Data
 class Email {
-	private final long id = new Random(System.nanoTime()).nextLong();
-	private String sender;
-	private String subject;
-	private String body;
-	private String replyTo;
-	private String to;
+    private final long id = new Random(System.nanoTime()).nextLong();
+    private String sender;
+    private String subject;
+    private String body;
+    private String replyTo;
+    private String to;
 }
