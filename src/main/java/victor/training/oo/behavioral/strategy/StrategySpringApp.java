911d864a9f4a67523f3157795f6be2ebdbf2e908
Victor Rentea <victorrentea@gmail.com>
2019-08-29 16:43:10 +0300

    Chain of Responsibility

--- src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
@@ -1,8 +1,15 @@
 package victor.training.oo.behavioral.strategy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @SpringBootApplication
 public class StrategySpringApp implements CommandLineRunner {
@@ -19,8 +26,8 @@ public class StrategySpringApp implements CommandLineRunner {
 	// TODO [2] Convert it to Chain Of Responsibility
 	// TODO [3] Wire with Spring
 	// TODO [4] ConfigProvider: selected based on environment props, with Spring
+		@Autowired CustomsService service;
 	public void run(String... args) {
-		CustomsService service = new CustomsService();
 		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
 		System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
 		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
@@ -29,40 +36,56 @@ public class StrategySpringApp implements CommandLineRunner {
 	}
 }
 
+@Service
 class CustomsService {
 	public double computeCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
 		TaxComputer peceva = determineTaxComputer(originCountry);
 		return peceva.compute(tobaccoValue, regularValue);
 	}
 
+	@Autowired
+	List<TaxComputer> taxComputers;
 	private TaxComputer determineTaxComputer(String originCountry) {
-		switch (originCountry) {
-		case "UK": return new UKTaxComputer();
-		case "CN": return new CNTaxComputer();
-		case "FR":
-		case "ES": // other EU country codes...
-		case "RO": return new EUTaxComputer();
-		default: throw new IllegalArgumentException("JDD: Not a valid country ISO2 code: " + originCountry);
+
+		for (TaxComputer computer : taxComputers) {
+			if (computer.areYouTheOne(originCountry)) {
+				return computer;
+			}
 		}
+		throw new IllegalArgumentException("JDD: Not a valid country ISO2 code: " + originCountry);
 	}
 
 
 }
 interface TaxComputer {
+	boolean areYouTheOne(String originCountry);
 	double compute(double tobaccoValue, double regularValue);
 }
+@Service
 class EUTaxComputer implements TaxComputer{
+	public boolean areYouTheOne(String originCountry) {
+		return asList("RO","ES","FR").contains(originCountry);
+	}
+
 	public double compute(double tobaccoValue, double regularValue) {
 		return tobaccoValue/3;
 	}
 }
+@Service
 class CNTaxComputer implements TaxComputer{
+	public boolean areYouTheOne(String originCountry) {
+		return "CN".equalsIgnoreCase(originCountry);
+	}
 	public double compute(double tobaccoValue, double regularValue) {
 		return tobaccoValue + regularValue;
 	}
 }
 
+@Service
 class UKTaxComputer implements TaxComputer{
+	public boolean areYouTheOne(String originCountry) {
+		return "UK".equalsIgnoreCase(originCountry);
+	}
 	public double compute(double tobaccoValue, double regularValue) {
 		return tobaccoValue/2 + regularValue;
 	}
