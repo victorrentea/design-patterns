33678087b7ee92ae470e37c746cfb6dd88772787
Victor Rentea <victorrentea@gmail.com>
2019-09-26 16:40:11 +0300

    @Async- de evitat. mai crestin e cu .submit()

--- src/main/java/victor/training/oo/behavioral/command/CommandSpringApp.java
+++ src/main/java/victor/training/oo/behavioral/command/CommandSpringApp.java
@@ -1,6 +1,9 @@
 package victor.training.oo.behavioral.command;
 
+import static java.util.concurrent.CompletableFuture.completedFuture;
+
 import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,7 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
@@ -23,10 +27,11 @@ import victor.training.oo.stuff.ThreadUtils;
 
 @EnableAsync
 @SpringBootApplication
-@EnableBinding({Sink.class, Source.class})
+@EnableBinding({ Sink.class, Source.class })
 public class CommandSpringApp {
 	public static void main(String[] args) {
-		SpringApplication.run(CommandSpringApp.class, args).close(); // Note: .close to stop executors after CLRunner finishes
+		SpringApplication.run(CommandSpringApp.class, args).close(); // Note: .close to stop executors after CLRunner
+																		// finishes
 	}
 
 	@Bean
@@ -50,17 +55,17 @@ class Drinker implements CommandLineRunner {
 	private Barman barman;
 	@Autowired
 	private ThreadPoolTaskExecutor pool;
-	
 
 	// TODO [1] inject and use a ThreadPoolTaskExecutor.submit
 	// TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
-    // TODO [3] wanna try it out over JMS? try out ServiceActivatorPattern
+	// TODO [3] wanna try it out over JMS? try out ServiceActivatorPattern
 	public void run(String... args) throws Exception {
-		log.debug("Submitting my order");
-		
-		Future<Ale> futureAle = pool.submit(() -> barman.getOneAle());
-		Future<Whiskey> futureWhiskey = pool.submit(() -> barman.getOneWhiskey());
-		
+		log.debug("Submitting my order to " + barman.getClass());
+
+		Future<Ale> futureAle = barman.getOneAle();
+		Future<Whiskey> futureWhiskey = barman.getOneWhiskey();
+
+		log.debug("A plecat fata cu comanda");
 		Ale ale = futureAle.get();
 		Whiskey whiskey = futureWhiskey.get();
 		log.debug("Got my order! Thank you lad! " + Arrays.asList(ale, whiskey));
@@ -70,17 +75,19 @@ class Drinker implements CommandLineRunner {
 @Slf4j
 @Service
 class Barman {
-	public Ale getOneAle() {
-		 log.debug("Pouring Ale...");
-		 ThreadUtils.sleep(1000);
-		 return new Ale();
-	 }
-	
-	 public Whiskey getOneWhiskey() {
-		 log.debug("Pouring Whiskey...");
-		 ThreadUtils.sleep(1000);
-		 return new Whiskey();
-	 }
+	@Async
+	public Future<Ale> getOneAle() {
+		log.debug("Pouring Ale...");
+		ThreadUtils.sleep(1000);
+		return completedFuture(new Ale());
+	}
+
+	@Async
+	public Future<Whiskey> getOneWhiskey() {
+		log.debug("Pouring Whiskey...");
+		ThreadUtils.sleep(1000);
+		return completedFuture(new Whiskey());
+	}
 }
 
 @Data
