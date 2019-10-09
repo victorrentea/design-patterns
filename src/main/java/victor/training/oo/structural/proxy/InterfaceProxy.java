401e7292ff4c68709cb9a32bba3339b3feb7bc77
Victor Rentea <victorrentea@gmail.com>
2019-10-09 14:03:56 +0300

    Class Proxies. Frate !!

--- src/main/java/victor/training/oo/structural/proxy/InterfaceProxy.java
+++ src/main/java/victor/training/oo/structural/proxy/InterfaceProxy.java
@@ -1,5 +1,10 @@
 package victor.training.oo.structural.proxy;
 
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
@@ -10,19 +15,17 @@ public class InterfaceProxy {
 
         MateReal aiaPeBune = new MateReal();
 
-        InvocationHandler h = new InvocationHandler() {
+        MethodInterceptor callback = new MethodInterceptor() {
             @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
                 System.out.println("SRI: cumva suni metoda " + method.getName() + " cu param " + Arrays.toString(args));
                 return method.invoke(aiaPeBune, args);
             }
         };
-        Mate mate = (Mate) Proxy.newProxyInstance(
-                InterfaceProxy.class.getClassLoader(),
-                new Class<?>[]{Mate.class},
-                h);
+        MateReal mate = (MateReal) Enhancer.create(MateReal.class, callback);
 
         // demo
+        System.out.println("Oare cu cine vorbesc ? " + mate.getClass());
         System.out.println(mate.sum(1,1));
         System.out.println(mate.sum(2,0));
         System.out.println(mate.sum(3,-1));
@@ -32,19 +35,11 @@ public class InterfaceProxy {
 }
 
 
-interface Mate {
-    Integer sum(int a, int b);
-    Integer product(int a, int b);
-}
 
-class MateReal implements Mate {
-
-    @Override
+class MateReal  {
     public Integer sum(int a, int b) {
         return a + b;
     }
-
-    @Override
     public Integer product(int a, int b) {
         return a *b;
     }
