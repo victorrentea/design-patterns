59e2f57305861f373b59d7cc1943a16c13071ea6
Victor Rentea <victorrentea@gmail.com>
2019-10-23 06:09:41 +0300

    revert command pattern

--- src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
@@ -1,15 +1,12 @@
 package victor.training.oo.behavioral.strategy;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 public class StrategySpringApp implements CommandLineRunner {
@@ -20,92 +17,31 @@ public class StrategySpringApp implements CommandLineRunner {
 	}
 
 	
-	private ConfigProvider configProvider = new ConfigFileProvider();
-	@Autowired
-	CustomsService service;
-
+	private ConfigProvider configProvider = new ConfigFileProvider(); 
+	
 	// TODO [1] Break CustomsService logic into Strategies
 	// TODO [2] Convert it to Chain Of Responsibility
 	// TODO [3] Wire with Spring
 	// TODO [4] ConfigProvider: selected based on environment props, with Spring
 	public void run(String... args) {
-		System.out.println("Tax for (RO,100,100) = " + service.calculateCustomsTax("RO", 100, 100));
-		System.out.println("Tax for (CN,100,100) = " + service.calculateCustomsTax("CN", 100, 100));
-		System.out.println("Tax for (UK,100,100) = " + service.calculateCustomsTax("UK", 100, 100));
+		CustomsService service = new CustomsService();
+		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
+		System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
+		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
 		
 		System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
 	}
 }
 
-@Service
 class CustomsService {
-	public double calculateCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
-		TaxCalculator calculator = selectCalculator(originCountry);
-		return calculator.calculate(tobaccoValue, regularValue);
-	}
-	public static Map<String, TaxCalculator> calculators = new HashMap<>();
-
-	@Autowired
-	private List<TaxCalculator> toate;
-
-	private TaxCalculator selectCalculator(String originCountry) {
-		for (TaxCalculator taxCalculator : toate) {
-			if (taxCalculator.accept(originCountry)) {
-				return taxCalculator;
-			}
-		}
-		throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
-	}
-
-	// oter typesafe spring - aware
-	enum Type1 {
-		VAL1(CNTaxCalculator.class),
-		;
-
-		private Class<? extends TaxCalculator> cnTaxCalculatorClass;
-
-		Type1(Class<CNTaxCalculator> cnTaxCalculatorClass) {
-
-			this.cnTaxCalculatorClass = cnTaxCalculatorClass;
-		}
-	}
-
-}
-interface TaxCalculator {
-	double calculate(double tobaccoValue, double regularValue);
-	boolean accept(String originCountry);
-}
-@Service
-class CNTaxCalculator implements TaxCalculator {
-	public double calculate(double tobaccoValue, double regularValue) {
-		return tobaccoValue + regularValue*2;
-	}
-	public boolean accept(String originCountry) {
-			return "CN".equals(originCountry);
-		}
-}
-@Service
-class UKTaxCalculator implements TaxCalculator {
-	public double calculate(double tobaccoValue, double regularValue) {
-		return tobaccoValue / 2 + regularValue;
-	}
-
-	@Override
-	public boolean accept(String originCountry) {
-		return "UK".equals(originCountry);
+	public double computeCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
+		switch (originCountry) { 
+		case "UK": return tobaccoValue/2 + regularValue;
+		case "CN": return tobaccoValue + regularValue;
+		case "FR": 
+		case "ES": // other EU country codes...
+		case "RO": return tobaccoValue/3;
+		default: throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
+		} 
 	}
 }
-@Service
-class EUTaxCalculator implements TaxCalculator {
-	public double calculate(double tobaccoValue, double regularValue) {
-		return tobaccoValue/3;
-	}
-	public boolean accept(String originCountry) {
-		return Arrays.asList("RO","ES","FR").contains(originCountry);
-	}
-}
-
-
-
-
-
