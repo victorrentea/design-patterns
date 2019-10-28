820e2e415f58c655da87ff5dbf1767e78bdac53e
Victor Rentea <victorrentea@gmail.com>
2019-10-28 14:01:51 +0200

    class proxies

--- src/main/java/victor/training/oo/structural/proxy/RezistGravitatieiClaseiAlaialalteDe2KLinii.java
+++ src/main/java/victor/training/oo/structural/proxy/RezistGravitatieiClaseiAlaialalteDe2KLinii.java
@@ -5,50 +5,47 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
 
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
 public class RezistGravitatieiClaseiAlaialalteDe2KLinii {
 
 	public static void main(String[] args) {
 		
 		MateReal mateReal = new MateReal();
-		InvocationHandler h = new InvocationHandler() {
+		MethodInterceptor callback = new MethodInterceptor() {
 			@Override
-			public Object invoke(Object arg0, Method method, Object[] args) throws Throwable {
+			public Object intercept(Object arg0, Method method, Object[] args, MethodProxy arg3) throws Throwable {
 				System.out.println("Cheama fraeru metoda " + method.getName() + " arg " + Arrays.toString(args));
 				return method.invoke(mateReal, args);
 			}
 		};
-		Mate mate = (Mate) Proxy.newProxyInstance(
-				RezistGravitatieiClaseiAlaialalteDe2KLinii.class.getClassLoader(), 
-				new Class<?>[] {Mate.class}, h);
-		
+		MateReal mate = (MateReal) Enhancer.create(MateReal.class, callback);
 		//demo
 		
 		bizLogic(mate);
 		
 	}
 
-	private static void bizLogic(Mate mate) {
-		System.out.println(mate.suma(1, 1));
-		System.out.println(mate.suma(2, 0));
-		System.out.println(mate.suma(3, -1));
-		System.out.println(mate.suma(3, 1));
-		System.out.println(mate.product(2, 1));
+	private static void bizLogic(MateReal tzeapa) {
+		System.out.println("Oare cu cine vorghesc de fapt ? " + tzeapa.getClass());
+		System.out.println(tzeapa.suma(1, 1));
+		System.out.println(tzeapa.suma(2, 0));
+		System.out.println(tzeapa.suma(3, -1));
+		System.out.println(tzeapa.suma(3, 1));
+		System.out.println(tzeapa.product(2, 1));
 	}
 	
 }
-interface Mate {
-	Integer suma(int a, int b);
-	Integer product(int a, int b);
-}
 
-class MateReal implements Mate {
+class MateReal  {
 
-	@Override
 	public Integer suma(int a, int b) {
 		return a + b;
 	}
 
-	@Override
 	public Integer product(int a, int b) {
 		return a * b;
 	}
