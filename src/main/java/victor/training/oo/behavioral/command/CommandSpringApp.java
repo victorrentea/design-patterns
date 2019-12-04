ec4d60e366ee548bccb578ce160c2619570e00d7
Victor Rentea <victorrentea@gmail.com>
2019-12-04 15:25:40 +0200

    Executors JavaSE style

--- src/main/java/victor/training/oo/behavioral/command/CommandSpringApp.java
+++ src/main/java/victor/training/oo/behavioral/command/CommandSpringApp.java
@@ -1,5 +1,7 @@
 package victor.training.oo.behavioral.command;
 
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,8 +15,10 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import lombok.Data;
-import lombok.extern.slf4j.Slf4j;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import static java.util.Arrays.asList;
 import static victor.training.oo.stuff.ThreadUtils.sleep;
@@ -43,7 +47,7 @@ public class CommandSpringApp {
 
 @Slf4j
 @Component
-class Drinker implements CommandLineRunner {
+class Beutor implements CommandLineRunner {
 	@Autowired
 	private Barman barman;
 	@Autowired
@@ -52,12 +56,17 @@ class Drinker implements CommandLineRunner {
 	// TODO [1] inject and use a ThreadPoolTaskExecutor.submit
 	// TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
     // TODO [3] wanna try it out over JMS? try out ServiceActivatorPattern
-	public void run(String... args) {
+	public void run(String... args) throws ExecutionException, InterruptedException {
 		log.debug("Submitting my order");
-		Beer beer = barman.pourBeer();
-		Vodka vodka = barman.pourVodka();
+
+		ExecutorService pool = Executors.newFixedThreadPool(2);
+		Future<Beer> futureBeer = pool.submit(barman::pourBeer);
+		Future<Vodka> futureVodka = pool.submit(barman::pourVodka);
+		Beer beer = futureBeer.get();
+		Vodka vodka = futureVodka.get();
 		log.debug("Waiting for my drinks...");
 		log.debug("Got my order! Thank you lad! " + asList(beer, vodka));
+		pool.shutdown();
 	}
 }
 
