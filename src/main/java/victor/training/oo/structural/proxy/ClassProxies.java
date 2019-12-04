842cc94a2ec8819ea8a4b810d1bd6da3711a773a
Victor Rentea <victorrentea@gmail.com>
2019-12-04 12:23:28 +0200

    Interceptarea metodelor dintr-o clasa concreta

--- src/main/java/victor/training/oo/structural/proxy/ClassProxies.java
+++ src/main/java/victor/training/oo/structural/proxy/ClassProxies.java
@@ -1,28 +1,32 @@
 package victor.training.oo.structural.proxy;
 
-import java.lang.reflect.InvocationHandler;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.Arrays;
 
-public class InterfaceProxies {
+public class ClassProxies {
     public static void main(String[] args) {
         MateImpl realStuff = new MateImpl();
-        InvocationHandler h = new InvocationHandler() {
+//        Mate mate = (Mate) Proxy.newProxyInstance(InterfaceProxies.class.getClassLoader(),
+//                new Class<?>[]{Mate.class}, h);
+
+        MethodInterceptor methodInterceptor = new MethodInterceptor() {
             @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
                 System.out.println("SRI: O chemi pe "  +method.getName() +
                         " cu param " + Arrays.toString(args));
                 return method.invoke(realStuff, args);
             }
         };
-        Mate mate = (Mate) Proxy.newProxyInstance(InterfaceProxies.class.getClassLoader(),
-                new Class<?>[]{Mate.class}, h);
+        MateImpl mate = (MateImpl) Enhancer.create(MateImpl.class, methodInterceptor);
 
         biznissLogic(mate);
     }
 
-    private static void biznissLogic(Mate mate) {
+    private static void biznissLogic(MateImpl mate) {
         System.out.println(mate.getClass());
         System.out.println(mate.suma(1, 4));
         System.out.println(mate.suma(0, 5));
@@ -32,18 +36,11 @@ public class InterfaceProxies {
     }
 }
 
-interface Mate {
-    int suma(int a, int b);
-    int produs(int a, int b);
-}
 
-class MateImpl implements Mate {
-    @Override
+class MateImpl {
     public int suma(int a, int b) {
         return a + b;
     }
-
-    @Override
     public int produs(int a, int b) {
         return a * b;
     }
