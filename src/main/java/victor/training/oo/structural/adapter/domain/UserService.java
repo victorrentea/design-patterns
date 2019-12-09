47c8a9874fb51efb06bb4c64319c195f0df1193e
Victor Rentea <victorrentea@gmail.com>
2019-12-09 18:29:05 +0200

    Adapter: 9/10: ai lucrat exact precum Cainele lui Pavlov. AI vazut duplicare, ai salivat, si apoi ai extras metoda

--- src/main/java/victor/training/oo/structural/adapter/domain/UserService.java
+++ src/main/java/victor/training/oo/structural/adapter/domain/UserService.java
@@ -1,45 +1,54 @@
 package victor.training.oo.structural.adapter.domain;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import lombok.extern.slf4j.Slf4j;
 import victor.training.oo.structural.adapter.infra.LdapUser;
 import victor.training.oo.structural.adapter.infra.LdapUserWebserviceClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Slf4j
-@Service
 public class UserService {
-	@Autowired
-	private LdapUserWebserviceClient wsClient;
+	private final LdapUserWebserviceClient wsClient;
+
+	public UserService(LdapUserWebserviceClient wsClient) {
+		this.wsClient = wsClient;
+	}
 
 	public void importUserFromLdap(String username) {
-		List<LdapUser> list = wsClient.search(username.toUpperCase(), null, null);
+		List<LdapUser> list = searchByUsername(username);
 		if (list.size() != 1) {
 			throw new IllegalArgumentException("There is no single user matching username " + username);
 		}
 		LdapUser ldapUser = list.get(0);
-		String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
-		User user = new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
-		
+		User user = transformUser(ldapUser);
+
 		if (user.getWorkEmail() != null) {
 			log.debug("Send welcome email to " + user.getWorkEmail());
 		}
 		log.debug("Insert user in my database");
 	}
-	
+
 	public List<User> searchUserInLdap(String username) {
-		List<LdapUser> list = wsClient.search(username.toUpperCase(), null, null);
+		List<LdapUser> list = searchByUsername(username);
 		List<User> results = new ArrayList<>();
 		for (LdapUser ldapUser : list) {
-			String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
-			User user = new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
-			results.add(user);
+			results.add(transformUser(ldapUser));
 		}
 		return results;
 	}
-	
+
+	private User transformUser(LdapUser ldapUser) {
+		String fullName = getFullName(ldapUser);
+		return new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
+	}
+
+	private String getFullName(LdapUser ldapUser) {
+		return ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
+	}
+
+	private List<LdapUser> searchByUsername(String username) {
+		return wsClient.search(username.toUpperCase(), null, null);
+	}
+
 }
