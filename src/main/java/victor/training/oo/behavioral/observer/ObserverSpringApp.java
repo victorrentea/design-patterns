658afb3262761cd0ce2d25a9c5e9c69a0699d491
Victor Rentea <victorrentea@gmail.com>
2019-12-12 15:13:10 +0100

    Multithread will give you loooooong nights

--- src/main/java/victor/training/oo/behavioral/observer/ObserverSpringApp.java
+++ src/main/java/victor/training/oo/behavioral/observer/ObserverSpringApp.java
@@ -2,27 +2,33 @@ package victor.training.oo.behavioral.observer;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.annotation.Order;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.stereotype.Service;
 
 @SpringBootApplication
+
 public class ObserverSpringApp implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(ObserverSpringApp.class, args);
 	}
-	
-//	@Bean
-//    public ApplicationEventMulticaster applicationEventMulticaster() {
-//        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
-//        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
-//        return eventMulticaster;
-//    }
+	private static final Logger log = LoggerFactory.getLogger(ObserverSpringApp.class);
+	@Bean
+    public ApplicationEventMulticaster applicationEventMulticaster() {
+        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+        return eventMulticaster;
+    }
 
 	@Autowired
 	private ApplicationEventPublisher publisher;
@@ -30,13 +36,17 @@ public class ObserverSpringApp implements CommandLineRunner {
 	@Autowired
 	private ObserverTransaction afterTransaction;
 
+
 	// TODO [1] also generate invoice
 	// TODO [2] control the order
 	// TODO [3] chain events
 	// TODO [opt] Transaction-scoped events
+
 	public void run(String... args) throws Exception {
 		publisher.publishEvent(new OrderPlaced(13));
 		//afterTransaction.runInTransaction();
+		log.info("order placed");
+
 	}
 }
 @Data
@@ -48,23 +58,26 @@ class OrderPlaced {
 class StockManagementService {
 	@Autowired
 	private ApplicationEventPublisher publisher;
-@Order(10)
+
 	@EventListener
 	public void handle(OrderPlaced event) {
 		log.info("Checking stock for products in order " + event.getOrderId());
 		log.info("If something goes wrong - throw an exception");
+		publisher.publishEvent(new OrderInStockEvent(event.getOrderId()));
 	}
+}
+@Data
+class OrderInStockEvent {
+	private final long orderId;
 }
 
 @Slf4j
 @Service
 class InvoiceService {
-	@Order(20)
-
 	@EventListener
-	public void generateInvoice(OrderPlaced event) {
+	public void generateInvoice(OrderInStockEvent event) {
 		log.info("Generating invoice for order id: " + event.getOrderId());
 		// TODO what if...
-		// throw new RuntimeException("thrown from generate invoice");
+		 throw new RuntimeException("thrown from generate invoice");
 	} 
 }
