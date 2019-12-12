f90c35958a703c25ca0a37dc77bf306225a79129
Victor Rentea <victorrentea@gmail.com>
2019-12-12 11:44:35 +0100

    YOLO

---
+++ src/main/java/victor/training/oo/creational/builder/BuildPlay.java
@@ -0,0 +1,63 @@
+//package victor.training.oo.creational.builder;
+//
+//import lombok.Data;
+//
+//public class BuildPlay {
+//    public static void main(String[] args) {
+//
+//        new X()
+//                .setA("a")
+//                .setB("b");
+//    }
+//}
+//}
+//
+//
+//@Data
+//class X {
+//    private String a,b,c,d,e;
+//
+//    public String getA() {
+//        return a;
+//    }
+//
+//    public X setA(String a) {
+//        this.a = a;
+//        return this;
+//    }
+//
+//    public String getB() {
+//        return b;
+//    }
+//
+//    public X setB(String b) {
+//        this.b = b;
+//        return this;
+//    }
+//
+//    public String getC() {
+//        return c;
+//    }
+//
+//    public X setC(String c) {
+//        this.c = c;
+//        return this;
+//    }
+//
+//    public String getD() {
+//        return d;
+//    }
+//
+//    public X setD(String d) {
+//        this.d = d;
+//        return this;
+//    }
+//
+//    public String getE() {
+//        return e;
+//    }
+//
+//    public X setE(String e) {
+//        this.e = e;
+//        return this;
+//}
