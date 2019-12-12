2072c2575c8c7e073f97811d0fa82f00adc37023
Victor Rentea <victorrentea@gmail.com>
2019-12-12 11:19:14 +0100

    ObjectFactory is a more narrow dependency thatn ApplicationContext

--- src/main/java/victor/training/oo/creational/singleton/SingletonSpringApp.java
+++ src/main/java/victor/training/oo/creational/singleton/SingletonSpringApp.java
@@ -2,13 +2,13 @@ package victor.training.oo.creational.singleton;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.SimpleThreadScope;
@@ -54,10 +54,10 @@ public class SingletonSpringApp implements CommandLineRunner{
 @RequiredArgsConstructor
 class OrderExporter  {
 	private final InvoiceExporter invoiceExporter;
-	private final ApplicationContext theWholeSpring;
+	private final ObjectFactory<LabelService> labelServiceFactory;
 
 	public void export(Locale locale) {
-		LabelService labelService = theWholeSpring.getBean(LabelService.class);
+		LabelService labelService = labelServiceFactory.getObject();
 		log.debug("Running export in " + locale);
 		labelService.load(locale);
 		log.debug("Origin Country: " + labelService.getCountryName("rO"));
