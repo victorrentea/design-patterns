169a612b7d91aa6b6233576cfff8d0953b90b998
Victor Rentea <victorrentea@gmail.com>
2019-12-12 11:54:44 +0100

    Class Interfaces

--- src/main/java/victor/training/oo/structural/proxy/LetsPlay.java
+++ src/main/java/victor/training/oo/structural/proxy/LetsPlay.java
@@ -1,29 +1,31 @@
 package victor.training.oo.structural.proxy;
 
-import java.lang.reflect.InvocationHandler;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.Arrays;
 
 public class LetsPlay {
     public static void main(String[] args) {
         MathsImpl realImplem = new MathsImpl();
-        InvocationHandler h = new InvocationHandler() {
+        MethodInterceptor interceptor = new MethodInterceptor() {
             @Override
-            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
                 System.out.println("SISMI: " +
                         method.getName() +
                         " with args " + Arrays.toString(args));
                 return method.invoke(realImplem, args);
             }
         };
-        Maths maths = (Maths) Proxy.newProxyInstance(LetsPlay.class.getClassLoader(),
-                new Class<?>[]{Maths.class}, h);
+        MathsImpl maths = (MathsImpl) Enhancer.create(MathsImpl.class, interceptor);
 
         bizLogic(maths);
     }
 
-    private static void bizLogic(Maths maths) {
+    private static void bizLogic(MathsImpl maths) {
+        System.out.println("Class : " + maths.getClass());
         System.out.println(maths.sum(1, 1));
         System.out.println(maths.sum(2, 0));
         System.out.println(maths.sum(3, -1));
@@ -34,25 +36,17 @@ public class LetsPlay {
 }
 
 
-interface Maths {
-    int sum(int a, int b);
-    int product(int a, int b);
-    int sqrt(int a);
-}
 
-class MathsImpl implements Maths {
+class MathsImpl {
 
-    @Override
     public int sum(int a, int b) {
         return a +b;
     }
 
-    @Override
     public int product(int a, int b) {
         return a*b;
     }
 
-    @Override
     public int sqrt(int a) {
         return (int) Math.sqrt(a);
     }
