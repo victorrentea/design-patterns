67d72f8e8c01ce670a77ddcc91c45ad628a426ab
Victor Rentea <victorrentea@gmail.com>
2020-01-14 19:50:44 +0200

    take(1) vs first()

--- src/main/java/victor/training/oo/behavioral/observable/pitfall/TakeOneVsFirst.java
+++ src/main/java/victor/training/oo/behavioral/observable/pitfall/TakeOneVsFirst.java
@@ -8,7 +8,7 @@ public class TakeOneVsFirst {
         System.out.println("take(1):");
         obs().take(1).subscribe(System.out::println);
         System.out.println("first():");
-        obs().first().subscribe(System.out::println);
+        obs().first().subscribe(System.out::println); // often, the best
     }
 
     private static Observable<String> obs() {
