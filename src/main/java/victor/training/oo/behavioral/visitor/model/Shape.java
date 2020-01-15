0cdf50a2be69bfa900dd499d0d23a12792567c3c
Victor Rentea <victorrentea@gmail.com>
2020-01-15 18:32:54 +0200

    clear unrelated classes

--- src/main/java/victor/training/oo/behavioral/visitor/model/Shape.java
+++
@@ -1,7 +0,0 @@
-package victor.training.oo.behavioral.visitor.model;
-
-import victor.training.oo.behavioral.visitor.ShapeVisitor;
-
-public interface Shape {
-	void accept(ShapeVisitor visitor);
-}
