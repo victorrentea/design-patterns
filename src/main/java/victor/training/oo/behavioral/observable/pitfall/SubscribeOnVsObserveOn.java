53eced07d367007f1f05e1c57804d40f40f7e368
Victor Rentea <victorrentea@gmail.com>
2020-01-15 15:01:14 +0200

    Mama! Controlul threadurilor: 1: orice operator poate schimba schedulerul folosit. 2: primul subscribeOn castiga 3: observeOn au precedenta fata de subscribeOn si au impact SUB in workflow. 4: .blocking() ruleaza ce e mai jos pe threadul initial. (gazda care programeaza Observable)

--- src/main/java/victor/training/oo/behavioral/observable/pitfall/SubscribeOnVsObserveOn.java
+++ src/main/java/victor/training/oo/behavioral/observable/pitfall/SubscribeOnVsObserveOn.java
@@ -3,30 +3,50 @@ package victor.training.oo.behavioral.observable.pitfall;
 import lombok.extern.slf4j.Slf4j;
 import rx.Observable;
 import rx.schedulers.Schedulers;
+import victor.training.oo.stuff.ThreadUtils;
 
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class SubscribeOnVsObserveOn {
     public static void main(String[] args) {
-        // TODO understand this:
-        Observable.just("a")
+
+//        Observable.defer(() -> f())
+//                .subscribeOn(Schedulers.computation())
+//                .observeOn(Schedulers.io())
+//                .doOnNext(log::info)
+//                .subscribeOn(Schedulers.io())
+//                .subscribe(log::debug);
+//        ThreadUtils.sleep(1000);
+//
+//
+//        // TODO understand this:
+        Observable.defer(() -> f()) // io
                 .subscribeOn(Schedulers.io())
-                .doOnNext(log::debug)
+                .doOnNext(log::debug) // io
                 .observeOn(Schedulers.computation())
-                .doOnNext(log::debug)
+                .doOnNext(log::debug) //com
                 .map(String::toUpperCase)
                 .observeOn(Schedulers.io())
-                .doOnNext(log::debug)
-                .flatMap(SubscribeOnVsObserveOn::httpCall)
-                .doOnNext(b->log.debug("ok?"+b))
+                .doOnNext(log::debug) // io
+                .flatMap(SubscribeOnVsObserveOn::httpCall) // io
+                .doOnNext(b->log.debug("ok?"+b)) // io GRESIT: era comput
                 .subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.newThread())
+                .doOnNext(x -> log.debug("" + x))
                 .toBlocking()
-                .subscribe(b->log.debug("end:"+b));
+                .subscribe(b->log.debug("end:"+b)); //new GRESIT: era main
+        ThreadUtils.sleep(2000);
+    }
+
+    private static Observable<String> f() {
+        log.debug("Generez");
+        return Observable.just("a");
     }
 
     public static Observable<Boolean> httpCall(String s) {
-        return Observable.just(s.equals("A")).delay(1, TimeUnit.SECONDS);
+        log.debug("Lansez HTTP");
+        return Observable.just(s.equals("A"))
+                .delay(1, TimeUnit.SECONDS);
     }
 }
