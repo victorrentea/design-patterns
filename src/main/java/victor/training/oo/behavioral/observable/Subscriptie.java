ee8b797dcd707646ee085683479c07141a9b661e
Victor Rentea <victorrentea@gmail.com>
2020-01-15 12:34:39 +0200

    Subscriptions

---
+++ src/main/java/victor/training/oo/behavioral/observable/Subscriptie.java
@@ -0,0 +1,40 @@
+package victor.training.oo.behavioral.observable;
+
+import rx.Observable;
+import rx.subscriptions.CompositeSubscription;
+import victor.training.oo.stuff.ThreadUtils;
+
+import java.util.concurrent.TimeUnit;
+
+public class Subscriptie {
+
+    public static void main(String[] args) {
+        CompositeSubscription compositeSubscription = new CompositeSubscription();
+
+        Observable<Long> o1 = Observable.interval(100, TimeUnit.MILLISECONDS);
+
+        Observable<Long> o2 = Observable.interval(200, TimeUnit.MILLISECONDS);
+
+        compositeSubscription.add(o1.subscribe(System.out::println));
+        compositeSubscription.add(o2.subscribe(System.out::println));
+
+        ThreadUtils.sleep(1000);
+        // ies din pagina
+        // din callbackup de iesire
+        compositeSubscription.clear();
+        //vs .unsubscribe
+        System.out.println("Am iesit");
+        ThreadUtils.sleep(1000);
+        System.out.println("Am intrat din nou");
+
+
+        compositeSubscription.add(o1.subscribe(System.out::println));
+        compositeSubscription.add(o2.subscribe(System.out::println));
+        ThreadUtils.sleep(1000);
+
+        compositeSubscription.clear();
+        System.out.println("Am iesit");
+
+
+    }
+}
