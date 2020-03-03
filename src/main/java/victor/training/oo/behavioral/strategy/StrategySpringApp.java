2ffcfee65cb2bd87debbb01e78b2e59c5414e3a8
Victor Rentea <victorrentea@gmail.com>
2020-03-03 16:47:55 +0200

    Chain of respo

--- src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
@@ -4,52 +4,53 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 @SpringBootApplication
 public class StrategySpringApp implements CommandLineRunner {
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(StrategySpringApp.class)
-			.profiles("localProps")
-			.run(args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(StrategySpringApp.class)
+                .profiles("localProps")
+                .run(args);
+    }
 
-	
-	private ConfigProvider configProvider = new ConfigFileProvider(); 
-	
-	// TODO [1] Break CustomsService logic into Strategies
-	// TODO [2] Convert it to Chain Of Responsibility
-	// TODO [3] Wire with Spring
-	// TODO [4] ConfigProvider: selected based on environment props, with Spring
-	public void run(String... args) {
-		CustomsService service = new CustomsService();
-		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
-		System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
-		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
-		
-		System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
-	}
+
+    private ConfigProvider configProvider = new ConfigFileProvider();
+
+    // TODO [1] Break CustomsService logic into Strategies
+    // TODO [2] Convert it to Chain Of Responsibility
+    // TODO [3] Wire with Spring
+    // TODO [4] ConfigProvider: selected based on environment props, with Spring
+    public void run(String... args) {
+        CustomsService service = new CustomsService();
+        System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
+        System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
+        System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
+
+        System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
+    }
 }
 
 class CustomsService {
-	public double computeCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
-		TaxComputer computer = selectComputer(originCountry);
-		return computer.compute(tobaccoValue, regularValue);
-	}
+    public double computeCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
+        TaxComputer computer = selectComputer(originCountry);
+        return computer.compute(tobaccoValue, regularValue);
+    }
+	private static List<TaxComputer> taxComputers = asList(
+			new UKTaxComputer(),
+			new ChinaTaxComputer(),
+			new EUTaxComputer());
+    private TaxComputer selectComputer(String originCountry) {
 
-	private TaxComputer selectComputer(String originCountry) {
-		// other EU country codes...
-		switch (originCountry) {
-			case "UK":
-				return new UKTaxComputer();
-			case "CN":
-				return new ChinaTaxComputer();
-			case "FR":
-			case "ES":
-			case "RO":
-				return new EUTaxComputer();
-			default:
-				throw new IllegalStateException("Unexpected value: " + originCountry);
-		}
-	}
+        for (TaxComputer taxComputer : taxComputers) {
+            if (taxComputer.accepts(originCountry)) {
+                return taxComputer;
+            }
+        }
+        throw new IllegalStateException("Unexpected value: " + originCountry);
+    }
 //	enum E {
 //		A,B,C
 //	}
@@ -61,34 +62,57 @@ class CustomsService {
 //		};
 //	}
 }
+
 interface TaxComputer {
-	double compute(double tobaccoValue, double regularValue);
+    boolean accepts(String originCountry);
+
+    double compute(double tobaccoValue, double regularValue);
 }
+
 class UKTaxComputer implements TaxComputer {
-	public double compute(double tobaccoValue, double regularValue) {
-		// Gigel: pun si io aicea astea 200de linii de cod si un if intautru si paote alt switch inauntru
-		return tobaccoValue / 2 + regularValue;
+	@Override
+	public boolean accepts(String originCountry) {
+		return "UK".equals(originCountry);
 	}
+
+	public double compute(double tobaccoValue, double regularValue) {
+        // Gigel: pun si io aicea astea 200de linii de cod si un if intautru si paote alt switch inauntru
+        return tobaccoValue / 2 + regularValue;
+    }
 }
+
 class ChinaTaxComputer implements TaxComputer {
+	@Override
+	public boolean accepts(String originCountry) {
+		return "CN".equals(originCountry);
+	}
+
 	public double compute(double tobaccoValue, double regularValue) {
-		return tobaccoValue + regularValue;
-	}
+        return tobaccoValue + regularValue;
+    }
 }
+
 class ChinaExtraLogic {
-	private final ChinaTaxComputer computer;
+    private final ChinaTaxComputer computer;
 
-	ChinaExtraLogic(ChinaTaxComputer computer) {
-		this.computer = computer;
-	}
-	public void logicaExtra() {
+    ChinaExtraLogic(ChinaTaxComputer computer) {
+        this.computer = computer;
+    }
 
-	}
+    public void logicaExtra() {
+
+    }
 }
+
 class EUTaxComputer implements TaxComputer {
 	@Override
-	public double compute(double tobaccoValue, double regularValue) {
-		// Maricica:
-		return tobaccoValue / 3;
+	public boolean accepts(String originCountry) {
+		return asList("RO","ES","FR").contains(originCountry);
 	}
+
+	@Override
+    public double compute(double tobaccoValue, double regularValue) {
+        // Maricica:
+        return tobaccoValue / 3;
+    }
 }
