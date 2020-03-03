f4ec780cc77a8e2a35acd8334a0c16d9d17316cd
Victor Rentea <victorrentea@gmail.com>
2020-03-03 14:59:25 +0200

    Thread loal propagation pe calluri async

--- src/main/java/victor/training/oo/structural/proxy/CumPasezSessionulFaraSaMurdarescSemnaturi.java
+++ src/main/java/victor/training/oo/structural/proxy/CumPasezSessionulFaraSaMurdarescSemnaturi.java
@@ -1,11 +1,24 @@
 package victor.training.oo.structural.proxy;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CumPasezSessionulFaraSaMurdarescSemnaturi {
+    private static final ExecutorService pool = Executors.newFixedThreadPool(1);
+
+    static public void startAsync(Runnable runnable) {
+        UserSession ses = UserSessionHolder.getCurrentSession();
+        pool.submit(() -> {
+            UserSessionHolder.setCurrentSession(ses);
+            runnable.run();
+        });
+    }
+
     public static void main(String[] args) {
         A a = new A(new B());
         // in framework
@@ -13,10 +26,9 @@ public class CumPasezSessionulFaraSaMurdarescSemnaturi {
         UserSessionHolder.setCurrentSession(ses);
 
 
-        // ses=getCurrSession(); executor.submit(() -> {restoreSessionONThread(ses);task.run();}
-
         try {
             a.m();
+            startAsync(() -> a.m());
         } finally {
             //evita mem leak + user impersonation
             UserSessionHolder.setCurrentSession(null);
@@ -27,18 +39,23 @@ public class CumPasezSessionulFaraSaMurdarescSemnaturi {
         return new UserSession("gigel");
     }
 }
+
 class UserSessionHolder {
     private final static ThreadLocal<UserSession> threadLocalSession = ThreadLocal.withInitial(() -> null);
+
     public static UserSession getCurrentSession() {
         return threadLocalSession.get();
     }
+
     public static void setCurrentSession(UserSession userSession) {
         threadLocalSession.set(userSession);
     }
 }
+
 class A {
 
     private final B b;
+
     A(B b) {
         this.b = b;
     }
@@ -48,31 +65,19 @@ class A {
     }
 
 }
+
+@Slf4j
 class B {
     public void met() {
         UserSession ses = UserSessionHolder.getCurrentSession();// de unde oare Doamne?
-        System.out.println(ses.getUsername());
+        log.debug(ses.getUsername());
     }
 
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 @Data
-class UserSession{
+class UserSession {
     private final String username;
     private final List<Long> competitii = new ArrayList<>();
 }
