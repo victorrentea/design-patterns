fe64bce10ce372014dc9f9fcaee89905fb0faa8d
Victor Rentea <victorrentea@gmail.com>
2020-03-09 00:15:59 +0200

    undo builder pattern

--- src/main/java/victor/training/oo/creational/builder/CustomerBuilder.java
+++ src/main/java/victor/training/oo/creational/builder/CustomerBuilder.java
@@ -8,22 +8,6 @@ public class CustomerBuilder {
 		return this;
 	}
 
-	// SOLUTION(
-	public CustomerBuilder withLabel(String label) {
-		customer.getLabels().add(label);
-		return this;
-	}
-
-	public CustomerBuilder withAddress(Address address) { 
-		customer.setAddress(address);
-		return this;
-	}
-	
-	public CustomerBuilder withAddress(AddressBuilder addressBuilder) { 
-		return withAddress(addressBuilder.build());
-	}
-	// SOLUTION)
-
 	public Customer build() {
 		return customer;
 	}
--- src/main/java/victor/training/oo/creational/builder/CustomerValidatorTest.java
+++ src/main/java/victor/training/oo/creational/builder/CustomerValidatorTest.java
@@ -1,8 +1,6 @@
 package victor.training.oo.creational.builder;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 public class CustomerValidatorTest {
 
@@ -10,45 +8,12 @@ public class CustomerValidatorTest {
 	
 	@Test
 	public void validCustomer_ok() {
-//		validator.validate(new CustomerBuilder()
-//			.withName("John Doe")
-//			.withAddress(new AddressBuilder()
-//					.withCity("Bucharest")
-//					.build())
-//			.build());
-		validator.validate(aValidCustomer().build()); // SOLUTION
-	}
-	
-	
-	@Test(expected = IllegalArgumentException.class) // SOLUTION
-	public void customerWithoutName_fails() {
-		validator.validate(aValidCustomer().withName(null).build()); // SOLUTION
-	}
-	
-	@Test(expected = IllegalArgumentException.class) // SOLUTION
-	public void customerWithoutAddress_fails() {
-		validator.validate(aValidCustomer().withAddress((Address)null).build()); // SOLUTION
-	}
-	
-	@Rule
-	public ExpectedException expectedException = ExpectedException.none();
-	
-	@Test // SOLUTION
-	public void customerWithoutAddressCity_fails() {
-		expectedException.expectMessage("address city");
-		validator.validate(aValidCustomer().withAddress(aValidAddress().withCity("")).build()); // SOLUTION
+		Customer customer = new Customer();
+		customer.setName("John Doe");
+		Address address = new Address();
+		address.setCity("Bucharest");
+		customer.setAddress(address);
+		validator.validate(customer);
 	}
 
-	// SOLUTION(
-	private CustomerBuilder aValidCustomer() {
-		return new CustomerBuilder()
-				.withName("John Doe")
-				.withAddress(aValidAddress());
-	}
-
-	private AddressBuilder aValidAddress() {
-		return new AddressBuilder()
-				.withStreetName("Viorele")
-				.withCity("Bucharest");
-	} 	// SOLUTION)
 }
