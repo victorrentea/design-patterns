3e7290facc17b0828d309aff0a74dec2c1ec4a5f
Victor Rentea <victorrentea@gmail.com>
2020-03-10 12:19:41 +0200

    Event Chaining

--- src/main/java/victor/training/oo/behavioral/observer/ObserverSpringApp.java
+++ src/main/java/victor/training/oo/behavioral/observer/ObserverSpringApp.java
@@ -9,16 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @SpringBootApplication
 public class ObserverSpringApp {
-	public static void main(String[] args) {
-		SpringApplication.run(ObserverSpringApp.class, args);
-	}
-	
+    public static void main(String[] args) {
+        SpringApplication.run(ObserverSpringApp.class, args);
+    }
+
 //	@Bean
 //    public ApplicationEventMulticaster applicationEventMulticaster() {
 //        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
@@ -26,49 +25,58 @@ public class ObserverSpringApp {
 //        return eventMulticaster;
 //    }
 
-	@Autowired
-	private ApplicationEventPublisher publisher;
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
 
-	@Autowired
-	private ObserverTransaction afterTransaction;
+    @Autowired
+    private ObserverTransaction afterTransaction;
 
-	// TODO [1] also generate invoice
-	// TODO [2] control the order
-	// TODO [3] chain events
-	// TODO [opt] Transaction-scoped events
-	@EventListener
-	public void placeOrder(ContextRefreshedEvent event) {
-		log.info("Halo!");
-		publisher.publishEvent(new OrderPlaced(13));
-		// afterTransaction.runInTransaction();
-	}
+    // TODO [1] also generate invoice
+    // TODO [2] control the order
+    // TODO [3] chain events
+    // TODO [opt] Transaction-scoped events
+    @EventListener
+    public void placeOrder(ContextRefreshedEvent event) {
+        log.info("Halo!");
+        publisher.publishEvent(new OrderPlaced(13));
+        // afterTransaction.runInTransaction();
+    }
 }
+
 @Data
 class OrderPlaced {
-	private final long orderId;
+    private final long orderId;
 }
+
 // mari si tari
 @Slf4j
 @Service
 @RequiredArgsConstructor
 class StockManagementService {
-//	private static final Logger log = LoggerFactory.getLogger(StockManagementService.class);
-	@Order(10)
-	@EventListener
-	public void handle(OrderPlaced orderPlaced) {
-		log.info("Checking stock for products in order " + orderPlaced.getOrderId());
-		log.info("If something goes wrong - throw an exception");
-	}
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @EventListener
+    public void handle(OrderPlaced orderPlaced) {
+        log.info("Checking stock for products in order " + orderPlaced.getOrderId());
+        log.info("If something goes wrong - throw an exception");
+        publisher.publishEvent(new OrderInStock(orderPlaced.getOrderId()));
+    }
 }
+
+@Data
+class OrderInStock {
+    private final long orderId;
+}
+
 @Slf4j
 @Service
 class InvoiceService {
-	@Order(20)
-	@EventListener
-	public void generateInvoice(OrderPlaced orderPlaced) {
-		log.info("Generating invoice for order id: " + orderPlaced.getOrderId());
-		// TODO what if...
-		// throw new RuntimeException("thrown from generate invoice");
-	} 
+    @EventListener
+    public void generateInvoice(OrderInStock orderInStock) {
+        log.info("Generating invoice for order id: " + orderInStock.getOrderId());
+        // TODO what if...
+        // throw new RuntimeException("thrown from generate invoice");
+    }
 }
