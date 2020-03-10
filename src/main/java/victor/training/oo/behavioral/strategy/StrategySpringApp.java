3f954ebb1d1afc7d559d93f21ba7378d950e89bb
Victor Rentea <victorrentea@gmail.com>
2020-03-10 15:29:12 +0200

    Strategy pattern

--- src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
@@ -1,47 +1,78 @@
 package victor.training.oo.behavioral.strategy;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 public class StrategySpringApp implements CommandLineRunner {
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(StrategySpringApp.class)
-			.profiles("localProps")
-			.run(args);
-	}
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(StrategySpringApp.class)
+                .profiles("localProps")
+                .run(args);
+    }
 
-	
-	private ConfigProvider configProvider = new ConfigFileProvider(); 
-	
-	// TODO [1] Break CustomsService logic into Strategies
-	// TODO [2] Convert it to Chain Of Responsibility
-	// TODO [3] Wire with Spring
-	// TODO [4] ConfigProvider: selected based on environment props, with Spring
-	public void run(String... args) {
-		CustomsService service = new CustomsService();
-		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
-		System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
-		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
-		
-		System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
-	}
+
+    private ConfigProvider configProvider = new ConfigFileProvider();
+
+    // TODO [1] Break CustomsService logic into Strategies
+    // TODO [2] Convert it to Chain Of Responsibility
+    // TODO [3] Wire with Spring
+    // TODO [4] ConfigProvider: selected based on environment props, with Spring
+    public void run(String... args) {
+        CustomsService service = new CustomsService();
+        System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
+        System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
+        System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
+
+        System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
+    }
 }
 
 class CustomsService {
-	public double computeCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
-		switch (originCountry) { 
-		case "UK": return tobaccoValue/2 + regularValue;
-		case "CN": return tobaccoValue + regularValue;
-		case "FR": 
-		case "ES": // other EU country codes...
-		case "RO": return tobaccoValue/3;
-		default: throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
-		} 
-	}
+    public double computeCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
+        TaxCalculator calculator = selectCalculator(originCountry);
+        return calculator.calculate(regularValue, tobaccoValue);
+    }
+
+    private TaxCalculator selectCalculator(String originCountry) {
+        switch (originCountry) {
+            case "UK":
+                return new UKTaxCalculator();
+            case "CN":
+                return new ChinaTaxCalculator();
+            case "FR":
+            case "ES": // other EU country codes...
+            case "RO":
+                return new EUTaxCalculator();
+			default:
+				throw new IllegalStateException("Unexpected value: " + originCountry);
+		}
+    }
+
+}
+
+interface TaxCalculator {
+    double calculate(double tobaccoValue, double regularValue);
+}
+
+class UKTaxCalculator implements TaxCalculator {
+    public double calculate(double tobaccoValue, double regularValue) {
+        // mult cod 80
+        return tobaccoValue / 2 + regularValue;
+    }
+}
+
+class ChinaTaxCalculator implements TaxCalculator {
+    public double calculate(double tobaccoValue, double regularValue) {
+        // mult cod 50
+        return tobaccoValue + regularValue;
+    }
+}
+
+class EUTaxCalculator implements TaxCalculator {
+    public double calculate(double tobaccoValue, double regularValueDegeaba) {
+        // mult cod 50
+        return tobaccoValue / 3;
+    }
 }
