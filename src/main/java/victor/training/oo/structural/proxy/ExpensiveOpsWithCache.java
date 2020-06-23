4ac1137c4e389b4512b080ffb3869e8c32a54bf5
Victor Rentea <victorrentea@gmail.com>
2020-06-23 17:45:58 +0300

    Decorator cu Spring @Primary + @Profile poate fi enablat din jvm arguments

--- src/main/java/victor/training/oo/structural/proxy/ExpensiveOpsWithCache.java
+++ src/main/java/victor/training/oo/structural/proxy/ExpensiveOpsWithCache.java
@@ -1,6 +1,7 @@
 package victor.training.oo.structural.proxy;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -9,6 +10,7 @@ import java.util.Map;
 
 @Service
 @Primary
+@Profile("with-cache")
 public class ExpensiveOpsWithCache implements IExpensiveOps {
    private final IExpensiveOps ops;
    private Map<Integer, Boolean> cache = new HashMap<>();
