31ab7dfec083ed7f852977f63b781e197548fede
Victor Rentea <victorrentea@gmail.com>
2020-06-24 12:00:39 +0300

    Facade

--- src/main/java/victor/training/oo/structural/facade/facade/dto/CustomerDto.java
+++ src/main/java/victor/training/oo/structural/facade/facade/dto/CustomerDto.java
@@ -1,5 +1,7 @@
 package victor.training.oo.structural.facade.facade.dto;
 
+import victor.training.oo.structural.facade.entity.Customer;
+
 public class CustomerDto {
     public Long id;
 	public String name;
@@ -12,6 +14,14 @@ public class CustomerDto {
     public CustomerDto(String name, String email) {
         this.name = name;
         this.email = email;
+//       File file;
+//       OutputStream os = new FileOutputStream(file);
+//       OutputStream bufferedOutputStream = new BufferedOutputStream(os);
     }
+
+   public CustomerDto(Customer entity) {
+       name = entity.getName();
+       email = entity.getEmail();
+   }
 
 }
