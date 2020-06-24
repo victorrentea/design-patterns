299e282e0a46112351275d098174339fd0162f70
Victor Rentea <victorrentea@gmail.com>
2020-06-24 17:22:15 +0300

    Composing a Strategy

--- src/main/java/victor/training/oo/behavioral/template/TemplateSpringApp.java
+++ src/main/java/victor/training/oo/behavioral/template/TemplateSpringApp.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
 import java.util.Random;
 
 @SpringBootApplication
@@ -20,17 +21,23 @@ public class TemplateSpringApp implements CommandLineRunner {
 
    private void placeOrder() {
       // other logic
-      new OrderReceivedEmailSender().sendEmail("a@b.com");
+      new EmailSender(new OrderReceivedEmailSender()).sendEmail("a@b.com");
    }
 
    private void shipOrder() {
       // other logic
-      new OrderShippedEmailSender().sendEmail("a@b.com");
+      new EmailSender(new OrderShippedEmailSender()).sendEmail("a@b.com");
       // TODO send order shipped email 'similar to how send order received was implemented'
    }
 }
 
-abstract class AbstractEmailSender {
+class EmailSender {
+   private final EmailComposer composer;
+
+   EmailSender(EmailComposer composer) {
+      this.composer = composer;
+   }
+
    public void sendEmail(String emailAddress) {
       EmailContext context = new EmailContext(/*smtpConfig,etc*/);
       int MAX_RETRIES = 3;
@@ -39,25 +46,26 @@ abstract class AbstractEmailSender {
          email.setSender("noreply@corp.com");
          email.setReplyTo("/dev/null");
          email.setTo(emailAddress);
-         composeEmail(email);
+         composer.composeEmail(email);
          boolean success = context.send(email);
          if (success) break;
       }
    }
-
-   public abstract void composeEmail(Email email);
+}
+@FunctionalInterface //-- imi place
+// pero no me gusta default int x () { reutrn 1;}
+interface EmailComposer {
+   void composeEmail(Email email);
 }
 
-class OrderReceivedEmailSender extends AbstractEmailSender {
-   @Override
+class OrderReceivedEmailSender implements EmailComposer {
    public void composeEmail(Email email) {
       email.setSubject("Order Received");
       email.setBody("Thank you for your order");
    }
 }
 
-class OrderShippedEmailSender extends AbstractEmailSender {
-   @Override
+class OrderShippedEmailSender implements EmailComposer {
    public void composeEmail(Email email) {
       email.setSubject("Order Shipped");
       email.setBody("Ti-am trimis. Speram sa ajunga * de data asta");
@@ -80,3 +88,42 @@ class Email {
    private String replyTo;
    private String to;
 }
+
+
+//interface I1 {
+//   default int x() {
+//      return 1;
+//   }
+//}
+//interface I2 {
+//   default int x() {
+//      return 2;
+//   }
+//}
+//
+//class A implements I1,I2 {
+////   @Override
+////   public int x() {
+////      return 0;
+////   }
+//}
+
+interface ICsvWriter {
+   void writeCell(String cell);
+   void writeLine();
+
+   default void writeCells(List<String> cells) {
+      cells.forEach(this::writeCell); // imbogatesti API-ul cu functii derivate din fct abstracte din interfata
+   }
+
+   default void writeCellsUpper(List<String> cells) { // apelezi tu niste functii Util/static
+      cells.stream().map(String::toUpperCase).forEach(this::writeCell);
+   }
+
+   // din java 9+
+//    private void checkValidCellValue(String cell) {
+////      if //
+//   }
+}
+
+
