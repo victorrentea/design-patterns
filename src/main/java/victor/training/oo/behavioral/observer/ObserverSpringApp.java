829d2d8b5d50dd7b1c37dcbf0c3b2195e811f346
Victor Rentea <victorrentea@gmail.com>
2020-06-24 12:39:02 +0300

    procesare asincrona - NU O FACE

--- src/main/java/victor/training/oo/behavioral/observer/ObserverSpringApp.java
+++ src/main/java/victor/training/oo/behavioral/observer/ObserverSpringApp.java
@@ -6,61 +6,68 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.context.event.SimpleApplicationEventMulticaster;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @SpringBootApplication
 public class ObserverSpringApp {
-	public static void main(String[] args) {
-		SpringApplication.run(ObserverSpringApp.class, args);
-	}
-	
-//	@Bean
-//    public ApplicationEventMulticaster applicationEventMulticaster() {
-//        SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
-//        eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
-//        return eventMulticaster;
-//    }
+   public static void main(String[] args) {
+      SpringApplication.run(ObserverSpringApp.class, args);
+   }
 
-	@Autowired
-	private ApplicationEventPublisher publisher;
-	
-	@Autowired
-	private ObserverTransaction afterTransaction;
+   // NU FACETI ASTA!
+   @Bean
+   public ApplicationEventMulticaster applicationEventMulticaster() {
+      SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
+      eventMulticaster.setTaskExecutor(new SimpleAsyncTaskExecutor());
+      return eventMulticaster;
+   }
 
-	@Transactional
-	@EventListener
-	public void atStartup(ContextRefreshedEvent event) {
-		placeOrder(13L);
-		// afterTransaction.runInTransaction();
-	}
+   @Autowired
+   private ApplicationEventPublisher publisher;
 
-	// TODO [1] also generate invoice
-	// TODO [2] control the order
-	// TODO [3] chain events
-	// TODO [opt] Transaction-scoped events
+   @Autowired
+   private ObserverTransaction afterTransaction;
 
-	private void placeOrder(Long orderId) {
-		log.debug("Halo!");
-		publisher.publishEvent(new OrderPlacedEvent(orderId));
-		log.debug("After");
+   @Transactional
+   @EventListener
+   public void atStartup(ContextRefreshedEvent event) {
+      placeOrder(13L);
+      // afterTransaction.runInTransaction();
+   }
+
+   // TODO [1] also generate invoice
+   // TODO [2] control the order
+   // TODO [3] chain events
+   // TODO [opt] Transaction-scoped events
+
+   private void placeOrder(Long orderId) {
+      log.debug("Halo!");
+      publisher.publishEvent(new OrderPlacedEvent(orderId));
+      log.debug("After");
 //		invoiceService.generateInvoice(orderId);
-	}
+   }
 //	@Autowired
 //	private StockManagementService stockManagementService;
 //	@Autowired
 //	private InvoiceService invoiceService;
 }
+
 @Value
 class OrderPlacedEvent {
-	long orderId;
+   long orderId;
 }
+
 @Value
 class OrderInStockEvent {
-	long orderId;
+   long orderId;
 }
 
 // "record" java 14
@@ -72,23 +79,23 @@ class StockManagementService {
 //	@Autowired
 //	private ApplicationEventPublisher publisher;
 
-	@EventListener
-	public OrderInStockEvent checkStock(OrderPlacedEvent event) { // NOSONAR
-		log.debug("Checking stock for products in order " + event.getOrderId());
-		log.debug("If something goes wrong - throw an exception");
+   @EventListener
+   public OrderInStockEvent checkStock(OrderPlacedEvent event) { // NOSONAR
+      log.debug("Checking stock for products in order " + event.getOrderId());
+      log.debug("If something goes wrong - throw an exception");
 //		publisher.publishEvent(new OrderInStockEvent(event.getOrderId()));
-		return new OrderInStockEvent(event.getOrderId()); // idem behavior
-	}
+      return new OrderInStockEvent(event.getOrderId()); // idem behavior
+   }
 }
 
 /// ------- izolare sociala
 @Service
 @Slf4j
 class InvoiceService {
-	@EventListener
-	public void generateInvoice(OrderInStockEvent event) {
-		log.debug("Generating invoice for order id: " + event.getOrderId());
-		// TODO what if...
-		// throw new RuntimeException("thrown from generate invoice");
-	} 
+   @EventListener
+   public void generateInvoice(OrderInStockEvent event) {
+      log.debug("Generating invoice for order id: " + event.getOrderId());
+      // TODO what if...
+      // throw new RuntimeException("thrown from generate invoice");
+   }
 }
