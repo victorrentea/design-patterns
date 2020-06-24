0a067394e5ff8559fbb39bae0f3373844b5e07eb
Victor Rentea <victorrentea@gmail.com>
2020-06-24 16:09:53 +0300

    Hygienic Switch

--- src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
@@ -6,65 +6,72 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class StrategySpringApp implements CommandLineRunner {
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(StrategySpringApp.class)
-			.profiles("localProps")
-			.run(args);
-	}
+   public static void main(String[] args) {
+      new SpringApplicationBuilder(StrategySpringApp.class)
+          .profiles("localProps")
+          .run(args);
+   }
 
-	
-	private ConfigProvider configProvider = new ConfigFileProvider(); 
-	
-	// TODO [1] Break CustomsService logic into Strategies
-	// TODO [2] Convert it to Chain Of Responsibility
-	// TODO [3] Wire with Spring
-	// TODO [4] ConfigProvider: selected based on environment props, with Spring
-	public void run(String... args) {
-		CustomsService service = new CustomsService();
-		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
-		System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
-		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
-		
-		System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
-	}
+
+   private ConfigProvider configProvider = new ConfigFileProvider();
+
+   // TODO [1] Break CustomsService logic into Strategies
+   // TODO [2] Convert it to Chain Of Responsibility
+   // TODO [3] Wire with Spring
+   // TODO [4] ConfigProvider: selected based on environment props, with Spring
+   public void run(String... args) {
+      CustomsService service = new CustomsService();
+      System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
+      System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
+      System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
+
+      System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
+   }
 }
 
 class CustomsService {
-	public double computeCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
-		TaxCalculator calculator;
-		switch (originCountry) {
-		case "UK": calculator = new UKTaxCalculator(); break;
-		case "CN": calculator = new CNTaxCalculator(); break;
-		case "FR":
-		case "ES": // other EU country codes...
-		case "RO": calculator = new EUTaxCalculator(); break;
-		default: throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
-		}
-		return calculator.calculate(tobaccoValue, regularValue);
-	}
+   public double computeCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
+      TaxCalculator calculator = selectTaxCalculator(originCountry);
+      return calculator.calculate(tobaccoValue, regularValue);
+   }
+
+   private TaxCalculator selectTaxCalculator(String originCountry) {
+      switch (originCountry) {
+         case "UK": return new UKTaxCalculator();
+         case "CN": return new CNTaxCalculator();
+         case "FR":
+         case "ES": // other EU country codes...
+         case "RO": return new EUTaxCalculator();
+         default: throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
+      }
+   }
 }
+
 interface TaxCalculator {
-	double calculate(double tobaccoValue, double regularValue);
+   double calculate(double tobaccoValue, double regularValue);
 }
+
 class CNTaxCalculator implements TaxCalculator {
-	public double calculate(double tobaccoValue, double regularValue) {
-		// Maricica inca 15 linii
-		// Maricica inca 15 linii
-		// Maricica inca 15 linii
-		// Marcel a lasat si el aici 20 linii de cod
-		return tobaccoValue + regularValue;
-	}
+   public double calculate(double tobaccoValue, double regularValue) {
+      // Maricica inca 15 linii
+      // Maricica inca 15 linii
+      // Maricica inca 15 linii
+      // Marcel a lasat si el aici 20 linii de cod
+      return tobaccoValue + regularValue;
+   }
 }
+
 class EUTaxCalculator implements TaxCalculator {
-	public double calculate(double tobaccoValue, double regularValue) { // loss of specificity. luam un param de-a-n boulea
-		return tobaccoValue/3;
-	}
+   public double calculate(double tobaccoValue, double regularValue) { // loss of specificity. luam un param de-a-n boulea
+      return tobaccoValue / 3;
+   }
 }
+
 class UKTaxCalculator implements TaxCalculator {
-	public double calculate(double tobaccoValue, double regularValue) {
-		// Marcel a lasat si el aici 20 linii de cod
-		// Marcel a lasat si el aici 20 linii de cod
-		// Marcel a lasat si el aici 20 linii de cod
-		return tobaccoValue/2 + regularValue;
-	}
+   public double calculate(double tobaccoValue, double regularValue) {
+      // Marcel a lasat si el aici 20 linii de cod
+      // Marcel a lasat si el aici 20 linii de cod
+      // Marcel a lasat si el aici 20 linii de cod
+      return tobaccoValue / 2 + regularValue;
+   }
 }
