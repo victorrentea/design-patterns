cfb0ae3eea11773473e3cb734624a7628e6eb597
Victor Rentea <victorrentea@gmail.com>
2020-07-20 15:20:51 +0300

    si un varf de Spring

--- src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/oo/behavioral/strategy/StrategySpringApp.java
@@ -1,8 +1,10 @@
 package victor.training.oo.behavioral.strategy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,8 +24,9 @@ public class StrategySpringApp implements CommandLineRunner {
 	// TODO [2] Convert it to Chain Of Responsibility
 	// TODO [3] Wire with Spring
 	// TODO [4] ConfigProvider: selected based on environment props, with Spring
+	@Autowired
+	CustomsService service;
 	public void run(String... args) {
-		CustomsService service = new CustomsService();
 		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
 		System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
 		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
@@ -31,20 +34,17 @@ public class StrategySpringApp implements CommandLineRunner {
 		System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
 	}
 }
-
+@Service
 class CustomsService {
+	@Autowired
+	private List<CustomsComputer> all;
+
 	public double computeCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
 		CustomsComputer customsComputer = selectCustomsComputer(originCountry);
 		return customsComputer.compute(tobaccoValue, regularValue);
 	}
 
 	private CustomsComputer selectCustomsComputer(String originCountry) {
-
-		List<CustomsComputer> all = Arrays.asList(
-			new UKCustomsComputer(),
-			new ChinaCustomsComputer(),
-			new EUCustomsComputer());
-
 		for (CustomsComputer computer : all) {
 			if (computer.canHandleCountry(originCountry)) {
 				return computer;
@@ -59,6 +59,7 @@ interface CustomsComputer {
 	boolean canHandleCountry(String originCountry); // 2
 }
 
+@Service
 class ChinaCustomsComputer implements CustomsComputer {
 	public double compute(double tobaccoValue, double regularValue) {
 		// 50 linii cod
@@ -70,6 +71,7 @@ class ChinaCustomsComputer implements CustomsComputer {
 	}
 
 }
+@Service
 class UKCustomsComputer implements CustomsComputer {
 	private String originCountryCode;
 
@@ -84,6 +86,7 @@ class UKCustomsComputer implements CustomsComputer {
 		return "UK".equals(originCountry);
 	}
 }
+@Service
 class EUCustomsComputer implements CustomsComputer { // Tirania majoritatii - UN MINUS
 	@Override
 	public double compute(double tobaccoValue, double regularValue) { // --- iau regular value degeaba param. Sa-i traiasca familia lu mentenatoru'
