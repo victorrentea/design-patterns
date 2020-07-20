1b48006123fa57f5b086f16f8a0a9b40f61c7c21
Victor Rentea <victorrentea@gmail.com>
2020-07-20 10:29:06 +0300

    cache implementat ca metoda in aceeasi clas.a

--- src/main/java/victor/training/oo/structural/proxy/ExpensiveOps.java
+++ src/main/java/victor/training/oo/structural/proxy/ExpensiveOps.java
@@ -11,13 +11,27 @@ import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class ExpensiveOps {
-	
+
 	private static final BigDecimal TWO = new BigDecimal("2");
-	
-	public Boolean isPrime(int n) { 
+
+	private final Map<Integer, Boolean> cache = new HashMap<>();
+
+	public Boolean isPrime(int n) {
+		if (cache.containsKey(n)) {
+			return cache.get(n);
+		}
+		Boolean response = isPrime_(n);
+		cache.put(n, response);
+		return response;
+	}
+
+	private Boolean isPrime_(int n) {
+
 		log.debug("Computing isPrime({})", n);
 		BigDecimal number = new BigDecimal(n);
 		if (number.compareTo(TWO) <= 0) {
@@ -26,9 +40,9 @@ public class ExpensiveOps {
 		if (number.remainder(TWO).equals(BigDecimal.ZERO)) {
 			return false;
 		}
-		for (BigDecimal divisor = new BigDecimal("3"); 
-			divisor.compareTo(number.divide(TWO)) < 0;
-			divisor = divisor.add(TWO)) {
+		for (BigDecimal divisor = new BigDecimal("3");
+			  divisor.compareTo(number.divide(TWO)) < 0;
+			  divisor = divisor.add(TWO)) {
 			if (number.remainder(divisor).equals(BigDecimal.ZERO)) {
 				return false;
 			}
@@ -48,7 +62,7 @@ public class ExpensiveOps {
 				.forEach(s -> md.update(s.getBytes()));
 		}
 		byte[] digest = md.digest();
-	    return DatatypeConverter.printHexBinary(digest).toUpperCase();
+		return DatatypeConverter.printHexBinary(digest).toUpperCase();
 	}
-	
+
 }
