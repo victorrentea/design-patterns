2cb94964b75b660c3e28c0815a3302b932df7ccc
Victor Rentea <victorrentea@gmail.com>
2020-08-08 03:32:47 +0300

    Add ArchUnit

---
+++ src/main/java/victor/training/oo/behavioral/observer/ArchUnitTest.java
@@ -0,0 +1,21 @@
+package victor.training.oo.behavioral.observer;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.Test;
+
+public class ArchUnitTest {
+   @Test
+   public void check() {
+      JavaClasses importedClasses = new ClassFileImporter().importPackages("victor");
+
+      ArchRule rule = ArchRuleDefinition.classes().that().resideInAPackage("..repo..")
+          .should().onlyBeAccessed().byAnyPackage("..facade..");
+
+      rule.evaluate(importedClasses).handleViolations((collection, s) -> System.out.printf(collection + ""));
+//      rule.check(importedClasses);
+
+   }
+}
