d0d25c819a88d718069322b342fc0b5abbd35a3f
Victor Rentea <victorrentea@gmail.com>
2020-09-22 16:22:48 +0300

    CLass Proxies CGLIB in action

--- src/main/java/victor/training/oo/structural/proxy/Magie.java
+++ src/main/java/victor/training/oo/structural/proxy/Magie.java
@@ -1,26 +1,33 @@
 package victor.training.oo.structural.proxy;
 
-import java.lang.reflect.InvocationHandler;
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.Arrays;
 
 public class Magie {
    public static void main(String[] args) {
 
       Mate realImplem = new Mate();
-      InvocationHandler h = new InvocationHandler() {
+
+//      IMate mate = (IMate) Proxy.newProxyInstance(Magie.class.getClassLoader(),
+//          new Class<?>[]{IMate.class}, h);
+
+      Callback callback = new org.springframework.cglib.proxy.InvocationHandler() {
          @Override
-         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+         public Object invoke(Object o, Method method, Object[] args) throws Throwable {
             System.out.println("Calling " + method.getName() + " cu param " + Arrays.toString(args));
             return method.invoke(realImplem, args);
          }
       };
-
-      IMate mate = (IMate) Proxy.newProxyInstance(Magie.class.getClassLoader(),
-          new Class<?>[]{IMate.class}, h);
+      Mate mate = (Mate) Enhancer.create(Mate.class, callback);
       System.out.println(mate.getClass());
 
+      metodaCareDepindeMate(mate);
+   }
+
+   private static void metodaCareDepindeMate(Mate mate) {
       System.out.println(mate.suma(1, 1));
       System.out.println(mate.suma(2, 0));
       System.out.println(mate.suma(3, -1));
@@ -29,18 +36,12 @@ public class Magie {
    }
 }
 
-interface IMate {
-   int suma(int a, int b);
-   int produs(int a, int b);
-}
-class Mate implements IMate {
+/*final */class Mate {
 
-   @Override
-   public int suma(int a, int b) {
+   public /*final*/ int suma(int a, int b) {
       return a+b;
    }
 
-   @Override
    public int produs(int a, int b) {
       return a*b;
    }
