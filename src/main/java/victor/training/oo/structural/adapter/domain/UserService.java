1e2e3902026a60f4173c2dbe9c11d0163c053595
Victor Rentea <victorrentea@gmail.com>
2020-09-22 14:42:14 +0300

    Am izolat partea de interactiune cu exterior intr-o clasa separata - Adapter

--- src/main/java/victor/training/oo/structural/adapter/domain/UserService.java
+++ src/main/java/victor/training/oo/structural/adapter/domain/UserService.java
@@ -3,52 +3,35 @@ package victor.training.oo.structural.adapter.domain;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import victor.training.oo.structural.adapter.infra.LdapUser;
-import victor.training.oo.structural.adapter.infra.LdapUserWebserviceClient;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 // Suntem in cel mai sacru loc din app noastra: in Domain Service, Unde ar trebui sa fie cea mai pura si frumoasa logica posibila
 public class UserService {
-	private final LdapUserWebserviceClient wsClient;
+   private final LdapUserServiceAdapter ldapUserServiceAdapter;
+
+   // viata-i grea, n-o poate toti
+   public void importUserFromLdap(String username) {
+      List<User> list = ldapUserServiceAdapter.searchByUsername(username);
+      if (list.size() != 1) {
+         throw new IllegalArgumentException("There is no single user matching username " + username);
+      }
+      User user = list.get(0);
+
+      if (user.getWorkEmail() != null) {
+         log.debug("Send welcome email to " + user.getWorkEmail());
+      }
+      log.debug("Insert user in my database");
+   }
+
+   public List<User> searchUserInLdap(String username) {
+      return ldapUserServiceAdapter.searchByUsername(username);
+   }
+   // paradisul
 
 
-	// viata-i grea, n-o poate toti
-	public void importUserFromLdap(String username) {
-		List<User> list = searchByUsername(username);
-		if (list.size() != 1) {
-			throw new IllegalArgumentException("There is no single user matching username " + username);
-		}
-		User user = list.get(0);
-
-		if (user.getWorkEmail() != null) {
-			log.debug("Send welcome email to " + user.getWorkEmail());
-		}
-		log.debug("Insert user in my database");
-	}
-	
-	public List<User> searchUserInLdap(String username) {
-		return searchByUsername(username);
-	}
-	// paradisul
-
-	// ------------- linie ------------------------------------------------
-
-	//infern: tu cel ce intri, abandoneaza orice speranta
-
-	private User convert(LdapUser ldapUser) {
-		String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
-		return new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
-	}
-
-	private List<User> searchByUsername(String username) {
-		return wsClient.search(username.toUpperCase(), null, null).stream()
-			.map(this::convert)
-			.collect(Collectors.toList());
-	}
 
 }
