ff38eee0b0259a518003e0b4a743654257409b6b
Victor Rentea <victorrentea@gmail.com>
2020-09-23 14:28:12 +0300

    Thread pool managed by spring

--- src/main/java/victor/training/oo/behavioral/command/CommandSpringApp.java
+++ src/main/java/victor/training/oo/behavioral/command/CommandSpringApp.java
@@ -3,6 +3,7 @@ package victor.training.oo.behavioral.command;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,8 +17,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 import static java.util.Arrays.asList;
@@ -32,10 +31,10 @@ public class CommandSpringApp {
 	}
 
 	@Bean
-	public ThreadPoolTaskExecutor executor() {
+	public ThreadPoolTaskExecutor executor(@Value("${barman.count:2}")int threadCount) {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-		executor.setCorePoolSize(1);
-		executor.setMaxPoolSize(1);
+		executor.setCorePoolSize(threadCount);
+		executor.setMaxPoolSize(threadCount);
 		executor.setQueueCapacity(500);
 		executor.setThreadNamePrefix("barman-");
 		executor.initialize();
@@ -53,12 +52,14 @@ class Drinker implements CommandLineRunner {
 	@Autowired
 	private ServiceActivatorPattern serviceActivatorPattern;
 
+	@Autowired
+	private ThreadPoolTaskExecutor pool;
+
 	// TODO [1] inject and use a ThreadPoolTaskExecutor.submit
 	// TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
     // TODO [3] wanna try it out over JMS? try out ServiceActivatorPattern
 	public void run(String... args) throws ExecutionException, InterruptedException {
 		log.debug("Submitting my order");
-		ExecutorService pool = Executors.newFixedThreadPool(2);
 
 		Future<Beer> futureBeer = pool.submit(() -> barman.pourBeer());
 		Future<Vodka> futureVodka = pool.submit(() -> barman.pourVodka());
