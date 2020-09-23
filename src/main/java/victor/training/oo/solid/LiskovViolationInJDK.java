956a693646718a1895598775ff345c673980b97e
Victor Rentea <victorrentea@gmail.com>
2020-09-23 11:28:34 +0300

    Geeky tricks

--- src/main/java/victor/training/oo/solid/LiskovViolationInJDK.java
+++ src/main/java/victor/training/oo/solid/LiskovViolationInJDK.java
@@ -1,5 +1,7 @@
 package victor.training.oo.solid;
 
+import victor.training.oo.structural.facade.entity.Customer;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -7,17 +9,21 @@ import java.util.List;
 public class LiskovViolationInJDK {
    public static void main(String[] args) {
 
-      List<String> list = new ArrayList<>();
-      list.add("a");
-      list.add("b");
+      List<Customer> list = new ArrayList<>();
+      list.add(new Customer());
 
-      List<String> readOnly = Collections.unmodifiableList(list);
+      List<Customer> readOnly = Collections.unmodifiableList(list);
 
       met(readOnly);
       System.out.println(list);
    }
 
-   private static void met(List<String> list) {
-      list.add("c");
+//   private static void met(List<? extends Customer> list) {
+//      list.add(new Customer());
+   private static void met(Iterable<Customer> list) {
+//      list.add(new Customer());
+      for (Customer s : list) {
+         System.out.println(s);
+      }
    }
 }
