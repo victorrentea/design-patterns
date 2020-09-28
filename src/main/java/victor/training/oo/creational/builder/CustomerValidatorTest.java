dd17bad908a50899d4802bba8cbbd43b18af1e7c
Victor Rentea <victorrentea@gmail.com>
2020-09-28 12:21:42 +0300

    factory methods in teste care intorc Builderi 'resonably valid' pe care apoi in modific pt fiecare test cum imi trebuie

--- src/main/java/victor/training/oo/creational/builder/CustomerValidatorTest.java
+++ src/main/java/victor/training/oo/creational/builder/CustomerValidatorTest.java
@@ -5,14 +5,36 @@ import org.junit.Test;
 public class CustomerValidatorTest {
 
 	private CustomerValidator validator = new CustomerValidator();
-	
+
+	private CustomerBuilder aValidCustomer() {
+		return new CustomerBuilder()
+			.withName("John Doe")
+			.withAddress(aValidAddress());
+	}
+
+	private AddressBuilder aValidAddress() {
+		return new AddressBuilder()
+			.withCity("Chisinau")
+			.withStreetName("St mare")
+			.withCity("Chisinau");
+	}
+
 	@Test
 	public void validCustomer_ok() {
-		Customer customer = new Customer();
-		customer.setName("John Doe");
-		Address address = new Address();
-		address.setCity("Bucharest");
-		customer.setAddress(address);
+		Customer customer = aValidCustomer().build();
+		validator.validate(customer);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void throwsForNullCustomerName() {
+		Customer customer = aValidCustomer().withName(null).build();
+		validator.validate(customer);
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void throwsForNullAddressCity() {
+		Customer customer = aValidCustomer()
+			.withAddress(aValidAddress().withCity(null))
+			.build();
 		validator.validate(customer);
 	}
 
