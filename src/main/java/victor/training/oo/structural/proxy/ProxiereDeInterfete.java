d12c6c1fc971ae8a2c32f30d0deae4a13e33ff60
Victor Rentea <victorrentea@gmail.com>
2020-09-28 16:41:55 +0300

    Proxies of concrete classes

--- src/main/java/victor/training/oo/structural/proxy/ProxiereDeInterfete.java
+++ src/main/java/victor/training/oo/structural/proxy/ProxiereDeInterfete.java
@@ -1,30 +1,50 @@
 package victor.training.oo.structural.proxy;
 
-import java.lang.reflect.InvocationHandler;
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
 import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
 import java.util.Arrays;
 
 public class ProxiereDeInterfete {
 
    public static void main(String[] args) {
 
-      MateImpl mateImpl = new MateImpl();
-      InvocationHandler callHandler = new InvocationHandler() {
+      Mate mateImpl = new Mate();
+//      InvocationHandler callHandler = new InvocationHandler() {
+//         @Override
+//         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//            System.out.println("Trec prin proxy catre metoda " + method.getName() + " cu param " + Arrays.toString(args));
+//            return method.invoke(mateImpl, args);
+//         }
+//      };
+
+//      Mate mate = (Mate) Proxy.newProxyInstance(ProxiereDeInterfete.class.getClassLoader(),
+//          new Class<?>[]{Mate.class}, callHandler);
+      Callback methodCallback = new MethodInterceptor() {
          @Override
-         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+         public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
             System.out.println("Trec prin proxy catre metoda " + method.getName() + " cu param " + Arrays.toString(args));
             return method.invoke(mateImpl, args);
          }
       };
-
-      Mate mate = (Mate) Proxy.newProxyInstance(ProxiereDeInterfete.class.getClassLoader(),
-          new Class<?>[]{Mate.class}, callHandler);
+      Mate mate = (Mate) Enhancer.create(Mate.class, methodCallback);
+      // generates a dynamic subclass of your concrete class
+//      m(new Mate(){
+//         @Override
+//         public int suma(int a, int b) {
+//            return super.suma(a, b);
+//         }
+//      });
 
       m(mate);
+
    }
 
    private static void m(Mate mate) {
+      System.out.println("Oare cu ce clasa de mate vorbesc ? " + mate.getClass());
       System.out.println(mate.suma(1, 1));
       System.out.println(mate.suma(2, 0));
       System.out.println(mate.suma(3, -1));
@@ -35,19 +55,13 @@ public class ProxiereDeInterfete {
 
 }
 
-interface Mate {
-   int suma(int a, int b);
-   int produs(int a, int b);
-}
-
-class MateImpl implements Mate {
-   @Override
-   public int suma(int a, int b) {
+//cum o hackui: vezi comentarii mai jos
+/*final */class Mate {
+   public /*static*/  /*final*/ int suma(int a, int b) {
       System.out.println("In metoda reala!");
       return a+b;
    }
 
-   @Override
    public int produs(int a, int b) {
       return a*b;
    }
