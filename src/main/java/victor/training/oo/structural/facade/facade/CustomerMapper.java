ba42171f87752731f6f605e07762978b88e5510b
Victor Rentea <victorrentea@gmail.com>
2020-09-29 10:18:19 +0300

    UIConstants

--- src/main/java/victor/training/oo/structural/facade/facade/CustomerMapper.java
+++ src/main/java/victor/training/oo/structural/facade/facade/CustomerMapper.java
@@ -4,16 +4,14 @@ import org.springframework.stereotype.Component;
 import victor.training.oo.structural.facade.entity.Customer;
 import victor.training.oo.structural.facade.facade.dto.CustomerDto;
 
-import java.text.SimpleDateFormat;
-
 @Component
 public class CustomerMapper {
    public CustomerDto convertToDto(Customer customer) {
-      CustomerDto dto = new CustomerDto();
-      dto.name = customer.getName();
-      dto.email = customer.getEmail();
-      dto.creationDateStr = new SimpleDateFormat("yyyy-MM-dd").format(customer.getCreationDate());
-      dto.id = customer.getId();
+      CustomerDto dto = new CustomerDto(customer);
+//      dto.name = customer.getName();
+//      dto.email = customer.getEmail();
+//      dto.creationDateStr = new SimpleDateFormat("yyyy-MM-dd").format(customer.getCreationDate());
+//      dto.id = customer.getId();
       return dto;
    }
 
