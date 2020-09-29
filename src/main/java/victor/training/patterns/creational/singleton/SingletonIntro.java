0dcc93df7acdea61df3c48d7678498c743a8a480
Victor Rentea <victorrentea@gmail.com>
2020-09-29 08:59:52 +0300

    SingletonIntro

--- src/main/java/victor/training/patterns/creational/singleton/SingletonIntro.java
+++ src/main/java/victor/training/patterns/creational/singleton/SingletonIntro.java
@@ -1,15 +1,15 @@
 package victor.training.patterns.creational.singleton;
 
 import org.apache.commons.io.IOUtils;
-import org.jooq.lambda.Unchecked;
 
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SingletonIntro {
+   public static void main(String[] args) {
+      System.out.println(new BizService().bizMethod());
+   }
 }
 
 class BizService {
@@ -18,7 +18,8 @@ class BizService {
       // TODO reuse B instance (performance)
       // TODO push life mgmt to B (getInstance)
       // TODO Test it
-      String config = ServiceLocator.getInstance(ConfigManager.class).getConfig();
+      ConfigManager configManager = new ConfigManager();
+      String config = configManager.getConfig();
       if (config.equals("NOOP")) {
          return -1;
       }
@@ -26,35 +27,28 @@ class BizService {
    }
 }
 
-class ServiceLocator {
-   private static final Map<Class<?>, Object> singletons = new HashMap<>();
-
-   public static <T> T getInstance(Class<T> aClass) {
-      return (T) singletons.computeIfAbsent(aClass,
-          Unchecked.function(Class::newInstance));
-   }
-   public static <T> void setTestInstance(Class<T> aClass, T testDouble) {
-      singletons.put(aClass, testDouble);
-   }
-}
 
 class ConfigManager {
 
-   public static ConfigManager getInstance() {
-      return new ConfigManager();
-   }
-   private String config;
-
-   public ConfigManager() {
+   public String getConfig() {
+      String config;
       try (Reader reader = new FileReader("f.txt")) {
          config = IOUtils.toString(reader); // takes time
       } catch (IOException e) {
          throw new RuntimeException(e);
       }
-
-   }
-
-   public String getConfig() {
       return config;
    }
 }
+
+//class ServiceLocator {
+//   private static final Map<Class<?>, Object> singletons = new HashMap<>();
+//
+//   public static <T> T getInstance(Class<T> aClass) {
+//      return (T) singletons.computeIfAbsent(aClass,
+//          Unchecked.function(Class::newInstance));
+//   }
+//   public static <T> void setTestInstance(Class<T> aClass, T testDouble) {
+//      singletons.put(aClass, testDouble);
+//   }
+//}
--- src/test/java/victor/training/patterns/creational/singleton/BizServiceTest.java
+++ src/test/java/victor/training/patterns/creational/singleton/BizServiceTest.java
@@ -1,24 +1,16 @@
 package victor.training.patterns.creational.singleton;
 
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mockito;
-import org.powermock.modules.junit4.PowerMockRunner;
-
-
-@RunWith(PowerMockRunner.class)
+//@RunWith(PowerMockRunner.class)
 //@PrepareForTest(ConfigManager.class)
-public class BizServiceTest {
-   @Test
-   public void test() {
+//public class BizServiceTest {
+//   @Test
+//   public void test() {
 //      PowerMockito.mockStatic(ConfigManager.class);
-
-      ConfigManager mockManager = Mockito.mock(ConfigManager.class);
+//
+//      ConfigManager mockManager = Mockito.mock(ConfigManager.class);
 //      Mockito.when(ConfigManager.getInstance()).thenReturn(mockManager);
-      Mockito.when(mockManager.getConfig()).thenReturn("NOOP");
-      ServiceLocator.setTestInstance(ConfigManager.class, mockManager);
-
-      Assert.assertEquals(-1, new BizService().bizMethod());
-   }
-}
+//      Mockito.when(mockManager.getConfig()).thenReturn("NOOP");
+//
+//      Assert.assertEquals(-1, new BizService().bizMethod());
+//   }
+//}
