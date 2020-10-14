08cbc0f64dde8650011161d2fe20cf2c23d5b725
Victor Rentea <victorrentea@gmail.com>
2020-10-14 14:34:19 +0300

    Geek Stuff

---
+++ src/main/java/victor/training/patterns/creational/builder/geek/UserConfiguration.java
@@ -0,0 +1,22 @@
+package victor.training.patterns.creational.builder.geek;
+
+public class UserConfiguration {
+   private final String name;
+   private ServerDetails serverDetails;
+
+   public UserConfiguration(String name) {
+      this.name = name;
+   }
+
+   public void setServerDetails(ServerDetails serverDetails) {
+      this.serverDetails = serverDetails;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public ServerDetails getServerDetails() {
+      return serverDetails;
+   }
+}
