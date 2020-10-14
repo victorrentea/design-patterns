21c429ed97bac83096c8e9d40387685e0e307ced
Victor Rentea <victorrentea@gmail.com>
2020-10-14 15:53:09 +0300

    cleanup

--- src/main/java/victor/training/patterns/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/patterns/behavioral/strategy/StrategySpringApp.java
@@ -4,28 +4,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
-@SpringBootApplication
-public class StrategySpringApp implements CommandLineRunner {
+public class StrategySpringApp  {
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(StrategySpringApp.class)
-			.profiles("localProps")
-			.run(args);
-	}
-
-	
-	private ConfigProvider configProvider = new ConfigFileProvider(); 
-	
-	// TODO [1] Break CustomsService logic into Strategies
-	// TODO [2] Convert it to Chain Of Responsibility
-	// TODO [3] Wire with Spring
-	// TODO [4] ConfigProvider: selected based on environment props, with Spring
-	public void run(String... args) {
 		CustomsService service = new CustomsService();
 		System.out.println("Tax for (RO,100,100) = " + service.computeCustomsTax("RO", 100, 100));
 		System.out.println("Tax for (CN,100,100) = " + service.computeCustomsTax("CN", 100, 100));
 		System.out.println("Tax for (UK,100,100) = " + service.computeCustomsTax("UK", 100, 100));
 		
-		System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
 	}
 }
 
