27d29e270ef1c8825305f1f54e81db33ab4ecdc7
Victor Rentea <victorrentea@gmail.com>
2020-10-15 01:23:04 +0300

    remove javax.bind dep

--- src/test/java/victor/training/patterns/creational/builder/CustomerValidatorTest.java
+++ src/test/java/victor/training/patterns/creational/builder/CustomerValidatorTest.java
@@ -1,7 +1,9 @@
 package victor.training.patterns.creational.builder;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
+@Slf4j
 public class CustomerValidatorTest {
 
 	private CustomerValidator validator = new CustomerValidator();
@@ -15,6 +17,28 @@ public class CustomerValidatorTest {
 		address.setStreetAddress("Dristor 91");
 		customer.setAddress(address);
 		validator.validate(customer);
+	}
+
+	@Test
+	public void test1() throws InterruptedException {
+		run(1);
+	}
+	@Test
+	public void test2() throws InterruptedException {
+		run(2);
+	}
+	@Test
+	public void test3() throws InterruptedException {
+		run(3);
+	}
+	public void test4() throws InterruptedException {
+		run(4);
+	}
+
+	private void run(int s) throws InterruptedException {
+		log.debug("Start Test" + s);
+		Thread.sleep(3000);
+		log.debug("End Test" + s);
 	}
 
 }
