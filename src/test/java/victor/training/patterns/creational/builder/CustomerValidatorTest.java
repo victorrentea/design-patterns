707c466dacd381d9ae7c4741fa569d36890724e1
Victor Rentea <victorrentea@gmail.com>
2020-11-03 17:19:42 +0200

    prep

--- src/test/java/victor/training/patterns/creational/builder/CustomerValidatorTest.java
+++ src/test/java/victor/training/patterns/creational/builder/CustomerValidatorTest.java
@@ -1,13 +1,13 @@
 package victor.training.patterns.creational.builder;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class CustomerValidatorTest {
 
 	private CustomerValidator validator = new CustomerValidator();
-	
+
 	@Test
 	public void validCustomer_ok() {
 		Customer customer = new Customer();
@@ -17,28 +17,6 @@ public class CustomerValidatorTest {
 		address.setStreetAddress("Dristor 91");
 		customer.setAddress(address);
 		validator.validate(customer);
-	}
-
-	@Test
-	public void test1() throws InterruptedException {
-		run(1);
-	}
-	@Test
-	public void test2() throws InterruptedException {
-		run(2);
-	}
-	@Test
-	public void test3() throws InterruptedException {
-		run(3);
-	}
-	public void test4() throws InterruptedException {
-		run(4);
-	}
-
-	private void run(int s) throws InterruptedException {
-		log.debug("Start Test" + s);
-		Thread.sleep(3000);
-		log.debug("End Test" + s);
 	}
 
 }
