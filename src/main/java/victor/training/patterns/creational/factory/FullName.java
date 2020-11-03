176818aa17059eb3ca564896f02655904c2fa7c0
Victor Rentea <victorrentea@gmail.com>
2020-11-03 09:38:07 +0200

    add FullName immutable class

---
+++ src/main/java/victor/training/patterns/creational/factory/FullName.java
@@ -0,0 +1,37 @@
+package victor.training.patterns.creational.factory;
+
+import victor.training.patterns.creational.factory.FullName.Prefix;
+
+class ClientCode {
+   public void method() {
+      FullName fullName = new FullName(Prefix.MR, "Emma", "Rentea");
+   }
+}
+
+public class FullName {
+   enum Prefix {
+      MR, MS, MRS
+   }
+   private final Prefix prefix;
+   private final String firstName;
+   private final String lastName;
+
+   public FullName(Prefix prefix, String firstName, String lastName) {
+      this.prefix = prefix;
+      this.firstName = firstName;
+      this.lastName = lastName;
+   }
+
+
+   public String getLastName() {
+      return lastName;
+   }
+
+   public String getFirstName() {
+      return firstName;
+   }
+
+   public Prefix getPrefix() {
+      return prefix;
+   }
+}
