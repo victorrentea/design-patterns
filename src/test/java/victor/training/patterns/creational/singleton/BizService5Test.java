e03a4acdb44184890ba8391ce5fe23af6b4e39ce
Victor Rentea <victorrentea@gmail.com>
2020-11-03 23:10:01 +0200

    Add JUnit 5 + PowerMock

---
+++ src/test/java/victor/training/patterns/creational/singleton/BizService5Test.java
@@ -0,0 +1,23 @@
+package victor.training.patterns.creational.singleton;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BizService5Test {
+   @Test
+   public void test() {
+      try (MockedStatic<ConfigManager> mock = Mockito.mockStatic(ConfigManager.class)) {
+         ConfigManager mockManager = Mockito.mock(ConfigManager.class);
+         mock.when(ConfigManager::getInstance).thenReturn(mockManager);
+         Mockito.when(mockManager.getConfig()).thenReturn("NOOP");
+
+         Assertions.assertEquals(-1, new BizService().bizMethod());
+      }
+   }
+}
