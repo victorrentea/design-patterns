c0b8aef4f742805ea368ab3285a0cea305cdeb67
Victor Rentea <victorrentea@gmail.com>
2020-11-04 15:53:23 +0200

    Lombok generating getters and setters fluent )!

--- src/main/java/victor/training/patterns/creational/builder/Address.java
+++ src/main/java/victor/training/patterns/creational/builder/Address.java
@@ -1,34 +1,11 @@
 package victor.training.patterns.creational.builder;
 
+import lombok.Data;
+import lombok.Getter;
+
+@Data
 public class Address {
 	private String streetAddress;
 	private String city;
 	private String country;
-
-	public String getStreetAddress() {
-		return streetAddress;
-	}
-
-	public Address setStreetAddress(String streetAddress) {
-		this.streetAddress = streetAddress;
-		return this;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public Address setCity(String city) {
-		this.city = city;
-		return this;
-	}
-
-	public String getCountry() {
-		return country;
-	}
-
-	public Address setCountry(String country) {
-		this.country = country;
-		return this;
-	}
 }
--- src/main/java/victor/training/patterns/creational/builder/Customer.java
+++ src/main/java/victor/training/patterns/creational/builder/Customer.java
@@ -1,68 +1,22 @@
 package victor.training.patterns.creational.builder;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+
 public class Customer {
+	@Getter @Setter
 	private Long id;
+	@Getter @Setter
 	private String name;
+	@Getter @Setter
 	private String phone;
 	private List<String> labels = new ArrayList<>();
+	@Getter @Setter
 	private Address address;
 	private Date createDate;
-
-	public Long getId() {
-		return id;
-	}
-
-	public Customer setId(Long id) {
-		this.id = id;
-		return this;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public Customer setName(String name) {
-		this.name = name;
-		return this;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public Customer setPhone(String phone) {
-		this.phone = phone;
-		return this;
-	}
-
-	public List<String> getLabels() {
-		return labels;
-	}
-
-	public Customer setLabels(List<String> labels) {
-		this.labels = labels;
-		return this;
-	}
-
-	public Date getCreateDate() {
-		return createDate;
-	}
-
-	public Customer setCreateDate(Date createDate) {
-		this.createDate = createDate;
-		return this;
-	}
-
-	public Address getAddress() {
-		return address;
-	}
-
-	public Customer setAddress(Address address) {
-		this.address = address;
-		return this;
-	}
 }
