79ac489fe1649868c092869886754c54123edf0d
Victor Rentea <victorrentea@gmail.com>
2020-11-04 07:15:01 +0200

    cleanup unused stuff

--- src/main/java/victor/training/patterns/behavioral/visitor/model/Shape.java
+++
@@ -1,7 +0,0 @@
-package victor.training.patterns.behavioral.visitor.model;
-
-import victor.training.patterns.behavioral.visitor.ShapeVisitor;
-
-public interface Shape {
-	void accept(ShapeVisitor visitor);
-}
