4133b999f1f8644b07f9888bf1393b36f31edbd4
Victor Rentea <victorrentea@gmail.com>
2020-12-07 12:08:02 +0200

    Extragerea duplicarii de cod se face reflex (~ cainele lui Pavlov), dupa ce ai verificat ca nu e doar intamplare ca e cod identic, ci chiar INSEAMNA acelasi lucru --> DRY

--- src/main/java/victor/training/patterns/structural/adapter/domain/UserService.java
+++ src/main/java/victor/training/patterns/structural/adapter/domain/UserService.java
@@ -1,7 +1,7 @@
 package victor.training.patterns.structural.adapter.domain;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import victor.training.patterns.structural.adapter.infra.LdapUser;
 import victor.training.patterns.structural.adapter.infra.LdapUserWebserviceClient;
@@ -11,34 +11,48 @@ import java.util.List;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
+// codu tau de care esti tu mandru, codul de domain logic
+// ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN
+// ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN
+// ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN
+// ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN
+// ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN
 public class UserService {
-	@Autowired
-	private LdapUserWebserviceClient wsClient;
+	private final LdapUserWebserviceClient wsClient;
+
 
 	public void importUserFromLdap(String username) {
-		List<LdapUser> list = wsClient.search(username.toUpperCase(), null, null);
+		List<LdapUser> list = searchByUsername(username);
 		if (list.size() != 1) {
 			throw new IllegalArgumentException("There is no single user matching username " + username);
 		}
-		LdapUser ldapUser = list.get(0);
-		String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
-		User user = new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
-		
+		LdapUser ldapUser = list.get(0); // modelul lu' ala
+		User user = convertToEntity(ldapUser);
+
 		if (user.getWorkEmail() != null) {
 			log.debug("Send welcome email to " + user.getWorkEmail());
 		}
 		log.debug("Insert user in my database");
 	}
-	
+	// 200 de linii de jos
 	public List<User> searchUserInLdap(String username) {
-		List<LdapUser> list = wsClient.search(username.toUpperCase(), null, null);
+		List<LdapUser> list = searchByUsername(username);
 		List<User> results = new ArrayList<>();
 		for (LdapUser ldapUser : list) {
-			String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
-			User user = new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
+			User user = convertToEntity(ldapUser);
 			results.add(user);
 		}
 		return results;
 	}
-	
+
+	private User convertToEntity(LdapUser ldapUser) {
+		String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
+		return new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
+	}
+
+	private List<LdapUser> searchByUsername(String username) {
+		return wsClient.search(username.toUpperCase(), null, null);
+	}
+
 }
