739ff969d917fcbc9fb241f2b9af091a4fe20d7f
Victor Rentea <victorrentea@gmail.com>
2020-12-08 10:34:17 +0200

    Command Pattern: in loc sa chem o functie, instantiez un obiect si-l pasez de colo colo:::

--- src/main/java/victor/training/patterns/behavioral/command/Drinker.java
+++ src/main/java/victor/training/patterns/behavioral/command/Drinker.java
@@ -15,55 +15,38 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.*;
+
 import static java.util.Arrays.asList;
 import static victor.training.patterns.stuff.ThreadUtils.sleepq;
 
-@EnableAsync
-@SpringBootApplication
-@EnableBinding({Sink.class, Source.class})
-public class CommandSpringApp {
-   public static void main(String[] args) {
-      SpringApplication.run(CommandSpringApp.class, args).close(); // Note: .close to stop executors after CLRunner finishes
-   }
-
-   @Bean
-   public ThreadPoolTaskExecutor executor() {
-      ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-      executor.setCorePoolSize(1);
-      executor.setMaxPoolSize(1);
-      executor.setQueueCapacity(500);
-      executor.setThreadNamePrefix("barman-");
-      executor.initialize();
-      executor.setWaitForTasksToCompleteOnShutdown(true);
-      return executor;
-   }
-
-}
 
 @Slf4j
-@Component
-class Drinker implements CommandLineRunner {
-   @Autowired
-   private Barman barman;
-   @Autowired
-   private ServiceActivatorPattern serviceActivatorPattern;
+public class Drinker  {
 
-   // TODO [1] inject and use a ThreadPoolTaskExecutor.submit
-   // TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
-   // TODO [3] wanna try it out over JMS? try out ServiceActivatorPattern
-   public void run(String... args) {
+   private static Barman barman = new Barman();
+
+   public static void main(String[] args) throws ExecutionException, InterruptedException {
       log.debug("Submitting my order");
       long t0 = System.currentTimeMillis();
       log.debug("Waiting for my drinks...");
-      Beer beer = barman.pourBeer();
-      Vodka vodka = barman.pourVodka();
+
+      ExecutorService pool = Executors.newFixedThreadPool(2);
+
+      Future<Beer> futureBeer = pool.submit(() -> barman.pourBeer());
+      Future<Vodka> futureVodka = pool.submit(() -> barman.pourVodka());
+
+      log.debug("Aici a plecat chelnerul cu comanda");
+
+      Beer beer = futureBeer.get(); // cat timp se sta aici: 1s
+      Vodka vodka = futureVodka.get();// cat timp se sta aici? 0s pentru ca acea secunda pt vodka s-a scurs intre timp deja
+
       long t1 = System.currentTimeMillis();
       log.debug("Got my order in {} ms ! Enjoying {}", t1 - t0, asList(beer, vodka));
    }
 }
 
 @Slf4j
-@Service
 class Barman {
    public Beer pourBeer() {
       log.debug("Pouring Beer...");
