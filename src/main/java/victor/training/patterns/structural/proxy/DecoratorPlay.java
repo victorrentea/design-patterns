8611837ce0229782a7435acfd4af42209f34ac49
Victor Rentea <victorrentea@gmail.com>
2020-12-09 17:27:34 +0200

    AM STRICAT DECORATOR: fara interfata

--- src/main/java/victor/training/patterns/structural/proxy/DecoratorPlay.java
+++ src/main/java/victor/training/patterns/structural/proxy/DecoratorPlay.java
@@ -2,28 +2,22 @@ package victor.training.patterns.structural.proxy;
 
 public class DecoratorPlay {
    public static void main(String[] args) {
-      IMate mate = new Mate();
-      mate = new Decorator(mate);
-      mate = new DecoratorCareMasoaraTimpul(new Decorator(mate));
-      client(mate);
+      Mate mate = new Mate();
+      client(new DecoratorCareMasoaraTimpul(mate));
    }
 
    // vreau sa intervin in calea apelului de metoda, fara ca clientul sa se prinda
-   public static void client(IMate mate) {
+   public static void client(DecoratorCareMasoaraTimpul mate) {
       System.out.println(mate.sum(1, 1));
    }
 }
 
-interface IMate {
-   int sum(int a, int b);
-}
-class DecoratorCareMasoaraTimpul implements IMate {
-   private final IMate mate;
-   public DecoratorCareMasoaraTimpul(IMate mate) {
+class DecoratorCareMasoaraTimpul {
+   private final Mate mate;
+   public DecoratorCareMasoaraTimpul(Mate mate) {
       this.mate = mate;
    }
 
-   @Override
    public int sum(int a, int b) {
       long t0 = System.currentTimeMillis();
       int result = mate.sum(a, b);
@@ -32,18 +26,7 @@ class DecoratorCareMasoaraTimpul implements IMate {
       return result;
    }
 }
-class Decorator implements IMate {
-   private final IMate mate;
-   public Decorator(IMate mate) {
-      this.mate = mate;
-   }
-   @Override
-   public int sum(int a, int b) {
-      System.out.println("Calling sum: " + a + "  +  " + b);
-      return mate.sum(a, b);
-   }
-}
-class Mate implements IMate {
+class Mate  {
    public int sum(int a, int b) {
       return a + b;
    }
