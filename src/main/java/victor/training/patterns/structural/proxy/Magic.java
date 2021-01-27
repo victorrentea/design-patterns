f6542825e06c979c582897c2c445d2ffd32cb2ef
Victor Rentea <victorrentea@gmail.com>
2021-01-27 12:41:57 +0200

    Concrete class-proxies; good stuff. The strong point and weakeness of Spring

--- src/main/java/victor/training/patterns/structural/proxy/Magic.java
+++ src/main/java/victor/training/patterns/structural/proxy/Magic.java
@@ -2,6 +2,8 @@ package victor.training.patterns.structural.proxy;
 
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cglib.proxy.Callback;
+import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,17 +17,25 @@ public class Magic {
    public static void main(String[] args) {
       Math realStuff = new Math();
 
-      InvocationHandler h = new InvocationHandler() {
+//      InvocationHandler h = new InvocationHandler() {
+//         @Override
+//         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+//            System.out.println("Calling method " + method.getName() + " with args " + Arrays.toString(args));
+//            return method.invoke(realStuff, args);
+//         }
+//      };
+//      Math math = (IMath) Proxy.newProxyInstance(Magic.class.getClassLoader(),
+//          new Class<?>[]{IMath.class},
+//          h
+//      );
+      Callback callback = new org.springframework.cglib.proxy.InvocationHandler() {
          @Override
-         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+         public Object invoke(Object o, Method method, Object[] args) throws Throwable {
             System.out.println("Calling method " + method.getName() + " with args " + Arrays.toString(args));
             return method.invoke(realStuff, args);
          }
       };
-      IMath math = (IMath) Proxy.newProxyInstance(Magic.class.getClassLoader(),
-          new Class<?>[]{IMath.class},
-          h
-      );
+      Math math = (Math) Enhancer.create(Math.class, callback);
 
       System.out.println(math.sum(1,1));
       System.out.println(math.sum(2,0));
@@ -36,18 +46,10 @@ public class Magic {
    }
 }
 
-interface IMath {
-   int sum(int a, int b);
-   int product(int a, int b);
-}
-
-class Math implements IMath{
-   @Override
+class Math {
    public int sum(int a, int b) {
       return a+b;
    }
-
-   @Override
    public int product(int a, int b) {
       return a*b;
    }
