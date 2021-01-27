f8ee615c6e178895e1f52a363949b4c81dfa6097
Victor Rentea <victorrentea@gmail.com>
2021-01-27 11:30:37 +0200

    we isolated our logic from garbage in the same class. we applied separation by layers of abstraction.

--- src/main/java/victor/training/patterns/structural/adapter/domain/UserService.java
+++ src/main/java/victor/training/patterns/structural/adapter/domain/UserService.java
@@ -1,13 +1,15 @@
 package victor.training.patterns.structural.adapter.domain;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import victor.training.patterns.structural.adapter.infra.LdapUser;
 import victor.training.patterns.structural.adapter.infra.LdapUserWebserviceClient;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 @Slf4j
 @Service
@@ -21,12 +23,11 @@ public class UserService {
 	}
 
 	public void importUserFromLdap(String username) {
-		List<LdapUser> list = searchByUsername(username);
+		List<User> list = searchByUsername(username);
 		if (list.size() != 1) {
 			throw new IllegalArgumentException("There is no single user matching username " + username);
 		}
-		LdapUser ldapUser = list.get(0);
-		User user = convert(ldapUser);
+		User user = list.get(0);
 
 		if (user.getWorkEmail() != null) {
 			log.debug("Send welcome email to " + user.getWorkEmail());
@@ -35,21 +36,20 @@ public class UserService {
 	}
 	
 	public List<User> searchUserInLdap(String username) {
-		List<LdapUser> list = searchByUsername(username);
-		List<User> results = new ArrayList<>();
-		for (LdapUser ldapUser : list) {
-			results.add(convert(ldapUser));
-		}
-		return results;
+		return searchByUsername(username);
 	}
 
+	// Heaven
+	// ------------------- a line --------------------------------
+	// gabage . Dante's Inferno
 	private User convert(LdapUser ldapUser) {
 		String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
 		return new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
 	}
 
-	private List<LdapUser> searchByUsername(String username) {
-		return wsClient.search(username.toUpperCase(), null, null);
+	private List<User> searchByUsername(String username) {
+		return wsClient.search(username.toUpperCase(), null, null)
+			.stream().map(this::convert).collect(toList());
 	}
 
 }
