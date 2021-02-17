66cec10e352298592a74cc7416d1fd6d57064047
Victor Rentea <victorrentea@gmail.com>
2021-02-17 13:53:00 +0200

    ArchUnit

--- src/main/java/victor/training/patterns/behavioral/observer/events/Event1.java
+++
@@ -1,4 +0,0 @@
-package victor.training.patterns.behavioral.observer.events;
-
-public class Event1 {
-}
--- src/main/java/victor/training/patterns/behavioral/observer/p2/C2.java
+++
@@ -1,9 +0,0 @@
-package victor.training.patterns.behavioral.observer.p2;
-
-import victor.training.patterns.behavioral.observer.events.Event1;
-
-public class C2 {
-   public static void main(String[] args) {
-      new Event1();
-   }
-}
