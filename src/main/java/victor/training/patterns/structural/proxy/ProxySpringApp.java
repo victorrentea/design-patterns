2b062a0747dfae093109f601b9d344a7fe7acd3c
Victor Rentea <victorrentea@gmail.com>
2021-02-18 16:24:38 +0200

    Ordinea aspectelor conteaza

--- src/main/java/victor/training/patterns/structural/proxy/ProxySpringApp.java
+++ src/main/java/victor/training/patterns/structural/proxy/ProxySpringApp.java
@@ -1,36 +1,55 @@
 package victor.training.patterns.structural.proxy;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
-@EnableCaching
+@EnableCaching(order = 2)
 @SpringBootApplication
+@EnableTransactionManagement(order = 1)
 public class ProxySpringApp implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(ProxySpringApp.class, args);
 	}
 
-	
-	// TODO [1] implement decorator 
+	@Autowired
+	Other other;
+
+	// TODO [1] implement decorator
 	// TODO [2] apply decorator via Spring
 	// TODO [3] generic java.lang.reflect.Proxy 
 	// TODO [4] Spring aspect 
 	// TODO [5] Spring cache support
 	// TODO [6] Back to singleton (are you still alive?)
 	public void run(String... args) throws Exception {
-		ExpensiveOps ops = new ExpensiveOps(); 
+		ExpensiveOps ops = new ExpensiveOps();
 
+		other.f();
 		log.debug("\n");
 		log.debug("---- CPU Intensive ~ memoization?");
 		log.debug("10000169 is prime ? ");
 		log.debug("Got: " + ops.isPrime(10000169) + "\n");
 		log.debug("10000169 is prime ? ");
 		log.debug("Got: " + ops.isPrime(10000169) + "\n");
-		
+
 	}
-	
+
+}
+
+@Component
+class Other {
+
+	@Transactional
+	@Cacheable("a")
+	public void f() {
+		new RuntimeException().printStackTrace();
+	}
 }
