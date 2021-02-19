93a49d4fb341cbbfdcc977f34d65cae52acc20a8
Victor Rentea <victorrentea@gmail.com>
2021-02-19 11:46:01 +0200

    Reactive Rest resource : CompletableFuture sa nu blochezi threadul de HTTP ca sa poti trata jdemii de requesturi paralel Rulez pasi pe thread pooluri dedicate

--- src/main/java/victor/training/patterns/behavioral/command/CommandSpringApp.java
+++ src/main/java/victor/training/patterns/behavioral/command/CommandSpringApp.java
@@ -3,21 +3,19 @@ package victor.training.patterns.behavioral.command;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
-import static java.util.Arrays.asList;
 import static victor.training.patterns.stuff.ThreadUtils.sleepq;
 
 @EnableAsync
@@ -25,16 +23,28 @@ import static victor.training.patterns.stuff.ThreadUtils.sleepq;
 //@EnableBinding({Sink.class, Source.class})
 public class CommandSpringApp {
    public static void main(String[] args) {
-      SpringApplication.run(CommandSpringApp.class, args).close(); // Note: .close to stop executors after CLRunner finishes
+      SpringApplication.run(CommandSpringApp.class, args); // Note: .close to stop executors after CLRunner finishes
    }
 
    @Bean
    public ThreadPoolTaskExecutor executor() {
       ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+      executor.setCorePoolSize(2);
+      executor.setMaxPoolSize(2);
+      executor.setQueueCapacity(500);
+      executor.setThreadNamePrefix("barman-");
+      executor.initialize();
+      executor.setWaitForTasksToCompleteOnShutdown(true);
+      return executor;
+   }
+
+   @Bean
+   public ThreadPoolTaskExecutor barmanBere() {
+      ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
       executor.setCorePoolSize(1);
       executor.setMaxPoolSize(1);
       executor.setQueueCapacity(500);
-      executor.setThreadNamePrefix("barman-");
+      executor.setThreadNamePrefix("bere-");
       executor.initialize();
       executor.setWaitForTasksToCompleteOnShutdown(true);
       return executor;
@@ -43,8 +53,8 @@ public class CommandSpringApp {
 }
 
 @Slf4j
-@Component
-class Drinker implements CommandLineRunner {
+@RestController
+class Drinker {
    @Autowired
    private Barman barman;
 //   @Autowired
@@ -53,43 +63,49 @@ class Drinker implements CommandLineRunner {
    // TODO [1] inject and use a ThreadPoolTaskExecutor.submit
    // TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
    // TODO [3] wanna try it out over JMS? try out ServiceActivatorPattern
-   public void run(String... args) throws ExecutionException, InterruptedException {
-      log.debug("Submitting my order");
-      long t0 = System.currentTimeMillis();
 
-      ExecutorService pool = Executors.newFixedThreadPool(2);
+   @GetMapping
+   public CompletableFuture<DillyDilly> f() throws ExecutionException, InterruptedException {
 
-      Future<Beer> futureBeer = pool.submit(() -> barman.pourBeer());
-      Future<Vodka> futureVodka = pool.submit(() -> barman.pourVodka());
+      CompletableFuture<Beer> futureBeer = barman.pourBeer();
+      CompletableFuture<Vodka> futureVodka = barman.pourVodka();
 
       log.debug("Waiting for my drinks...");
       // bati darabana
 
-      Beer beer = futureBeer.get();
-      Vodka vodka = futureVodka.get();
+//      HttpServletRequest r;
+//      r.startAsync()
 
-//      Beer beer = barman.pourBeer();
+//      DeferredResult<DillyDilly> deferredResult = null;
+//       futureDilly.thenAccept(d -> deferredResult.setResult(d));
+      CompletableFuture<DillyDilly> futureDilly = futureBeer.thenCombineAsync(futureVodka, DillyDilly::new);
 
-//      Vodka vodka = barman.pourVodka();
-
-      long t1 = System.currentTimeMillis();
-      log.debug("Got my order in {} ms ! Enjoying {}", t1 - t0, asList(beer, vodka));
+      return futureDilly;
    }
 }
+
+@Data
+class DillyDilly {
+   private final Beer beer;
+   private final Vodka vodka;
+}
+
 
 @Slf4j
 @Service
 class Barman {
-   public Beer pourBeer() {
+   @Async("barmanBere")
+   public CompletableFuture<Beer> pourBeer() {
       log.debug("Pouring Beer...");
       sleepq(1000); // REST call / SELECT/ FTP / writefile
-      return new Beer();
+      return CompletableFuture.completedFuture(new Beer());
    }
 
-   public Vodka pourVodka() {
+   @Async
+   public CompletableFuture<Vodka> pourVodka() {
       log.debug("Pouring Vodka...");
       sleepq(1000);
-      return new Vodka();
+      return CompletableFuture.completedFuture(new Vodka());
    }
 }
 
