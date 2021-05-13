91ca7093d229e916046ecf65d4f3522387ecfd2e
Victor Rentea <victorrentea@gmail.com>
2021-05-13 13:33:34 +0300

    Async

--- src/main/java/victor/training/patterns/behavioral/command/CommandSpringApp.java
+++ src/main/java/victor/training/patterns/behavioral/command/CommandSpringApp.java
@@ -11,11 +11,13 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
@@ -48,42 +50,57 @@ public class CommandSpringApp {
 class Drinker implements CommandLineRunner {
    @Autowired
    private Barman barman;
-   @Autowired
-   ThreadPoolTaskExecutor pool;
 
    // TODO [1] inject and use a ThreadPoolTaskExecutor.submit
    // TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
    // TODO [3] wanna try it out over JMS? try out ServiceActivatorPattern
    public void run(String... args) throws ExecutionException, InterruptedException {
-      log.debug("Submitting my order");
+      log.debug("Submitting my order to " + barman.getClass());
       long t0 = System.currentTimeMillis();
       log.debug("Waiting for my drinks...");
 
-      Future<Beer> futureBeer = pool.submit(() -> barman.pourBeer());
-      Future<Vodka> futureVodka = pool.submit(() -> barman.pourVodka());
+      Future<Beer> futureBeer = barman.pourBeer();
+      Future<Vodka> futureVodka = barman.pourVodka();
 
       log.debug("A plecat chelnerul cu comanda mea");
       Beer beer = futureBeer.get();
       Vodka vodka = futureVodka.get();
 
+
       long t1 = System.currentTimeMillis();
       log.debug("Got my order in {} ms ! Enjoying {}", t1 - t0, asList(beer, vodka));
+
+      barman.injura("&$^!&@$^@&!%*!@&%*!@*$");
+      System.out.println("Ajung in patuc");
    }
 }
 
 @Slf4j
 @Service
 class Barman {
-   public Beer pourBeer() {
+   @Async
+   public CompletableFuture<Beer> pourBeer() {
       log.debug("Pouring Beer...");
+//      if (true) {
+//         throw new IllegalArgumentException("nu mai bere!");
+//      }
       sleepq(1000);
-      return new Beer();
+      return CompletableFuture.completedFuture(new Beer());
    }
 
-   public Vodka pourVodka() {
+   @Async
+   public CompletableFuture<Vodka> pourVodka() {
       log.debug("Pouring Vodka...");
       sleepq(1000);
-      return new Vodka();
+      return CompletableFuture.completedFuture(new Vodka());
+   }
+
+   @Async
+   public void injura(String uratura) {
+      if (uratura != null) {
+         throw new IllegalArgumentException("Iti fac buzunar");
+      }
+
    }
 }
 
