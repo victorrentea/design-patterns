2d3f17f42fe1baead57f52f44f0a5db2134209f4
Victor Rentea <victorrentea@gmail.com>
2021-05-14 12:16:32 +0300

    Am extras partea abstract dn clasa abstracta intr-o interfata pe care am compus-o prntr-un camp luat prin constructor

--- src/main/java/victor/training/patterns/behavioral/template/TemplateSpringApp.java
+++ src/main/java/victor/training/patterns/behavioral/template/TemplateSpringApp.java
@@ -20,18 +20,23 @@ public class TemplateSpringApp implements CommandLineRunner {
 
    private void placeOrder() {
       // other logic
-      new OrderReceivedEmailSender().sendEmail("a@b.com");
+      new EmailSender(new OrderReceivedEmailComposer()).sendEmail("a@b.com");
    }
 
    private void shipOrder() {
       // other logic
-      new OrderShippedEmailSender().sendEmail("a@b.com");
+      new EmailSender(new OrderShippedEmailComposer()).sendEmail("a@b.com");
       // TODO send order shipped email 'similar to how send order received was implemented'
       // TODO URLEncoder.encode
    }
 }
 
-abstract class AbstractEmailSender {
+class EmailSender {
+   private final EmailComposer composer;
+
+   public EmailSender(EmailComposer composer) {
+      this.composer = composer;
+   }
 
    public void sendEmail(String emailAddress) {
       EmailContext context = new EmailContext(/*smtpConfig,etc*/);
@@ -42,7 +47,7 @@ abstract class AbstractEmailSender {
             email.setSender("noreply@corp.com");
             email.setReplyTo("/dev/null");
             email.setTo(emailAddress);
-            compose(email);
+            composer.compose(email);
             boolean success = context.send(email);
             if (success) break;
          }
@@ -50,23 +55,24 @@ abstract class AbstractEmailSender {
          throw new RuntimeException("Can't send email", e);
       }
    }
-
-   public abstract void compose(Email email);
 }
 
-class OrderReceivedEmailSender extends AbstractEmailSender {
+class OrderReceivedEmailComposer implements EmailComposer {
    public void compose(Email email) {
       email.setSubject("Order Received!");
       email.setBody("Thank you for your order");
    }
 }
 
-class OrderShippedEmailSender extends AbstractEmailSender {
-   @Override
+class OrderShippedEmailComposer implements EmailComposer {
    public void compose(Email email) {
       email.setSubject("Order Shipped!");
       email.setBody("Ti-am trimis coletu. Speram sa ajunga ( de data asta :)");
    }
+}
+
+interface EmailComposer {
+   void compose(Email email);
 }
 
 class EmailContext {
