779da324c767ac09a3a05774488914a9ab8e1677
Victor Rentea <victorrentea@gmail.com>
2021-06-08 20:21:39 +0300

    Injecting with Spring

--- src/main/java/victor/training/patterns/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/patterns/behavioral/strategy/StrategySpringApp.java
@@ -1,14 +1,19 @@
 package victor.training.patterns.behavioral.strategy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class StrategySpringApp implements CommandLineRunner {
    private ConfigProvider configProvider = new ConfigFileProvider();
 
@@ -22,8 +27,9 @@ public class StrategySpringApp implements CommandLineRunner {
    // TODO [2] Convert it to Chain Of Responsibility
    // TODO [3] Wire with Spring
    // TODO [4] ConfigProvider: selected based on environment props, with Spring
+   @Autowired
+   CustomsService service;
    public void run(String... args) {
-      CustomsService service = new CustomsService();
       System.out.println("Tax for (RO,100,100) = " + service.calculateCustomsTax("RO", 100, 100));
       System.out.println("Tax for (CN,100,100) = " + service.calculateCustomsTax("CN", 100, 100));
       System.out.println("Tax for (UK,100,100) = " + service.calculateCustomsTax("UK", 100, 100));
@@ -32,14 +38,22 @@ public class StrategySpringApp implements CommandLineRunner {
    }
 }
 
+@Service
+@ConfigurationProperties("calc")
 class CustomsService {
    public double calculateCustomsTax(String originCountry, double tobaccoValue, double regularValue) { // UGLY API we CANNOT change
       TaxComputer computer = selectTaxComputer(originCountry);
       return computer.compute(tobaccoValue, regularValue);
    }
 
+   @Autowired
+   List<TaxComputer> allComputers;
+
+   //   @Value("${calc}")
+   Map<String, Class<? extends TaxComputer>> calc;
+
    private TaxComputer selectTaxComputer(String originCountry) {
-      List<TaxComputer> allComputers = Arrays.asList(new ChinaTaxComputer(), new EuropeTaxComputer(), new BrexitTaxComputer());
+      System.out.println("Map: " + calc);
       // you are allowed to change the interface.
       for (TaxComputer computer : allComputers) {
          if (computer.supportsCountry(originCountry)) {
