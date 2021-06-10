3be39e4698189ec94a01a6625648d0815b13909d
Victor Rentea <victorrentea@gmail.com>
2021-06-10 17:29:59 +0300

    I've extracted an Adapter wrapping around the external api (converting data)

---
+++ src/main/java/victor/training/patterns/structural/adapter/domain/LdapUserServiceAdapter.java
@@ -0,0 +1,27 @@
+package victor.training.patterns.structural.adapter.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import victor.training.patterns.structural.adapter.infra.LdapUser;
+import victor.training.patterns.structural.adapter.infra.LdapUserWebserviceClient;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class LdapUserServiceAdapter {
+   @Autowired
+   private LdapUserWebserviceClient wsClient;
+
+   public List<User> searchByUsername(String username) {
+      return wsClient.search(username.toUpperCase(), null, null)
+          .stream().map(this::convert).collect(Collectors.toList());
+   }
+
+   private User convert(LdapUser ldapUser) {
+      String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
+      return new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
+   }
+
+   // TODO @end: Archunit!
+}
--- src/main/java/victor/training/patterns/structural/adapter/domain/UserService.java
+++ src/main/java/victor/training/patterns/structural/adapter/domain/UserService.java
@@ -3,11 +3,8 @@ package victor.training.patterns.structural.adapter.domain;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import victor.training.patterns.structural.adapter.infra.LdapUser;
-import victor.training.patterns.structural.adapter.infra.LdapUserWebserviceClient;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -30,10 +27,10 @@ import java.util.stream.Collectors;
 // ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN ZEN
 public class UserService {
 	@Autowired
-	private LdapUserWebserviceClient wsClient;
+	private LdapUserServiceAdapter adapter;
 
 	public void importUserFromLdap(String username) {
-		List<User> list = searchByUsername(username);
+		List<User> list = adapter.searchByUsername(username);
 		if (list.size() != 1) {
 			throw new IllegalArgumentException("There is no single user matching username " + username);
 		}
@@ -46,23 +43,11 @@ public class UserService {
 	}
 
 	public List<User> searchUserInLdap(String username) {
-		List<User> results = searchByUsername(username);
+		List<User> results = adapter.searchByUsername(username);
 		return results.subList(0, 5);
 	}
 
 	// Only MY CLASSES
 	// ------------------------------------------ architecture is the art of drawing lines --------------------------------
 	// shit can happen
-
-	private List<User> searchByUsername(String username) {
-		return wsClient.search(username.toUpperCase(), null, null)
-			.stream().map(this::convert).collect(Collectors.toList());
-	}
-
-	private User convert(LdapUser ldapUser) {
-		String fullName = ldapUser.getfName() + " " + ldapUser.getlName().toUpperCase();
-		return new User(ldapUser.getuId(), fullName, ldapUser.getWorkEmail());
-	}
-
-	// TODO @end: Archunit!
 }
