22a2d58e0e787d7bdb0db629f037288abffeb033
Victor Rentea <victorrentea@gmail.com>
2021-06-14 15:21:00 +0300

    Order between event listeners -> temporal coupling

--- src/main/java/victor/training/patterns/behavioral/observer/ObserverSpringApp.java
+++ src/main/java/victor/training/patterns/behavioral/observer/ObserverSpringApp.java
@@ -1,12 +1,18 @@
 package victor.training.patterns.behavioral.observer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.util.Random;
 
@@ -40,30 +46,60 @@ public class ObserverSpringApp {
 }
 
 @Service
+	// ~microservice
 class OrderService {
+	private static final Logger log = LoggerFactory.getLogger(OrderService.class);
+	//	@Autowired
+//	private StockManagementService stockManagementService;
+//	@Autowired
+//	private InvoiceService invoiceService;
 	@Autowired
-	private StockManagementService stockManagementService;
+	private ApplicationEventPublisher applicationEventPublisher;
 
+	//	@Transactional
 	public void placeOrder(Long orderId) {
-		System.out.println("Halo!");
-		stockManagementService.checkStock(orderId);
+		log.info("Halo!");
+//		stockManagementService.checkStock(orderId);
+//		invoiceService.generateInvoice(orderId);
 		// TODO call invoicing too
+		applicationEventPublisher.publishEvent(new OrderPlacedEvent(orderId));
+	}
+}
+
+class OrderPlacedEvent {
+	private final long orderId;
+
+	OrderPlacedEvent(long orderId) {
+		this.orderId = orderId;
+	}
+
+	public long getOrderId() {
+		return orderId;
 	}
 }
 
 @Service
+	// ~microservice
 class StockManagementService {
-	public void checkStock(long orderId) {
-		System.out.println("Checking stock for products in order " + orderId);
-		System.out.println("If something goes wrong - throw an exception");
+	private static final Logger log = LoggerFactory.getLogger(StockManagementService.class);
+
+	@EventListener
+	@Order(10)
+	public void checkStock(OrderPlacedEvent event) {
+		log.info("Checking stock for products in order " + event.getOrderId());
+		log.info("If something goes wrong - throw an exception");
 	}
 }
 
 @Service
-class InvoiceService {
-	public void generateInvoice(long orderId) {
-		System.out.println("Generating invoice for order id: " + orderId);
+class InvoiceService { // ~microservice
+	private static final Logger log = LoggerFactory.getLogger(InvoiceService.class);
+
+	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+	@Order(20)
+	public void generateInvoice(OrderPlacedEvent event) {
+		log.info("Generating invoice for order id: " + event.getOrderId());
 		// TODO what if...
 		// throw new RuntimeException("thrown from generate invoice");
-	} 
+	}
 }
