598e07aef62368381c0735802e353ab2bcc1babb
Victor Rentea <victorrentea@gmail.com>
2021-07-26 11:35:02 +0300

    Builder classic scris de mana (sau generat cu Lombok)

--- src/main/java/victor/training/patterns/creational/builder/CustomerBuilder.java
+++ src/main/java/victor/training/patterns/creational/builder/CustomerBuilder.java
@@ -1,14 +1,26 @@
 package victor.training.patterns.creational.builder;
 
+import java.util.Arrays;
+
 public class CustomerBuilder {
-	private final Customer customer = new Customer();
+   private final Customer customer = new Customer();
 
-	public CustomerBuilder withName(String name) {
-		customer.setName(name);
-		return this;
-	}
+   public CustomerBuilder withName(String name) {
+      customer.setName(name);
+      return this;
+   }
 
-	public Customer build() {
-		return customer;
-	}
+   public Customer build() {
+      return customer;
+   }
+
+   public CustomerBuilder withAddress(Address address) {
+      customer.setAddress(address);
+      return this;
+   }
+
+   public CustomerBuilder withLabels(String... labels) {
+      customer.setLabels(Arrays.asList(labels));
+      return this;
+   }
 }
