6fa19a757475cea23cdf0c5aa97aef223626edd3
Victor Rentea <victorrentea@gmail.com>
2021-07-26 11:56:26 +0300

    Builderii sunt de fapt utili in a scrie multe teste cu diverse combinati de date.

--- src/test/java/victor/training/patterns/creational/builder/CustomerValidatorTest.java
+++ src/test/java/victor/training/patterns/creational/builder/CustomerValidatorTest.java
@@ -1,21 +1,51 @@
 package victor.training.patterns.creational.builder;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Test;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 @Slf4j
 public class CustomerValidatorTest {
 
 	private CustomerValidator validator = new CustomerValidator();
 
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
 	@Test
 	public void validCustomer_ok() {
-		Customer customer = new Customer();
-		customer.setName("John Doe");
-		Address address = new Address();
-		address.setCity("Bucharest");
-		address.setStreetName("Dristor 91");
-		customer.setAddress(address);
+		Customer customer = aValidCustomer();
+		validator.validate(customer);
+	}
+
+	private Customer aValidCustomer() {
+		return new Customer()
+			.setName("John Doe")
+			.setAddress(new Address()
+				.setCity("Bucharest")
+				.setStreetName("Dristor"));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void customerWithoutName() {
+		Customer customer = aValidCustomer().setName(null);
+		validator.validate(customer);
+	}
+
+	@Test
+	public void customerWithoutCity() {
+		expectedException.expectMessage("city");
+		Customer customer = aValidCustomer();
+		customer.getAddress().setCity(null);
+		validator.validate(customer);
+	}
+
+	@Test
+	public void customerWithoutStreeName() {
+		expectedException.expectMessage("street");
+		Customer customer = aValidCustomer();
+		customer.getAddress().setStreetName(null);
 		validator.validate(customer);
 	}
 
