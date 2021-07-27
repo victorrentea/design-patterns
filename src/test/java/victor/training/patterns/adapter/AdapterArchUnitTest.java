c8103a6fdab5642d898885ad33d191e9d4f2dff5
Victor Rentea <victorrentea@gmail.com>
2021-07-27 11:31:36 +0300

    unit test

--- src/test/java/victor/training/patterns/adapter/AdapterArchUnitTest.java
+++ src/test/java/victor/training/patterns/adapter/AdapterArchUnitTest.java
@@ -3,19 +3,17 @@ package victor.training.patterns.adapter;
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
-import com.tngtech.archunit.lang.syntax.elements.ClassesShouldConjunction;
 import org.junit.Test;
 
 public class AdapterArchUnitTest {
    @Test
    public void dependencyInversionTest() {
-      JavaClasses classes = new ClassFileImporter().importPackages("victor.training");
+      JavaClasses classes = new ClassFileImporter().importPackages("victor.training.patterns.structural.adapter");
 
-      ClassesShouldConjunction domainDoesnDependOnInfra = ArchRuleDefinition.noClasses()
-          .that().resideInAPackage("..adapter.domain")
+      ArchRuleDefinition.noClasses()
+          .that().resideInAPackage("..domain")
           .should()
-          .dependOnClassesThat().resideInAPackage("..adapter.infra");
-
-      domainDoesnDependOnInfra.check(classes);
+          .dependOnClassesThat().resideInAPackage("..infra")
+          .check(classes);
    }
 }
