f53c84ef7a735aa197df83ce9a779e65102f983c
Victor Rentea <victorrentea@gmail.com>
2021-07-29 09:54:32 +0300

    Decorator

--- src/main/java/victor/training/patterns/structural/proxy/ExpensiveOps.java
+++ src/main/java/victor/training/patterns/structural/proxy/ExpensiveOps.java
@@ -1,36 +1,36 @@
 package victor.training.patterns.structural.proxy;
 
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Hex;
-import org.apache.commons.io.FileUtils;
-import org.jooq.lambda.Unchecked;
 
-import java.io.File;
 import java.math.BigDecimal;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
-public class ExpensiveOps {
-
-   private static final BigDecimal TWO = new BigDecimal("2");
-
+class ExpensiveOpsCuCache implements ExpensiveOps {
+   private final ExpensiveOps expensiveOps;
    private Map<Integer, Boolean> cache = new HashMap<>();
+
+   public ExpensiveOpsCuCache(ExpensiveOps expensiveOps) {
+      this.expensiveOps = expensiveOps;
+   }
 
    public Boolean isPrime(int n) {
       if (cache.containsKey(n)) {
          return cache.get(n);
       }
-      Boolean prime = isPrime_(n);
+      Boolean prime = expensiveOps.isPrime(n);
       cache.put(n, prime);
       return prime;
    }
 
-   private Boolean isPrime_(int n) {
+}
+
+@Slf4j
+class ExpensiveOpsImpl implements ExpensiveOps {
+   private static final BigDecimal TWO = new BigDecimal("2");
+
+   public Boolean isPrime(int n) {
       log.debug("Computing isPrime({})", n);
       BigDecimal number = new BigDecimal(n);
       if (number.compareTo(TWO) <= 0) {
@@ -49,19 +49,9 @@ public class ExpensiveOps {
       return true;
    }
 
-   @SneakyThrows
-   public String hashAllFiles(File folder) {
-      log.debug("Computing hashAllFiles({})", folder);
-      MessageDigest md = MessageDigest.getInstance("MD5");
-      for (int i = 0; i < 3; i++) { // pretend there is much more work to do here
-         Files.walk(folder.toPath())
-             .map(Path::toFile)
-             .filter(File::isFile)
-             .map(Unchecked.function(FileUtils::readFileToString))
-             .forEach(s -> md.update(s.getBytes()));
-      }
-      byte[] digest = md.digest();
-      return Hex.encodeHexString(digest).toUpperCase();
-   }
 
+}
+
+interface ExpensiveOps {
+   Boolean isPrime(int n);
 }
