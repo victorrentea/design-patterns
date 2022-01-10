06442b1082351deb8f436ca40bbf42add659c84e
Victor Rentea <victorrentea@gmail.com>
2022-01-10 17:32:50 +0200

    Proxy de interfete !

---
+++ src/main/java/victor/training/patterns/structural/proxy/HaiSaNeJucam.java
@@ -0,0 +1,56 @@
+package victor.training.patterns.structural.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Arrays;
+
+public class HaiSaNeJucam {
+
+   public static void main(String[] args) {
+
+      MatematicaImpl impl = new MatematicaImpl();
+
+      InvocationHandler handler = new InvocationHandler() {
+         @Override
+         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            System.out.println("Invoc metoda " + method.getName() + " cu param " + Arrays.toString(args));
+            return method.invoke(impl, args); // TODO chem metoda reala
+         }
+      };
+      Matematica proxyLaMatematica = (Matematica) Proxy.newProxyInstance(
+          HaiSaNeJucam.class.getClassLoader(),
+          new Class<?>[]{Matematica.class},
+          handler
+      );
+
+      cumArata(proxyLaMatematica);
+   }
+
+   private static void cumArata(Matematica m) {
+      System.out.println(m.suma(1, 1));
+      System.out.println(m.suma(2, 0));
+      System.out.println(m.suma(3, -1));
+      System.out.println(m.produs(2, 1));
+
+      System.out.println(m.produs(2, 2));
+   }
+}
+
+class MatematicaImpl implements Matematica {
+   @Override
+   public int suma(int a, int b) {
+      return a + b;
+   }
+
+   @Override
+   public int produs(int a, int b) {
+      return a * b;
+   }
+}
+
+interface Matematica {
+   int suma(int a, int b);
+
+   int produs(int a, int b);
+}
