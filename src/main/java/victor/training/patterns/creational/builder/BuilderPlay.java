514b898eed2db35ef54e6824c185faac9b57706a
Victor Rentea <victorrentea@gmail.com>
2022-01-11 11:32:43 +0200

    daca ai obiecte cu setteri, nu ai nevoie de builderi : faci setteri sa intoarca this

--- src/main/java/victor/training/patterns/creational/builder/BuilderPlay.java
+++ src/main/java/victor/training/patterns/creational/builder/BuilderPlay.java
@@ -3,6 +3,8 @@ package victor.training.patterns.creational.builder;
 //import java.util.ArrayList; // INITIAL
 //import java.util.List; // INITIAL
 
+import java.util.Arrays;
+
 public class BuilderPlay {
 
 	public static void main(String[] args) {
@@ -15,6 +17,13 @@ public class BuilderPlay {
 				.withCity("Bucharest")
 				.build())
 			.build();
+
+		customer = new Customer()
+			.setName("John Doe")
+			.setLabels(Arrays.asList("Label1"))
+			.setAddress(new Address()
+				.setStreetName("Viorele")
+				.setCity("Bucharest"));
 
 
 		System.out.println("Customer name: " + customer.getName());
