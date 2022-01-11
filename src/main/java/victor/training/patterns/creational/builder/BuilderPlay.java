bd5b5218ab083b0ed5bd0f4d88ffde7d5303bb39
Victor Rentea <victorrentea@gmail.com>
2022-01-11 11:28:03 +0200

    Builder clasici

--- src/main/java/victor/training/patterns/creational/builder/BuilderPlay.java
+++ src/main/java/victor/training/patterns/creational/builder/BuilderPlay.java
@@ -3,22 +3,19 @@ package victor.training.patterns.creational.builder;
 //import java.util.ArrayList; // INITIAL
 //import java.util.List; // INITIAL
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BuilderPlay {
 
 	public static void main(String[] args) {
 
-		Customer customer = new Customer();
-		customer.setName("John Doe");
-		List<String> labels = new ArrayList<>();
-		labels.add("Label1");
-		customer.setLabels(labels);
-		Address address = new Address();
-		address.setStreetName("Viorele");
-		address.setCity("Bucharest");
-		customer.setAddress(address);
+		Customer customer = new CustomerBuilder()
+			.withName("John Doe")
+			.withLabels("Label1", "Label2")
+			.withAddress(new AddressBuilder()
+				.withStreetName("Viorele")
+				.withCity("Bucharest")
+				.build())
+			.build();
+
 
 		System.out.println("Customer name: " + customer.getName());
 		System.out.println("Customer address: " + customer.getAddress().getStreetName());
