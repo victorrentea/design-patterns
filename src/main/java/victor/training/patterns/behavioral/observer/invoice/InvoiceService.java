094e936d084c1d057a88659025debaae75ee4127
Victor Rentea <victorrentea@gmail.com>
2022-01-14 11:39:01 +0200

    never work if events are sent on queus

--- src/main/java/victor/training/patterns/behavioral/observer/invoice/InvoiceService.java
+++ src/main/java/victor/training/patterns/behavioral/observer/invoice/InvoiceService.java
@@ -1,12 +1,14 @@
 package victor.training.patterns.behavioral.observer.invoice;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 import victor.training.patterns.behavioral.observer.events.OrderPlacedEvent;
 
 @Service
 public class InvoiceService { // 35.000
    @EventListener
+   @Order(20)
    public void generateInvoice(OrderPlacedEvent event) {
       System.out.println("Generating invoice for order id: " + event.getOrderId());
       // TODO what if...
