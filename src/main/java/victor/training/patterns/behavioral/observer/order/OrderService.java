c7ddcc1aeb84f67629fe4bbc36713183a60c2c9f
Victor Rentea <victorrentea@gmail.com>
2022-01-14 11:41:55 +0200

    @EventListenre advacnced

--- src/main/java/victor/training/patterns/behavioral/observer/order/OrderService.java
+++ src/main/java/victor/training/patterns/behavioral/observer/order/OrderService.java
@@ -9,10 +9,11 @@ import victor.training.patterns.behavioral.observer.events.OrderPlacedEvent;
 public class OrderService { // 2k
    @Autowired
    private ApplicationEventPublisher publisher;
-
+//@Transactional propagates to all listeners via current thread.
    public void placeOrder(Long orderId) {
       System.out.println("persist the order!");
-      publisher.publishEvent(new OrderPlacedEvent(orderId));
+      publisher.publishEvent(new OrderPlacedEvent(orderId)); // all listeners are invoker sync and sequentially
+      System.out.println("DONE");
 //      stockManagementService.checkStock(orderId);
 //      invoiceService.generateInvoice(orderId);
    }
