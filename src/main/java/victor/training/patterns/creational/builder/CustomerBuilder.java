cd5a89f98961c286949fd6fcf479330f1b121316
Victor Rentea <victorrentea@gmail.com>
2022-01-19 16:43:57 +0200

    Builder pattern peste obiect cu setteri (mutabil).

--- src/main/java/victor/training/patterns/creational/builder/CustomerBuilder.java
+++ src/main/java/victor/training/patterns/creational/builder/CustomerBuilder.java
@@ -1,14 +1,19 @@
 package victor.training.patterns.creational.builder;
 
 public class CustomerBuilder {
-	private final Customer customer = new Customer();
+   private Customer customer = new Customer();
 
-	public CustomerBuilder withName(String name) {
-		customer.setName(name);
-		return this;
-	}
+   public Customer build() {
+      return customer;
+   }
 
-	public Customer build() {
-		return customer;
-	}
+   public CustomerBuilder withName(String name) {
+      customer.setName(name);
+      return this;
+   }
+
+   public CustomerBuilder withAddress(Address address) {
+      customer.setAddress(address);
+      return this;
+   }
 }
