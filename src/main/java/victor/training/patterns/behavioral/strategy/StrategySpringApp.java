afd9295d0790ac751d7e4bdbe061ef673572b84c
Victor Rentea <victorrentea@gmail.com>
2022-01-27 13:41:37 +0200

    "Chain of Responsibility" pattern (AKA Filters) pushes the responsibility to select the handler DOWN to the handlers themselves

--- src/main/java/victor/training/patterns/behavioral/strategy/StrategySpringApp.java
+++ src/main/java/victor/training/patterns/behavioral/strategy/StrategySpringApp.java
@@ -1,9 +1,13 @@
 package victor.training.patterns.behavioral.strategy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
@@ -15,23 +19,25 @@ public class StrategySpringApp implements CommandLineRunner {
 	}
 
 	
-	private ConfigProvider configProvider = new ConfigFileProvider(); 
-	
+	private ConfigProvider configProvider = new ConfigFileProvider();
+
 	// TODO [1] Break CustomsService logic into Strategies
 	// TODO [2] Convert it to Chain Of Responsibility
 	// TODO [3] Wire with Spring
 	// TODO [4] ConfigProvider: selected based on environment props, with Spring
+	@Autowired
+	CustomsService service;
 	public void run(String... args) {
-		CustomsService service = new CustomsService();
 		System.out.println("Tax for (RO,100,100) = " + service.calculateCustomsTax("RO", 100, 100));
 		System.out.println("Tax for (CN,100,100) = " + service.calculateCustomsTax("CN", 100, 100));
 		System.out.println("Tax for (UK,100,100) = " + service.calculateCustomsTax("UK", 100, 100));
-		System.out.println("Tax for (UK,100,100) = " + service.calculateCustomsTax("MX", 100, 100));
+//		System.out.println("Tax for (UK,100,100) = " + service.calculateCustomsTax("MX", 100, 100));
 
 		System.out.println("Property: " + configProvider.getProperties().getProperty("someProp"));
 	}
 }
 
+@Service
 class CustomsService {
 	private static final Map<?, TaxCalculator> MAP = Map.of(
 		"UK", new UKTaxCalculator(),
@@ -52,43 +58,74 @@ class CustomsService {
 		return calculator.calculateTax(tobaccoValue, regularValue);
 	}
 
+	@Autowired
+	List<TaxCalculator> all;// = List.of(new EUTaxCalculator(), new ChinaTaxCalculator(), new UKTaxCalculator());
+
 	private TaxCalculator selectCalculator(String originCountry) {
-		switch (originCountry) {
-			case "UK":
-				return new UKTaxCalculator();
-			case "CN":
-				return new ChinaTaxCalculator();
-			case "FR":
-			case "BE":
-			case "NL":
-			case "ES": // other EU country codes...
-			case "RO":
-				return new EUTaxCalculator();
-			default:
-				throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
+
+		for (TaxCalculator taxCalculator : all) {
+			if (taxCalculator.accepts(originCountry)) {
+				return taxCalculator;
+			}
 		}
+		throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
+
+//		switch (originCountry) {
+//			case "UK":
+//				return new UKTaxCalculator();
+//			case "CN":
+//				return new ChinaTaxCalculator();
+//			case "FR":
+//			case "BE":
+//			case "NL":
+//			case "ES": // other EU country codes...
+//			case "RO":
+//				return new EUTaxCalculator();
+//			default:
+//				throw new IllegalArgumentException("Not a valid country ISO2 code: " + originCountry);
+//		}
 	}
 }
 
+@Component
 class ChinaTaxCalculator implements TaxCalculator {
 	public double calculateTax(double tobaccoValue, double regularValue) {
 		return tobaccoValue + regularValue;
 	}
+
+	@Override
+	public boolean accepts(String originCountry) {
+		return "CN".equals(originCountry);
+	}
 }
 
+@Component
 class UKTaxCalculator implements TaxCalculator {
 	public double calculateTax(double tobaccoValue, double regularValue) {
 		// heavy logic
 		return tobaccoValue / 2 + regularValue;
 	}
+
+	@Override
+	public boolean accepts(String originCountry) {
+		return "UK".equals(originCountry);
+	}
 }
 
+@Component
 class EUTaxCalculator implements TaxCalculator {
 	public double calculateTax(double tobaccoValue, double regularValueUNUSED_WHY_IT_THIS_HERE_YOU_WONDER_IN_2_YEARS) { // loss of precision.
 		return tobaccoValue / 3;
+	}
+
+	@Override
+	public boolean accepts(String originCountry) {
+		return List.of("RO", "ES", "FR", "BE", "NL").contains(originCountry);
 	}
 }
 
 interface TaxCalculator {
 	double calculateTax(double tobaccoValue, double regularValue);
+
+	boolean accepts(String originCountry);
 }
