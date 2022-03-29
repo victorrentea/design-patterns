dac91cceb8bd76fb8fa982f7b12980fbba98715e
Victor Rentea <victorrentea@gmail.com>
2022-03-29 15:14:56 +0300

    Template Method Pattern

--- src/main/java/victor/training/patterns/behavioral/template/TemplateSpringApp.java
+++ src/main/java/victor/training/patterns/behavioral/template/TemplateSpringApp.java
@@ -20,19 +20,19 @@ public class TemplateSpringApp implements CommandLineRunner {
 
    private void placeOrder() {
       // more logic
-      new EmailService().sendOrderPlacedEmail("a@b.com");
+      new OrderPlacedEmailSender().sendEmail("a@b.com");
    }
 
    private void shipOrder() {
       // more logic
       // TODO implement 'similar to how order placed email was implemented'
       // TODO URLEncoder.encode
+      new OrderShippedEmailSender().sendEmail("a@b.com");
    }
 }
 
-class EmailService {
-
-   public void sendOrderPlacedEmail(String emailAddress) {
+abstract class AbstractEmailSender {
+   public void sendEmail(String emailAddress) {
       EmailContext context = new EmailContext(/*smtpConfig,etc*/);
       int MAX_RETRIES = 3;
       try {
@@ -41,14 +41,28 @@ class EmailService {
             email.setSender("noreply@corp.com");
             email.setReplyTo("/dev/null");
             email.setTo(emailAddress);
-            email.setSubject("Order Received!");
-            email.setBody("Thank you for your order");
+            writeEmail(email);
             boolean success = context.send(email);
             if (success) break;
          }
       } catch (Exception e) {
          throw new RuntimeException("Can't send email", e);
       }
+   }
+   protected abstract void writeEmail(Email email);
+}
+class OrderPlacedEmailSender extends AbstractEmailSender {
+   protected void writeEmail(Email email) {
+      email.setSubject("Order Received!");
+      email.setBody("Thank you for your order");
+   }
+}
+
+class OrderShippedEmailSender extends AbstractEmailSender {
+   protected void writeEmail(Email email) {  // NEVER DO THAT: override concrete method > creates confusion and panic
+      email.setSubject("Order Shipped!");
+      email.setBody("We've shipped the order to you");
+      email.encrypt();
    }
 }
 
@@ -67,4 +81,12 @@ class Email {
    private String sender;
    private String replyTo;
    private String to;
+
+   public void setAttachment(String attach) {
+
+   }
+
+   public void encrypt() {
+
+   }
 }
