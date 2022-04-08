3eb50628c91f77778501cb32590d35fffb13cde8
Victor Rentea <victorrentea@gmail.com>
2022-04-08 14:43:47 +0300

    Command patter ca instante de Callable date unui thread pool spre executie. pt paralelizare sau pentru a rezista de blackfriday

--- src/main/java/victor/training/patterns/command/CommandSpringApp.java
+++ src/main/java/victor/training/patterns/command/CommandSpringApp.java
@@ -3,21 +3,23 @@ package victor.training.patterns.command;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.*;
 
+import static java.util.Arrays.asList;
 import static victor.training.patterns.stuff.ThreadUtils.sleepq;
 
 @EnableAsync
@@ -25,7 +27,7 @@ import static victor.training.patterns.stuff.ThreadUtils.sleepq;
 @EnableBinding({Sink.class, Source.class})
 public class CommandSpringApp {
    public static void main(String[] args) {
-      SpringApplication.run(CommandSpringApp.class, args); // Note: .close to stop executors after CLRunner finishes
+      SpringApplication.run(CommandSpringApp.class, args).close(); // Note: .close to stop executors after CLRunner finishes
    }
 
    @Bean
@@ -56,48 +58,35 @@ class Drinker {
    // TODO [2] make them return a CompletableFuture + @Async + asyncExecutor bean
    // TODO [3] wanna try it out over JMS? try out ServiceActivatorPattern
    @GetMapping("drink")
-   public CompletableFuture<DillyDilly> drink() throws ExecutionException, InterruptedException {
-      log.debug("Submitting my order cui: " + barman.getClass());
-
+   public void drink() throws ExecutionException, InterruptedException {
+      log.debug("Submitting my order");
       long t0 = System.currentTimeMillis();
       log.debug("Waiting for my drinks...");
 
-      CompletableFuture<Beer> futureBeer = barman.pourBeer();
-      CompletableFuture<Vodka> futureVodka = barman.pourVodka();
+      Future<Beer> futureBeer = threadPool.submit(() -> barman.pourBeer());
+      Future<Vodka> futureVodka = threadPool.submit(() -> barman.pourVodka());
 
-//      Beer beer = futureBeer.get(); // 1 secunda sta main blocat aici
-//      Vodka vodka = futureVodka.get(); // ~0 secunde sta main blocat aici
-
-      CompletableFuture<DillyDilly> futureDilly = futureBeer.thenCombine(futureVodka, (beer, vodka) -> new DillyDilly(beer, vodka));
+      Beer beer = futureBeer.get(); // 1 secunda sta main blocat aici
+      Vodka vodka = futureVodka.get(); // ~0 secunde sta main blocat aici
 
       long t1 = System.currentTimeMillis();
       log.debug("Got my order in {} ms ! Enjoying ", t1 - t0);
-      return futureDilly;
-
    }
-}
-
-@Data
-class DillyDilly {
-   private final Beer beer;
-   private final Vodka vodka;
 }
 
 @Slf4j
 @Service
 class Barman {
-   @Async//("threadPoolulMeu")
-   public CompletableFuture<Beer> pourBeer() {
+   public Beer pourBeer() {
       log.debug("Pouring Beer (1 second) REST CALL ...");
       sleepq(1000);
-      return CompletableFuture.completedFuture(new Beer());
+      return new Beer();
    }
 
-   @Async
-   public CompletableFuture<Vodka> pourVodka() {
+   public Vodka pourVodka() {
       log.debug("Pouring Vodka (1 second) SOAP/RMI/DB...");
       sleepq(1000);
-      return CompletableFuture.completedFuture(new Vodka());
+      return new Vodka();
    }
 }
 
