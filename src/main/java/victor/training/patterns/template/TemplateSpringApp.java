a4edd69ce450614068924debd5045e37d5b1e8dd
Victor Rentea <victorrentea@gmail.com>
2022-10-13 23:20:07 +0300

    END

--- src/main/java/victor/training/patterns/template/TemplateSpringApp.java
+++ src/main/java/victor/training/patterns/template/TemplateSpringApp.java
@@ -4,89 +4,111 @@ import lombok.Data;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.Random;
 
 @SpringBootApplication
 public class TemplateSpringApp implements CommandLineRunner {
-   public static void main(String[] args) {
-      SpringApplication.run(TemplateSpringApp.class, args);
-   }
+    public static void main(String[] args) {
+        SpringApplication.run(TemplateSpringApp.class, args);
+    }
 
-   public void run(String... args) {
-      placeOrder();
-      shipOrder();
-   }
+    public void run(String... args) {
+        placeOrder();
+        shipOrder();
+    }
 
-   private void placeOrder() {
-      // logic
-      new OrderPlacedEmailSender().sendEmail("a@b.com");
-   }
+    private void placeOrder() {
+        // logic
+        new OrderPlacedEmailSender().sendEmail("a@b.com");
+    }
 
-   private void shipOrder() {
-      // logic
-      new OrderShippedEmailSender().sendEmail("a@b.com");
-      // TODO implement 'similar to how order placed email was implemented'
-   }
+    private void shipOrder() {
+        // logic
+        new OrderShippedEmailSender().sendEmail("a@b.com");
+        // TODO implement 'similar to how order placed email was implemented'
+    }
+}
+
+interface Chestii {
+    //   f1,f2,f3
 }
 
 abstract class AbstractEmailSender {
 
-   public void sendEmail(String emailAddress) {
-      EmailContext context = new EmailContext(/*smtpConfig,etc*/);
-      int MAX_RETRIES = 3;
-      try {
-         for (int i = 0; i < MAX_RETRIES; i++) {
-            Email email = new Email(); // constructor generates new unique ID
-            email.setSender("noreply@corp.com");
-            email.setReplyTo("/dev/null");
-            email.setTo(emailAddress);
-            compose(email);
-            boolean success = context.send(email);
-            if (success) break;
-         }
-      } catch (Exception e) {
-         throw new RuntimeException("Can't send email", e);
-      }
-   }
+    public void sendEmail(String emailAddress) {
+        EmailContext context = new EmailContext(/*smtpConfig,etc*/);
+        int MAX_RETRIES = 3;
+        try {
+            for (int i = 0; i < MAX_RETRIES; i++) {
+                Email email = new Email(); // constructor generates new unique ID
+                email.setSender("noreply@corp.com");
+                email.setReplyTo("/dev/null");
+                email.setTo(emailAddress);
+                compose(email);
+                boolean success = context.send(email);
+                if (success) break;
+                afterEmailSent(email);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Can't send email", e);
+        }
+    }
 
-   public abstract void compose(Email email);
+    protected void afterEmailSent(Email email) { }
 
+    protected String safeguardSubject(String s) {
+        //      if (,,.,)
+        return "[EXTERNAL]" + s;
+    }
+
+    public abstract void compose(Email email);
 }
+
 class OrderPlacedEmailSender extends AbstractEmailSender {
-   public void compose(Email email)
-   {
-      email.setSubject("Order Placed");
-      email.setBody("Thank you for your order");
-   }
+    public void compose(Email email) {
+        email.setSubject(safeguardSubject("Order Placed"));
+        email.setBody("Thank you for your order");
+    }
 }
+
 class OrderShippedEmailSender extends AbstractEmailSender {
-   @Override
-   public void compose(Email email) {
-      email.setSubject("Order Shipped");
-      email.setBody("Ti-am trimis, speram sa ajunga (de data asta)");
-      email.addAttachment("routing numbrer AWB");
-   }
+    @Override
+    public void compose(Email email) {
+//        new TransactionTemplate().execute(s -> {
+//            insert
+//            insert
+//        })
+        email.setSubject(safeguardSubject("Order Shipped"));
+        email.setBody("Ti-am trimis, speram sa ajunga (de data asta)");
+        email.addAttachment("routing numbrer AWB");
+    }
+
+    @Override
+    protected void afterEmailSent(Email email) {
+        System.out.println("Audit " + email);
+    }
 }
 
 class EmailContext {
-   public boolean send(Email email) {
-      System.out.println("Trying to send " + email);
-      return new Random(System.nanoTime()).nextBoolean();
-   }
+    public boolean send(Email email) {
+        System.out.println("Trying to send " + email);
+        return new Random(System.nanoTime()).nextBoolean();
+    }
 }
 
 @Data
 class Email {
-   private final long id = new Random(System.nanoTime()).nextLong();
-   private String subject;
-   private String body;
-   private String sender;
-   private String replyTo;
-   private String to;
+    private final long id = new Random(System.nanoTime()).nextLong();
+    private String subject;
+    private String body;
+    private String sender;
+    private String replyTo;
+    private String to;
 
-   public void addAttachment(String routing_numbrer_awb) {
+    public void addAttachment(String routing_numbrer_awb) {
 
 
-   }
+    }
 }
