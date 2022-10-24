925572e3729fa2d630a1047d05f4dbe505bf4a56
Victor Rentea <victorrentea@gmail.com>
2022-10-24 16:01:51 +0300

    immutable play

---
+++ src/main/java/victor/training/patterns/proxy/Immutable.java
@@ -0,0 +1,41 @@
+package victor.training.patterns.proxy;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Immutable {
+    private final String s;
+    private final List<String> list;
+
+    public Immutable(String s, List<String> list) {
+        this.s = s;
+        this.list = list;
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("Immutable'{'s=''{0}'', list={1}'}'", s, list);
+    }
+
+    public String getS() {
+        return s;
+    }
+
+    public List<String> getList() {
+        return list;
+    }
+}
+
+
+class Play {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        Immutable obj = new Immutable("a", list);
+        System.out.println("Before: " + obj);
+
+        // --- can I change obj' state here?
+
+        System.out.println("After: " + obj);
+    }
+}
