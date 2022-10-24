24f72f6a3d7e5919a0b9ffb5053bcf0bbadb66fd
Victor Rentea <victorrentea@gmail.com>
2022-10-24 14:42:51 +0300

    yolo

--- src/test/java/victor/training/patterns/adapter/AdapterArchUnitTest.java
+++ src/test/java/victor/training/patterns/adapter/AdapterArchUnitTest.java
@@ -2,18 +2,19 @@ package victor.training.patterns.adapter;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
-import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import org.junit.Test;
 
-public class AdapterArchUnitTest {
-   @Test
-   public void dependencyInversionTest() {
-      JavaClasses classes = new ClassFileImporter().importPackages("victor.training.patterns.adapter");
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
-      ArchRuleDefinition.noClasses()
-          .that().resideInAPackage("..domain")
-          .should()
-          .dependOnClassesThat().resideInAPackage("..infra")
-          .check(classes);
+public class AdapterArchUnitTest {
+
+   private final JavaClasses allClasses = new ClassFileImporter().importPackages("victor.training.patterns.adapter");
+
+   @Test
+   // ⚠️ do NOT remove: call me instead: +407200195644 (the anArchitect)
+   public void dependencyInversionTest() {
+      noClasses().that().resideInAPackage("..domain")
+          .should().dependOnClassesThat().resideInAPackage("..infra")
+          .check(allClasses);
    }
 }
