cc2e930f50eceeb5ba1152f5786e0f9219471945
Victor Rentea <victorrentea@gmail.com>
2022-10-24 15:47:14 +0300

    Decorator pattern :another implem of an interface that wraps the original object.

--- src/main/java/victor/training/patterns/adapter/infra/DummyData.java
+++ src/main/java/victor/training/patterns/adapter/infra/DummyData.java
@@ -9,7 +9,7 @@ public class DummyData {
 	static {
 		ldapUser1.setfName("John");
 		ldapUser1.setlName("DOE");
-		ldapUser1.setuId("jdoe");
+//		ldapUser1.setuId("jdoe");
 		ldapUser1.setCreationDate(new Date());
 		ldapUser1.setWorkEmail("0123456789");
 		ldapUser1.setEmailAddresses(Arrays.asList(new LdapUserPhone("WORK", "jdoe@bigcorp.com")));
@@ -20,7 +20,7 @@ public class DummyData {
 	static {
 		ldapUser2.setfName("Jane");
 		ldapUser2.setlName("DOE");
-		ldapUser2.setuId("janedoe");
+//		ldapUser2.setuId("janedoe");
 		ldapUser2.setCreationDate(new Date());
 	}
 }
--- src/main/java/victor/training/patterns/proxy/ProxyIntro.java
+++ src/main/java/victor/training/patterns/proxy/ProxyIntro.java
@@ -1,13 +1,17 @@
 package victor.training.patterns.proxy;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 @SpringBootApplication
 public class ProxyIntro {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         // Play the role of Spring here (there's no framework)
         // TODO 1 : LOG the arguments of any invocation of a method in Maths w/ decorator
         // TODO 2 : without changing anything below the line (w/o any interface)
@@ -15,7 +19,9 @@ public class ProxyIntro {
 
         Maths maths = new Maths();
 
-        SecondGrade secondGrade = new SecondGrade(maths);
+//        new BufferedReader(new BufferedReader(new BufferedReader(new FileReader("cute.shit"))));
+
+        SecondGrade secondGrade = new SecondGrade(new MathsWithLog(new MathsWithLog(maths)));
 
         new ProxyIntro().run(secondGrade);
 
@@ -33,11 +39,32 @@ public class ProxyIntro {
 
 }
 
-@Service
-class SecondGrade {
-    private final Maths maths;
+@Slf4j
+class MathsWithLog implements IMaths {
+    private final IMaths delegate;
 
-    SecondGrade(Maths maths) {
+    MathsWithLog(IMaths delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    public int sum(int a, int b) {
+        log.info("Calling sum " + a + ", " + b);
+        return delegate.sum(a,b);
+    }
+
+    @Override
+    public int product(int a, int b) {
+        log.info("Calling product " + a + ", " + b);
+        return delegate.product(a, b);
+    }
+}
+
+@Service
+class SecondGrade { // my daughter
+    private final IMaths maths;
+
+    SecondGrade(IMaths maths) {
         this.maths = maths;
     }
 
@@ -47,13 +74,20 @@ class SecondGrade {
         System.out.println("2x3=" + maths.product(2, 3));
     }
 }
+//class MathsLogged extends  Maths {}
+interface IMaths{
+    int sum(int a, int b);
 
+    int product(int a, int b);
+}
 @Facade
-class Maths {
+class Maths implements IMaths {
+    @Override
     public int sum(int a, int b) {
         return a + b;
     }
 
+    @Override
     public int product(int a, int b) {
         int total = 0;
         for (int i = 0; i < a; i++) {
