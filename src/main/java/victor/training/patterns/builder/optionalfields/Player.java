4bd56b57090aa2334e53532929ed686fdd3e0a7a
Victor Rentea <victorrentea@gmail.com>
2022-10-25 10:14:44 +0300

    chained setters for mutable data

--- src/main/java/victor/training/patterns/builder/optionalfields/Player.java
+++ src/main/java/victor/training/patterns/builder/optionalfields/Player.java
@@ -1,29 +1,91 @@
 package victor.training.patterns.builder.optionalfields;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.NonNull;
-import lombok.Value;
-
 
 public class Player {
 
-    int id; // *required
-    String firstName; // *required
-    String lastName;
-    Integer age; // *required
-    String profilePhotoUrl; // *required
-    String bonusPackage;
-    long penalty;
+    private int id; // *required
+    private String firstName; // *required
+    private String lastName;
+    private Integer age; // *required
+    private String profilePhotoUrl; // *required
+    private String bonusPackage;
+    private long penalty;
 
+    public int getId() {
+        return id;
+    }
+
+    public Player setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public Player setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Player setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public Player setAge(Integer age) {
+        this.age = age;
+        return this;
+    }
+
+    public String getProfilePhotoUrl() {
+        return profilePhotoUrl;
+    }
+
+    public Player setProfilePhotoUrl(String profilePhotoUrl) {
+        this.profilePhotoUrl = profilePhotoUrl;
+        return this;
+    }
+
+    public String getBonusPackage() {
+        return bonusPackage;
+    }
+
+    public Player setBonusPackage(String bonusPackage) {
+        this.bonusPackage = bonusPackage;
+        return this;
+    }
+
+    public long getPenalty() {
+        return penalty;
+    }
+
+    public Player setPenalty(long penalty) {
+        this.penalty = penalty;
+        return this;
+    }
 }
 
 class PlayerPlay {
 
     public static void main(String[] args) {
-        Player player = new Player();
-        player.id = 1;
+        Player player = john().setFirstName(null);
 
         System.out.println(player);
+    }
+
+    private static Player john() {
+        return new Player()
+                .setId(1)
+                .setFirstName("John");
     }
 }
