665cb31c6f79131d5c2a4397fd16f5b280f64b7e
Victor Rentea <victorrentea@gmail.com>
2022-10-25 10:16:49 +0300

    immutable obj + canonical ctor

--- src/main/java/victor/training/patterns/builder/optionalfields/Player.java
+++ src/main/java/victor/training/patterns/builder/optionalfields/Player.java
@@ -1,91 +1,27 @@
 package victor.training.patterns.builder.optionalfields;
 
 
+import lombok.Data;
+
+@Data // + @see lombok.config
 public class Player {
 
-    private int id; // *required
-    private String firstName; // *required
-    private String lastName;
-    private Integer age; // *required
-    private String profilePhotoUrl; // *required
-    private String bonusPackage;
-    private long penalty;
+    private final int id; // *required
+    private final String firstName; // *required
+    private final String lastName;
+    private final Integer age; // *required
+    private final String profilePhotoUrl; // *required
+    private final String bonusPackage;
+    private final long penalty;
 
-    public int getId() {
-        return id;
-    }
-
-    public Player setId(int id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public Player setFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Player setLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public Player setAge(Integer age) {
-        this.age = age;
-        return this;
-    }
-
-    public String getProfilePhotoUrl() {
-        return profilePhotoUrl;
-    }
-
-    public Player setProfilePhotoUrl(String profilePhotoUrl) {
-        this.profilePhotoUrl = profilePhotoUrl;
-        return this;
-    }
-
-    public String getBonusPackage() {
-        return bonusPackage;
-    }
-
-    public Player setBonusPackage(String bonusPackage) {
-        this.bonusPackage = bonusPackage;
-        return this;
-    }
-
-    public long getPenalty() {
-        return penalty;
-    }
-
-    public Player setPenalty(long penalty) {
-        this.penalty = penalty;
-        return this;
-    }
 }
 
 class PlayerPlay {
 
     public static void main(String[] args) {
-        Player player = john().setFirstName(null);
+        Player player = new Player(1, "John", null, 36, "photo.jpg", "aa",0);
 
         System.out.println(player);
     }
 
-    private static Player john() {
-        return new Player()
-                .setId(1)
-                .setFirstName("John");
-    }
 }
