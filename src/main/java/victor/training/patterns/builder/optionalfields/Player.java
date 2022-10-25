de52f6ce48498b259ad38b28db421ba085edf601
Victor Rentea <victorrentea@gmail.com>
2022-10-25 10:18:05 +0300

    immutable obj + null checks

--- src/main/java/victor/training/patterns/builder/optionalfields/Player.java
+++ src/main/java/victor/training/patterns/builder/optionalfields/Player.java
@@ -3,9 +3,10 @@ package victor.training.patterns.builder.optionalfields;
 
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data // + @see lombok.config
 public class Player {
-
     private final int id; // *required
     private final String firstName; // *required
     private final String lastName;
@@ -14,6 +15,15 @@ public class Player {
     private final String bonusPackage;
     private final long penalty;
 
+    public Player(int id, String firstName, String lastName, Integer age, String profilePhotoUrl, String bonusPackage, long penalty) {
+        this.id = id;
+        this.firstName = Objects.requireNonNull(firstName);
+        this.lastName = lastName;
+        this.age = Objects.requireNonNull(age);
+        this.profilePhotoUrl = Objects.requireNonNull(profilePhotoUrl);
+        this.bonusPackage = bonusPackage;
+        this.penalty = penalty;
+    }
 }
 
 class PlayerPlay {
