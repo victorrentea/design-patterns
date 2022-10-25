b9a03c43b71f1999afddebddddc8aef558f6e40a
Victor Rentea <victorrentea@gmail.com>
2022-10-25 12:56:09 +0300

    Testing of a Loan pattern is very clean

--- src/test/java/victor/training/patterns/template/FileExporterTest.java
+++ src/test/java/victor/training/patterns/template/FileExporterTest.java
@@ -1,56 +1,50 @@
-package victor.training.patterns.template;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import victor.training.patterns.template.support.Order;
-import victor.training.patterns.template.support.OrderRepo;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.when;
-
-class FileExporterTest {
-    OrderRepo orderRepo = Mockito.mock(OrderRepo.class);
-    FileExporter exporter;
-
-    File exportFolder = Files.createTempDirectory("test").toFile();
-
-    FileExporterTest() throws IOException {
-    }
-
-    @BeforeEach
-    final void before() {
-        exporter = new FileExporter(orderRepo, exportFolder);
-    }
-    @AfterEach
-    void after() {
-        // TODO cleanup delete temp folder.
-    }
-
-    @Test
-    void fragileTest() throws IOException {
-        Order order = new Order().setId(1L).setCustomerId(13L).setAmount(10D);
-        when(orderRepo.findByActiveTrue()).thenReturn(List.of(order));
-
-        File exportedFile = exporter.exportOrders("orders.csv", writer1 -> {
-            writer1.write("OrderID;CustomerId;Date\n");
-
-            for (Order order : orderRepo.findByActiveTrue()) {
-                String csv = order.getId() + ";" + order.getCustomerId() + ";" + order.getAmount() + "\n";
-                writer1.write(csv);
-            }
-        });
-
-        String contents = Files.readString(exportedFile.toPath());
-        assertThat(contents).isEqualTo("""
-                OrderID;Date
-                1;13;10.0
-                """);
-    }
-}
+//package victor.training.patterns.template;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.Mockito;
+//import victor.training.patterns.template.support.Order;
+//import victor.training.patterns.template.support.OrderRepo;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.nio.file.Files;
+//import java.util.List;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.mockito.Mockito.when;
+//
+//class FileExporterTest {
+//    OrderRepo orderRepo = Mockito.mock(OrderRepo.class);
+//    FileExporter exporter;
+//
+//    File exportFolder = Files.createTempDirectory("test").toFile();
+//
+//    FileExporterTest() throws IOException {
+//    }
+//
+//    @BeforeEach
+//    final void before() {
+//        exporter = new FileExporter(orderRepo, exportFolder);
+//    }
+//    @AfterEach
+//    void after() {
+//        // TODO cleanup delete temp folder.
+//    }
+//
+// this is how testing was performed before: involving infrastructure in testing the format (FILES)
+//    @Test
+//    void fragileTest() throws IOException {
+//        Order order = new Order().setId(1L).setCustomerId(13L).setAmount(10D);
+//        when(orderRepo.findByActiveTrue()).thenReturn(List.of(order));
+//
+//        File exportedFile = exporter.exportOrders();
+//
+//        String contents = Files.readString(exportedFile.toPath());
+//        assertThat(contents).isEqualTo("""
+//                OrderID;Date
+//                1;13;10.0
+//                """);
+//    }
+//}
---
+++ src/test/java/victor/training/patterns/template/OrderContentWriterTest.java
@@ -0,0 +1,40 @@
+package victor.training.patterns.template;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import victor.training.patterns.template.support.Order;
+import victor.training.patterns.template.support.OrderRepo;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class OrderContentWriterTest {
+    @Mock
+    OrderRepo orderRepo;
+    @InjectMocks
+    OrderContentWriter target;
+
+    @Test
+    void writeOrdersCSV() throws IOException {
+        Order order = new Order().setId(1L).setCustomerId(13L).setAmount(10D);
+        when(orderRepo.findByActiveTrue()).thenReturn(List.of(order));
+        StringWriter writer = new StringWriter();
+
+        target.writeOrdersCSV(writer);
+
+        String contents = writer.toString();
+        assertThat(contents).isEqualTo("""
+                OrderID;CustomerId;Date
+                1;13;10.0
+                """);
+    }
+}
