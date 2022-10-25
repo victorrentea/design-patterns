f4ab1efecf98ce2a69176401e1c4cd9d73ebadfc
Victor Rentea <victorrentea@gmail.com>
2022-10-25 13:47:38 +0300

    filter working

--- src/main/java/victor/training/patterns/strategy/HttpFilter.java
+++ src/main/java/victor/training/patterns/strategy/HttpFilter.java
@@ -1,14 +1,17 @@
 package victor.training.patterns.strategy;
 
+import org.springframework.stereotype.Component;
+
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
+@Component
 public class HttpFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
-        System.out.println(request.getHeader("Authorization"));
+        System.out.println("Browser language: " + request.getHeader("Accept-Language"));
 
         chain.doFilter(req, res);
 
