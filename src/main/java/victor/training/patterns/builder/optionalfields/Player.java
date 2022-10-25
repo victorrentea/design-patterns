9a1a52f20167cfe0dd7ae8552e930ddc6f6d5694
Victor Rentea <victorrentea@gmail.com>
2022-10-25 10:21:39 +0300

    wither

--- src/main/java/victor/training/patterns/builder/optionalfields/Player.java
+++ src/main/java/victor/training/patterns/builder/optionalfields/Player.java
@@ -18,6 +18,7 @@ public class Player {
     private final String bonusPackage;
     private final long penalty;
 
+    // canonical cto
     public Player(int id, String firstName, String lastName, Integer age, String profilePhotoUrl, String bonusPackage, long penalty) {
         this.id = id;
         this.firstName = Objects.requireNonNull(firstName);
@@ -26,6 +27,14 @@ public class Player {
         this.profilePhotoUrl = Objects.requireNonNull(profilePhotoUrl);
         this.bonusPackage = bonusPackage;
         this.penalty = penalty;
+    }
+    public Player(int id, String firstName, Integer age, String profilePhotoUrl) {
+        this(id, firstName, null, age, profilePhotoUrl, null, null);
+    }
+
+    //± data class copy()
+    public Player withLastName(String lastName) {
+        return new Player(id, firstName, lastName, age, profilePhotoUrl, bonusPackage, penalty);
     }
 
     public Optional<String> getLastName() {
@@ -41,6 +50,9 @@ class PlayerPlay {
 
     public static void main(String[] args) {
         Player player = new Player(1, "John", null, 36, "photo.jpg", "aa",0);
+        Player player2 = new Player(1, "John", 36, "photo.jpg")
+                .withLastName("Doe");
+
 
         System.out.println(player);
     }
