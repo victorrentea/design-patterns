4b17fe3af5b33f7f573fb9bc03b9154867267911
Victor Rentea <victorrentea@gmail.com>
2022-10-25 12:51:21 +0300

    LOAN PATTERN: the function passed as arg uses a resource MANAGED for it by the infrastructure code left outside (in the FileExporter)

--- src/main/java/victor/training/patterns/template/Template2_Export.java
+++ src/main/java/victor/training/patterns/template/Template2_Export.java
@@ -1,26 +1,60 @@
 package victor.training.patterns.template;
 
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.commons.io.function.IOConsumer;
+import org.springframework.stereotype.Service;
 import victor.training.patterns.template.support.Order;
 import victor.training.patterns.template.support.OrderRepo;
+import victor.training.patterns.template.support.Product;
+import victor.training.patterns.template.support.ProductRepo;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 
 @RequiredArgsConstructor
 public class Template2_Export {
     private final FileExporter exporter;
-
+    private final OrderContentWriter orderContentWriter;
+    private final ProductsContentWriter productsContentWriter;
     public void exportOrders() throws Exception {
-        exporter.exportOrders();
+        exporter.exportOrders("products.csv", orderContentWriter::writeOrdersCSV);
     }
 
     public void exportProducts() throws Exception {
         // TODO 'the same way you did the export of orders'
         // RUN UNIT TESTS!
+        exporter.exportOrders("products.csv",
+                writer -> productsContentWriter.writeOrdersCSV(writer));
+    }
+}
+
+@RequiredArgsConstructor
+@Service
+class OrderContentWriter {
+    private final OrderRepo orderRepo;
+
+    public void writeOrdersCSV(Writer writer) throws IOException {
+        writer.write("OrderID;CustomerId;Date\n");
+
+        for (Order order : orderRepo.findByActiveTrue()) {
+            String csv = order.getId() + ";" + order.getCustomerId() + ";" + order.getAmount() + "\n";
+            writer.write(csv);
+        }
+    }
+}
+@RequiredArgsConstructor
+@Service
+class ProductsContentWriter {
+    private final ProductRepo productRepo;
+
+    public void writeOrdersCSV(Writer writer) throws IOException {
+        writer.write("ID;ProductName;Price\n");
+
+        for (Product product : productRepo.findAll()) {
+            writer.write(product.getId() + ";" + product.getName() + ";" + product.getPrice()+"\n");
+        }
     }
 }
 
@@ -34,18 +68,13 @@ class FileExporter {
         this.exportFolder = exportFolder;
     }
 
-    public File exportOrders() {
-        File file = new File(exportFolder, "orders.csv");
+    public File exportOrders(String fileName, IOConsumer<Writer> ioConsumer) {
+        File file = new File(exportFolder, fileName);
         long t0 = System.currentTimeMillis();
         try (Writer writer = new FileWriter(file)) {
             System.out.println("Starting export to: " + file.getAbsolutePath());
 
-            writer.write("OrderID;Date\n");
-
-            for (Order order : orderRepo.findByActiveTrue()) {
-                String csv = order.getId() + ";" + order.getCustomerId() + ";" + order.getAmount() + "\n";
-                writer.write(csv);
-            }
+            ioConsumer.accept(writer);
 
             System.out.println("File export completed: " + file.getAbsolutePath());
             return file;
@@ -53,7 +82,8 @@ class FileExporter {
             System.out.println("Pretend: Send Error Notification Email"); // TODO CR: only for export orders, not for products
             throw new RuntimeException("Error exporting data", e);
         } finally {
-            System.out.println("Pretend: Metrics: Export finished in: " + (System.currentTimeMillis() - t0));
+            System.out.println("Pretend: Metrics: Export finished in: " +
+                               (System.currentTimeMillis() - t0));
         }
     }
 
