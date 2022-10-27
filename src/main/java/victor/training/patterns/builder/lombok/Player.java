ccd33f775bdcf79f3293a0b2db4bc282839e255d
Victor Rentea <victorrentea@gmail.com>
2022-10-27 17:38:01 +0300

    builder to cover for a terrible ctor

--- src/main/java/victor/training/patterns/builder/lombok/Player.java
+++ src/main/java/victor/training/patterns/builder/lombok/Player.java
@@ -1,20 +1,63 @@
 package victor.training.patterns.builder.lombok;
 
-import lombok.Data;
+import lombok.Builder;
 import victor.training.patterns.util.pretend.Entity;
 
+@Builder
 @Entity
-@Data
 public class Player {
-    private Long id; // *required
-    private String firstName; // *required
-    private String lastName;
-    private Integer age; // *required
-    private String profilePhotoId; // *required
-    private String bonusPackage;
-    private long penalty;
-    private String country;
+    private final Long id; // *required
+    private final String firstName; // *required
+    private final String lastName;
+    private final Integer age; // *required
+    private final String profilePhotoId; // *required
+    private final String bonusPackage;
+    private final long penalty;
+    private final String country;
 
+    // canonical ctor
+    public Player(Long id, String firstName, String lastName, Integer age, String profilePhotoId, String bonusPackage, long penalty, String country) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.profilePhotoId = profilePhotoId;
+        this.bonusPackage = bonusPackage;
+        this.penalty = penalty;
+        this.country = country;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getProfilePhotoId() {
+        return profilePhotoId;
+    }
+
+    public String getBonusPackage() {
+        return bonusPackage;
+    }
+
+    public long getPenalty() {
+        return penalty;
+    }
+
+    public String getCountry() {
+        return country;
+    }
 }
 
 // imagine no Hibernate.....
--- src/main/java/victor/training/patterns/builder/lombok/PlayerPlay.java
+++ src/main/java/victor/training/patterns/builder/lombok/PlayerPlay.java
@@ -2,9 +2,16 @@ package victor.training.patterns.builder.lombok;
 
 public class PlayerPlay {
     public static void main(String[] args) {
-//        new Player()
-//                .setId(1L)
-//                .setFirstName("John")
-//                ...
+
+        new Player(1L,
+               "John", "DOE", 36,
+                "photoid", "bonuspackage1", 0, "RO");
+
+        Player player = Player.builder()
+                .id(1L)
+                .firstName("John")
+                .lastName("DOE")
+                .profilePhotoId("photoId")
+                .build();
     }
 }
