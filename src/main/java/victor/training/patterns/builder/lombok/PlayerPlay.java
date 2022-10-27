ce3dcf663df63c47ab14b19a1e948d9b63cc854f
Victor Rentea <victorrentea@gmail.com>
2022-10-27 18:00:49 +0300

    builder + flavors

--- src/main/java/victor/training/patterns/builder/lombok/PlayerPlay.java
+++ src/main/java/victor/training/patterns/builder/lombok/PlayerPlay.java
@@ -7,15 +7,29 @@ public class PlayerPlay {
                "John", "DOE", 36,
                 "photoid", "bonuspackage1", 0, "RO");
 
-        new Player(1L, "John", 36, "photo");
 
-        Player player = Player.builder()
-                .id(1L)
-                .firstName("John")
-                .lastName("DOE")
-                .profilePhotoId("photoId")
+
+
+        new Player(1L,
+                "John",
+                36,
+                "photo")
+                .withCountry("RO");
+
+        Player player = aPlayer()
                 .build();
 
+        new Player(1L, "firs", 36, "profile");
+
 //        player.getProfilePhotoId().toUpperCase()// NPE
+    }
+
+    private static Player.PlayerBuilder aPlayer() {
+        return Player.builder(1L,"first", 36,"photo")
+//                .id(1L)
+                // weakness of a builder:
+                    // it throws an error at runtime for a REQUIRED field
+                .lastName("DOE")
+//                .profilePhotoId("photoId");
     }
 }
