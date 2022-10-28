763d6199567b3cce9610f0fd9bbc1cc1f407a87a
Victor Rentea <victorrentea@gmail.com>
2022-10-28 15:03:30 +0300

    Strategy Pattern: complex alternative ways to implement the same task (same interface) + POLYMORPHIC CALL at line 20 :) ❤️

--- src/main/java/victor/training/patterns/strategy/Strategy.java
+++ src/main/java/victor/training/patterns/strategy/Strategy.java
@@ -1,10 +1,6 @@
 package victor.training.patterns.strategy;
 
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Service;
 
@@ -20,43 +16,44 @@ class CustomsService {
     //	private Map<String, Class<? extends TaxCalculator>> calculators; // configured in application.properties 😮
 
     public double calculateCustomsTax(Parcel parcel) { // UGLY API we CANNOT change
+        TaxCalculator taxCalculator = selectCalculator(parcel);
+        return taxCalculator.calculate(parcel);
+    }
+
+    // switch rules: 1) the only thing in its method. 2) each case: = 1 line 3) always default throw (unless java 17)
+    private static TaxCalculator selectCalculator(Parcel parcel) {
+        // DON't EVER DO THIS!!!!!
         switch (parcel.originCountry()) {
             case "UK":
-                return calculateBrexitTax(parcel);
+                return new BrexitTaxCalculator();
             case "CN":
-                return calculateChinaTax(parcel);
+                return new ChinaTaxCalculator();
             case "FR":
             case "ES": // other EU country codes...
             case "RO":
-                return calculateEUTax(parcel);
+                return new EUTaxCalculator();
             default:
                 throw new IllegalArgumentException("Not a valid country ISO2 code: " + parcel.originCountry());
         }
     }
+}
+interface TaxCalculator {
+    double calculate(Parcel parcel);
+}
 
-    private static double calculateEUTax(Parcel parcel) {
+class BrexitTaxCalculator implements TaxCalculator{
+    public double calculate(Parcel parcel) {
+        // in fact ... 40 lines , 12 if statements
+        return parcel.tobaccoValue() / 2 + parcel.regularValue();
+    }
+}
+class EUTaxCalculator implements TaxCalculator{
+    public double calculate(Parcel parcel) {
         return parcel.tobaccoValue() / 3;
     }
-
-    private static double calculateChinaTax(Parcel parcel) {
+}
+class ChinaTaxCalculator implements TaxCalculator{
+    public double calculate(Parcel parcel) {
         return parcel.tobaccoValue() + parcel.regularValue();
-    }
-
-    private static double calculateBrexitTax(Parcel parcel) {
-        // in fact ... 40 lines , 12 if statements
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        // in fact ... 40 lines
-        return parcel.tobaccoValue() / 2 + parcel.regularValue();
     }
 }
