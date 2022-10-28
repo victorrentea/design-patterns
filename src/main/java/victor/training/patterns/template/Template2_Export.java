bde5b5218aec8d2ba9ef1f002f76a9d00bf1c495
Victor Rentea <victorrentea@gmail.com>
2022-10-28 17:34:46 +0300

    Template Method pattern (very en vogue until 9 years ago in Java) an antipattern today? mostly

--- src/main/java/victor/training/patterns/template/Template2_Export.java
+++ src/main/java/victor/training/patterns/template/Template2_Export.java
@@ -1,22 +1,16 @@
 package victor.training.patterns.template;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.io.function.IOConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Service;
-import victor.training.patterns.facade.entity.Customer;
 import victor.training.patterns.template.support.Order;
 import victor.training.patterns.template.support.OrderRepo;
-import victor.training.patterns.template.support.ProductRepo;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Objects;
 
 @RequiredArgsConstructor
@@ -24,79 +18,27 @@ public class Template2_Export {
     private final FileExporter exporter;
 
     public void exportOrders() throws Exception {
-        exporter.exportOrders(w -> orderContentWriter.writeExport(w));
+        exporter.exportOrders();
     }
 
-    @Autowired
-    private OrderContentWriter orderContentWriter;
-    @Autowired
-    private ProductContentWriter productContentWriter;
-
     public void exportProducts() throws Exception {
-        exporter.exportOrders(w -> productContentWriter.writeExport(w));
-
         // TODO 'the same way you did the export of orders'
         // RUN UNIT TESTS!
     }
 }
-@RequiredArgsConstructor
-@Service
-class OrderContentWriter {
-    private final OrderRepo orderRepo;
-
-    public void writeExport(Writer writer) throws IOException {
-        writer.write("OrderID;Date\n");
-
-        for (Order order : orderRepo.findByActiveTrue()) {
-            String csv = order.getId() + ";" + order.getCustomerId() + ";" + order.getAmount() + "\n";
-            writer.write(csv);
-        }
-    }
-}
-@RequiredArgsConstructor
-@Service
-class ProductContentWriter {
-    private final ProductRepo productRepo;
-
-    public void writeExport(Writer writer) throws IOException {
-        writer.write("Prodycts....\n");
-
-        //loan pattern:
-        // some infrastructure code (Spring's JDBC template)
-        // take a function from you that will work with a ResultSet that is managed from the infra
-        // ie. JdbcTemplate will rs.close() and open the rs on the prepared statement.
-        JdbcTemplate jdbcTemplate = new JdbcTemplate();
-        jdbcTemplate.queryForObject("SQL", (rowInTheResultSet, rowNum) -> {
-            String name = rowInTheResultSet.getString(1);
-            Customer customer = new Customer().setName(name);
-            return customer;
-        });
-        //TODO
-    }
-}
 
 
-// what matters /changes/ where most bugs will be
-// =arch = the art of drawing lines =========================
-// infra
+abstract class FileExporter {
+    @Autowired
+    private  File exportFolder;
 
-class FileExporter {
-    private final OrderRepo orderRepo;
-    private final File exportFolder;
-
-    public FileExporter(OrderRepo orderRepo, File exportFolder) {
-        this.orderRepo = orderRepo;
-        this.exportFolder = exportFolder;
-    }
-
-    public File exportOrders(IOConsumer<Writer> ioConsumer) {
+    public File exportOrders() {
         File file = new File(exportFolder, "orders.csv");
         long t0 = System.currentTimeMillis();
         try (Writer writer = new FileWriter(file)) {
             System.out.println("Starting export to: " + file.getAbsolutePath());
 
-            // LOAN: the function that I receive as an arg will loan from me a Writer that I created/closed/managed for it
-            ioConsumer.accept(writer);
+            writeContent(writer);
 
             System.out.println("File export completed: " + file.getAbsolutePath());
             return file;
@@ -107,7 +49,6 @@ class FileExporter {
             System.out.println("Pretend: Metrics: Export finished in: " + (System.currentTimeMillis() - t0));
         }
     }
-
     public String escapeCell(Object cellValue) {
         if (cellValue instanceof String s) {
             if (!s.contains("\n")) return s;
@@ -115,5 +56,32 @@ class FileExporter {
         } else {
             return Objects.toString(cellValue);
         }
+    }
+
+    // we defer the implem of this missing step to subclasses
+    protected abstract void writeContent(Writer writer) throws IOException;
+
+}
+
+class OrderExporter extends FileExporter{
+    @Autowired
+    private  OrderRepo orderRepo;
+
+    protected void writeContent(Writer writer) throws IOException {
+        writer.write("OrderID;Date\n");
+
+        for (Order order : orderRepo.findByActiveTrue()) {
+            String csv = order.getId() + ";" + order.getCustomerId() + ";" + order.getAmount() + "\n";
+            writer.write(csv);
+        }
+    }
+}
+class ProductExporter extends FileExporter{
+    @Autowired
+    private  OrderRepo orderRepo;
+
+    protected void writeContent(Writer writer) throws IOException {
+        writer.write("Products...\n");
+        // TODO
     }
 }
