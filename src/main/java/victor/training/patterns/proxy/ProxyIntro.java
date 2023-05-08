75806c4d2046f06999d3477960ea9bc8966a9b1f
Victor Rentea <victorrentea@gmail.com>
2023-05-08 15:19:19 +0300

    Decorator

--- src/main/java/victor/training/patterns/proxy/ProxyIntro.java
+++ src/main/java/victor/training/patterns/proxy/ProxyIntro.java
@@ -2,10 +2,6 @@ package victor.training.patterns.proxy;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Service;
 
 //@SpringBootApplication
 public class ProxyIntro {
@@ -15,7 +11,7 @@ public class ProxyIntro {
         // TODO 2 : without changing anything below the line (w/o any interface)
         // TODO 3 : so that any new methods in Maths are automatically logged [hard]
 
-        Maths maths = new MathsWithLogger();
+        IMaths maths = new MathsWithLogger(new Maths());
 
         SecondGrade secondGrade = new SecondGrade(maths);
 
@@ -33,19 +29,35 @@ public class ProxyIntro {
         secondGrade.mathClass();
     }
 }
+interface IMaths {
+    int sum(int a, int b);
+    int product(int a, int b);
+}
 
-class MathsWithLogger extends Maths {
+//contine, nu e
+//has-a nu is-a
+//
+//favor composition (field)
+// over inheritance (extends)
+// > Decorator (aka delegate)
+class MathsWithLogger implements IMaths { // sa extinzi clase concrete e de evitat
+    private final IMaths delegate;
     private static final Logger log = LoggerFactory.getLogger(MathsWithLogger.class);
+
+    MathsWithLogger(IMaths delegate) {
+        this.delegate = delegate;
+    }
+
     @Override
     public int sum(int a, int b) {
-        int r = super.sum(a, b);
+        int r = delegate.sum(a, b);
         log.info("sum({},{})={}", a, b, r);
         return r;
     }
 
     @Override
     public int product(int a, int b) {
-        int r = super.product(a, b);
+        int r = delegate.product(a, b);
         log.info("product({},{})={}", a, b, r);
         return r;
     }
@@ -54,9 +66,9 @@ class MathsWithLogger extends Maths {
 
 //@Service
 class SecondGrade {
-    private final Maths maths; // dependinta injectata aici de mana
+    private final IMaths maths; // dependinta injectata aici de mana
 
-    SecondGrade(Maths maths) {
+    SecondGrade(IMaths maths) {
         this.maths = maths;
     }
 
@@ -68,7 +80,7 @@ class SecondGrade {
 }
 
 //@Facade
-class Maths {
+class Maths implements IMaths {
     public int sum(int a, int b) {
         return a + b;
     }
