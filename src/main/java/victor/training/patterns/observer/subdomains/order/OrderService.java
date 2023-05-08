2260522564e4f2f8dd41756e2eef7a6792da8c7f
Victor Rentea <victorrentea@gmail.com>
2023-05-08 17:10:51 +0300

    + cuplare intre 'module'

--- src/main/java/victor/training/patterns/observer/subdomains/order/OrderService.java
+++ src/main/java/victor/training/patterns/observer/subdomains/order/OrderService.java
@@ -3,6 +3,7 @@ package victor.training.patterns.observer.subdomains.order;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import victor.training.patterns.observer.subdomains.invoice.InvoiceService;
 import victor.training.patterns.observer.subdomains.stock.StockManagementService;
 
 @Service
@@ -10,12 +11,15 @@ import victor.training.patterns.observer.subdomains.stock.StockManagementService
 public class OrderService {
     @Autowired
     private StockManagementService stockManagementService;
+    @Autowired
+    private InvoiceService invoiceService;
 
     public void placeOrder() {
         log.info("ORDER: Saving the order in the DB");
         Long orderId = 13L; // = pretend the inserted PK
 
         stockManagementService.checkStock(orderId);
+        invoiceService.generateInvoice(orderId);
         // TODO call invoicing too
     }
 }
--- src/main/java/victor/training/patterns/observer/subdomains/stock/StockManagementService.java
+++ src/main/java/victor/training/patterns/observer/subdomains/stock/StockManagementService.java
@@ -1,10 +1,12 @@
 package victor.training.patterns.observer.subdomains.stock;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class StockManagementService {
     public void checkStock(long orderId) {
-        System.out.println("STOCK: Checking stock for products in order " + orderId);
+        log.info("STOCK: Checking stock for products in order " + orderId);
     }
 }
