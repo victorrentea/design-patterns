aeb5c0ca7ef739a0850d05e47acb945ea51db9fb
Victor Rentea <victorrentea@gmail.com>
2023-09-14 15:05:33 +0300

    yolo

--- src/main/java/victor/training/patterns/strategy/HttpFilter.java
+++ src/main/java/victor/training/patterns/strategy/HttpFilter.java
@@ -12,6 +12,7 @@ import static java.lang.System.currentTimeMillis;
 public class HttpFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
         HttpServletRequest request = (HttpServletRequest) req;
         System.out.println("Browser language: " + request.getHeader("Accept-Language"));
  // inainte
