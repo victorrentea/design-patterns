3c4ffea7ac211d5386ac141a09096fe55cfed62f
Victor Rentea <victorrentea@gmail.com>
2024-01-10 17:02:50 +0200

    upgrade SB + java

--- src/main/java/victor/training/patterns/strategy/HttpFilter.java
+++ src/main/java/victor/training/patterns/strategy/HttpFilter.java
@@ -1,15 +1,15 @@
 package victor.training.patterns.strategy;
 
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Component
 public class HttpFilter implements Filter {
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         System.out.println("Browser language: " + request.getHeader("Accept-Language"));
 
