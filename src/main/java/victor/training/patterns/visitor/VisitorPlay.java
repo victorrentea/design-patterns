0482e0666685b6327954208ca2011c253513b4c4
Victor Rentea <victorrentea@gmail.com>
2024-04-24 12:44:49 +0300

    undo

--- src/main/java/victor/training/patterns/visitor/VisitorPlay.java
+++ src/main/java/victor/training/patterns/visitor/VisitorPlay.java
@@ -15,43 +15,10 @@ public class VisitorPlay {
                 new Circle(5),
                 new Square(5));
 
-        System.out.println("Total perimeter: " + oop(shapes));
-        System.out.println("Total perimeter: " + visitor(shapes));
-//        System.out.println("Total perimeter: " + sealed(shapes));
-
         double totalArea = 0;// TODO
         System.out.println("Total area: " + totalArea);
     }
 
-    private static double oop(List<Shape> shapes) {
-        double total = 0;
-        for (Shape shape : shapes) {
-            // TODO compute perimeter depending on the type of the shape (Circle, Square,...)
-        }
-        return total;
-    }
-
-
-    //<editor-fold desc="Visitor Pattern">
-    private static double visitor(List<Shape> shapes) {
-        PerimeterVisitor perimeterCalculator = new PerimeterVisitor();
-        for (Shape shape : shapes) {
-            shape.accept(perimeterCalculator);
-        }
-        return perimeterCalculator.getTotalPerimeter();
-    }
-    //</editor-fold>
-
-    //<editor-fold desc="java 19 + sealed Shape interface">
-//    private static double sealed(List<Shape> shapes) {
-//        return shapes.stream()
-//                .mapToDouble(shape -> switch (shape) {
-//                    case Square s -> s.edge() * 4;
-//                    case Circle c -> c.radius() * Math.PI * 2;
-//                })
-//                .sum();
-//    }
-    //</editor-fold>
 }
 
 
