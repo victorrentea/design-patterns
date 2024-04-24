31f59c3925a4cae54c856ad1fb4856fabc88e531
Victor Rentea <victorrentea@gmail.com>
2024-04-24 10:58:52 +0300

    CDI next

--- src/main/java/victor/training/patterns/proxy/ProxyIntro.java
+++ src/main/java/victor/training/patterns/proxy/ProxyIntro.java
@@ -24,7 +24,8 @@ public class ProxyIntro {
                 return r;
             }
         };
-        Maths proxy = (Maths) Enhancer.create(Maths.class, h); // generate a dynamic subclass of your bean
+        Maths proxy = (Maths) Enhancer.create(Maths.class, h); // generate a dynamic subclass of your bean with the
+        // CGLIB,Bytebuggy,javaassist framewokrk
 
         SecondGrade secondGrade = new SecondGrade(proxy);
 
