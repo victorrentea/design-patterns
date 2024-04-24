2402366d0a05390c96b6ac0be6e66bba5f7bcdfc
Victor Rentea <victorrentea@gmail.com>
2024-04-24 11:04:10 +0300

    AOP with CDI

--- src/main/java/victor/training/patterns/proxy/cdi/LoggedInterceptor.java
+++ src/main/java/victor/training/patterns/proxy/cdi/LoggedInterceptor.java
@@ -6,15 +6,3 @@ import jakarta.interceptor.InvocationContext;
 
 import java.io.Serializable;
 
-@Logged
-@Interceptor
-public class LoggedInterceptor implements Serializable {
-  @AroundInvoke
-  public Object logMethodEntry(InvocationContext invocationContext) throws Exception {
-    System.out.println("Entering method: "
-                       + invocationContext.getMethod().getName() + " in class "
-                       + invocationContext.getMethod().getDeclaringClass().getName());
-
-    return invocationContext.proceed();
-  }
-}
